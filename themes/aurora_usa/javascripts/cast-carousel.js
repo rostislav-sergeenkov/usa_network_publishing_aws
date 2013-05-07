3f96d0f7b89be0219a82e97e2c98063aeca26650
rebecca mullin <rebecca.mullin@nbcuni.com>
2013-05-07 11:47:19 -0400

    adding fred carousel js

---
+++ themes/aurora_usa/javascripts/cast-carousel.js
@@ -0,0 +1,43 @@
+
+// using fred carousel
+(function ($) {
+
+  Drupal.behaviors.flex_carousel = {
+    attach: function (context, settings) {
+
+    
+       $( ".view-usa-cast li" ).each(function( index ) {
+          $(this).attr('data-flexindex',index);
+        });
+        var moveTo = $('.view-usa-cast li.active').attr('data-flexindex');
+
+        $('<div class="prev btns">Prev</div><div class="next btns">Next</div>').appendTo('.view-usa-cast .view-content');
+
+        $(".view-usa-cast ul").addClass('slides').carouFredSel({
+          auto: false,
+          //circular: false,
+          items       : {
+            visible: 5,
+            start       : parseInt(moveTo),
+            },
+          prev : '.prev',
+          next    : '.next',
+          swipe       : {
+              onTouch     : true,
+              onMouse     : true
+            },
+          }, {  
+          wrapper     : {
+              classname       : "cast-carousel"
+            },
+        });
+
+        //console.log(moveTo);
+
+
+    },
+
+  };
+
+
+}(jQuery));

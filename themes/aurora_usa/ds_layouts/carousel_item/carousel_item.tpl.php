c7f7962ec4f86c27677cf2700de6c393e02b527e
suzi <suzi@thinkinkless.com>
2013-05-09 21:39:37 -0400

    new ds layout for aspot

--- themes/aurora_usa/ds_layouts/carousel_item/carousel_item.tpl.php
+++ themes/aurora_usa/ds_layouts/carousel_item/carousel_item.tpl.php
@@ -1,11 +1,14 @@
-<figure class="<?php print $classes;?> clearfix">
-  <?php if ($media): print $media; endif; ?>
-
-  <?php if ($title && $title != "&nbsp;"): ?>
-    <h4><?php print $title; ?></h4>
+<div class="<?php print $classes;?> carousel-item">
+  <?php if ($media): ?>
+  <div class="asset-img"><?php print $media; ?></div>
   <?php endif; ?>
-
-  <?php if ($caption && $caption != "&nbsp;"): ?>
-    <figcaption><?php print ($caption); ?></figcaption>
-  <?php endif; ?>
-</figure>
+  
+  <div class="title-overlay">
+    <?php if ($title && $title != "&nbsp;"): ?>
+      <div class="title"><?php print $title; ?></div>
+    <?php endif; ?>
+    <?php if ($caption && $caption != "&nbsp;"): ?>
+      <div class="caption"><?php print ($caption); ?></div>
+    <?php endif; ?>
+  </div>
+</div>

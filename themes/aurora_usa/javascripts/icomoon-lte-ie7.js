54f1996f9b73a4005807cf0a562f6e8a28328b4e
Mason Wendell <mason@canarypromo.com>
2013-05-14 11:04:28 -0400

    update icon font

--- themes/aurora_usa/javascripts/icomoon-lte-ie7.js
+++ themes/aurora_usa/javascripts/icomoon-lte-ie7.js
@@ -6,16 +6,14 @@ window.onload = function() {
 		el.innerHTML = '<span style="font-family: \'usanetwork\'">' + entity + '</span>' + html;
 	}
 	var icons = {
-			'icon-on_now' : '&#xe000;',
+			'icon-on_now' : '&#x6f;&#x6e;&#x6e;&#x6f;&#x77;',
 			'icon-arrow1' : '&#xe00d;',
 			'icon-arrow3' : '&#xe00b;',
 			'icon-close' : '&#xe00a;',
 			'icon-fb' : '&#x66;&#x61;&#x63;&#x65;&#x62;&#x6f;&#x6f;&#x6b;',
 			'icon-open' : '&#x6f;&#x70;&#x65;&#x6e;',
 			'icon-options' : '&#x6d;&#x65;&#x6e;&#x75;',
-			'icon-pin' : '&#x70;&#x69;&#x6e;&#x74;&#x65;&#x72;&#x65;&#x73;&#x74;',
 			'icon-search' : '&#x73;&#x65;&#x61;&#x72;&#x63;&#x68;',
-			'icon-tumblr' : '&#x74;&#x75;&#x6d;&#x62;&#x6c;&#x72;',
 			'icon-twitter' : '&#x74;&#x77;&#x69;&#x74;&#x74;&#x65;&#x72;',
 			'icon-usa' : '&#x263a;',
 			'icon-x1' : '&#x63;&#x6c;&#x6f;&#x73;&#x65;',
@@ -24,7 +22,9 @@ window.onload = function() {
 			'icon-arrow3-left' : '&#x2192;',
 			'icon-arrow2' : '&#x4e;&#x65;&#x78;&#x74;',
 			'icon-usa-2' : '&#x55;&#x53;&#x41;',
-			'icon-usa-3' : '&#x55;&#x53;&#x41;&#x4e;&#x65;&#x74;&#x77;&#x6f;&#x72;&#x6b;'
+			'icon-usa-3' : '&#x55;&#x53;&#x41;&#x4e;&#x65;&#x74;&#x77;&#x6f;&#x72;&#x6b;',
+			'icon-tumblr' : '&#x74;&#x75;&#x6d;&#x62;&#x6c;&#x72;',
+			'icon-pin' : '&#x70;&#x69;&#x6e;&#x74;&#x65;&#x72;&#x65;&#x73;&#x74;'
 		},
 		els = document.getElementsByTagName('*'),
 		i, attr, html, c, el;

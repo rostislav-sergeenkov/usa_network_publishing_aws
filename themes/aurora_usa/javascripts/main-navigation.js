3910c8b5f9c138bbf3c0282e495281aa8cd1a094
Mason Wendell <mason@canarypromo.com>
2013-05-15 11:19:24 -0400

    in-progress styles and js

--- themes/aurora_usa/javascripts/main-navigation.js
+++ themes/aurora_usa/javascripts/main-navigation.js
@@ -2,29 +2,42 @@
 (function ($) {
   Drupal.behaviors.mainNavigation = {
     attach: function (context, settings) {
-      console.log('your mom is so fat');
       $primary_nav = $('.primary-nav');
       // create a touch menu button
       $touch_btn = '<div id="main-menu-toggle" class="mobi-menu-icon slide-menu-toggle" data-module-type="SlideOut" data-active-layer="mega-menu"></div>'
       $primary_nav.prepend($touch_btn);
 
-      // var jPM = $.jPanelMenu({
-      //     menu: '#block-usanetwork-blocks-usa-meganav',
-      //     trigger: '#main-menu-toggle',
-      //     excludedPanelContent: '#environment-indicator'
-      // });
-      // jPM.on();
+      var jPM = $.jPanelMenu({
+          menu: '#block-usanetwork-blocks-usa-meganav',
+          trigger: '#main-menu-toggle',
+          openPosition: '258px',
+          afterOn: function() {
+            // console.log();
+            $('#jPanelMenu-menu')
+              .prepend('<h1 class="menu-title">Main Menu</h1>')
+              .find('a').removeClass('mega-nav-link').addClass('slide-panel-link');
+          },
+          excludedPanelContent: '#environment-indicator'
+      });
+      jPM.on();
 
 
-      // off_canvas_auto_close();
-      // $(window).resize(function(){
-      //   off_canvas_auto_close();
-      // });
-      // function off_canvas_auto_close() {
-      //   if ($('#main-menu-toggle').css("display") != "block" ){
-      //     jPM.close();
-      //   }
-      // }
+      off_canvas_auto_close();
+      $(window).resize(function(){
+        off_canvas_auto_close();
+      });
+      function off_canvas_auto_close() {
+        if ($('#main-menu-toggle').css("display") != "block" ){
+          jPM.close();
+          if ($('.jPanelMenu-panel')) {
+            jPM.off();
+          }
+        } else {
+          if ($('.jPanelMenu-panel').length == 0) {
+            jPM.on();
+          }
+        }
+      }
     },
   };
 

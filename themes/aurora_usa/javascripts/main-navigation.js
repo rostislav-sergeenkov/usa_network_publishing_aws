70a3afe7ecf47ee145e6ff0d0ccf419885772a77
Mason Wendell <mason@canarypromo.com>
2013-05-16 02:17:57 -0400

    js and styles for submenu trays

--- themes/aurora_usa/javascripts/main-navigation.js
+++ themes/aurora_usa/javascripts/main-navigation.js
@@ -3,6 +3,8 @@
   Drupal.behaviors.mainNavigation = {
     attach: function (context, settings) {
       $primary_nav = $('.primary-nav');
+
+      // TOUCH NAVIGATION
       // create a touch menu button
       $touch_btn = '<div id="main-menu-toggle" class="mobi-menu-icon slide-menu-toggle" data-module-type="SlideOut" data-active-layer="mega-menu"></div>'
       $primary_nav.prepend($touch_btn);
@@ -16,7 +18,10 @@
             $jPanelMenu_menu = $('#jPanelMenu-menu');
             $jPanelMenu_menu
               .prepend('<h1 class="menu-title">Main Menu</h1>')
-              .find('a').removeClass('mega-nav-link').addClass('slide-panel-link').end()
+              .find('a')
+                .removeClass('mega-nav-link')
+                .addClass('slide-panel-link')
+                .end()
               .find('.mega-sub-nav-container')
                 .removeClass('mega-sub-nav-container')
                 .addClass('panel-sub-nav-container')
@@ -40,6 +45,7 @@
                   .addClass('expandable-menu');
           },
           beforeOpen: function() {
+            $('#wall').remove();
             $('.jPanelMenu-panel')
               .append('<div id="wall" data-module-type="Wall"></div>');
           },
@@ -49,6 +55,7 @@
           excludedPanelContent: '#environment-indicator'
       });
       jPM.on();
+      $('.panel-menu-items.active').removeClass('active');
 
       off_canvas_auto_close();
       $(window).resize(function(){
@@ -59,12 +66,38 @@
           jPM.close();
           if ($('.jPanelMenu-panel')) {
             jPM.off();
+            $('.mega-menu-items.active').removeClass('active');
           }
         } else {
           if ($('.jPanelMenu-panel').length == 0) {
             jPM.on();
+            $('.panel-menu-items.active').removeClass('active');
           }
         }
+      }
+
+      // WIDE NAVIGATION
+      $('.mega-menu-items.active').removeClass('active');
+      mega_nav_panels();
+      function mega_nav_panels() {
+        $('.slide-container')
+          .find('.mega-sub-nav-container')
+            .siblings('a')
+              .css('cursor', 'default')
+              .click(function() {
+                $('.mega-menu-items.active').not($(this).parent()).removeClass('active');
+                $(this).parent().toggleClass('active');
+                if ($('.mega-menu-items.active').length == 0) {
+                  $('#wall').remove(); 
+                } else {
+                  if ($('#wall').length == 0) {
+                    $('body')
+                      .append('<div id="wall" data-module-type="Wall"></div>');
+                  }
+                }
+                return false;
+              })
+              .end()
       }
     },
   };

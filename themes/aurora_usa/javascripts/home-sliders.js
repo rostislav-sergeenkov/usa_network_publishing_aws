ec14a02996e515c93d7f181f59687e5eae5c9aed
suzi <suzi@thinkinkless.com>
2013-05-17 19:46:00 -0400

    homepage debug for slider, aspot colors more dynamic when in an array

--- themes/aurora_usa/javascripts/home-sliders.js
+++ themes/aurora_usa/javascripts/home-sliders.js
@@ -12,23 +12,26 @@
         slideshow: false,
         before: function(slider) {
           var target = slider.animatingTo,
-              currentSlide = slider.currentSlide;
+            currentSlide = slider.currentSlide;
             $secondaryslider.each(function (index, element) {
             var flexslider = $(element).data('flexslider');
             // Setting the animation direction of the secondary slider to be the 
             // same as the primary slider.
-            flexslider.direction = slider.direction;
-            flexslider.flexAnimate(target, true);
+            // but ONLY if we have more than one liabilities// else the main slider breaks
+            if ($(this).find('li').length > 1) {
+              flexslider.direction = slider.direction;
+              flexslider.flexAnimate(target, true);
+            }
           });
         }
-    });
-    $secondaryslider.flexslider({
-      animation: 'slide',
-      controlNav: false,
-      directionNav: false,
-      slideshow: false,
-      touch: false
-    });
+      });
+      $secondaryslider.flexslider({
+        animation: 'slide',
+        controlNav: false,
+        directionNav: false,
+        slideshow: false,
+        touch: false
+      });
     },
   };
 

7f22301b8f80246adab22f959dbec02ef17a3e5a
rebecca mullin <rebecca.mullin@nbcuni.com>
2013-05-17 10:15:53 -0400

    updating toggle selector

--- themes/aurora_usa/javascripts/follow-social.js
+++ themes/aurora_usa/javascripts/follow-social.js
@@ -2,8 +2,8 @@
 (function ($) {
   Drupal.behaviors.follow_social_toggle = {
     attach: function (context, settings) {
-      $('.view-display-id-attachment_1 .view-header').click(function() {
-        $( ".view-display-id-attachment_1 .view-content" ).toggle();
+      $('.view-usa-shows.view-display-id-attachment_1 .view-header').click(function() {
+        $( ".view-usa-shows.view-display-id-attachment_1 .view-content" ).toggle();
      });
     },
   };

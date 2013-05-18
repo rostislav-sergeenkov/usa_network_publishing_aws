6cb978a7453359810bfb97aa75f25f3805d57c10
suzi <suzi@thinkinkless.com>
2013-05-18 19:24:23 -0400

    video layout, tpl and css form empathy

--- themes/aurora_usa/ds_layouts/usa_video/usa_video.tpl.php
+++ themes/aurora_usa/ds_layouts/usa_video/usa_video.tpl.php
@@ -1,7 +1,35 @@
 <?php 
-  // @TODO: tve auth
-  // may be done in a block
-  // and placed into this tpl via layout
+// @TODO: tve auth
+// may be done in a block
+// and placed into this tpl via layout
+// mpx details for testing
+// account pid = OyMl-B
+// player fid = f_h1HWlNhYtQ
+//http://player.theplatform.com/p/OyMl-B/f_h1HWlNhYtQ
+// tve example
+// http://player.theplatform.com/p/dCK2IC/usa-vod-stage/embed/select/zR6MZeBntysk
+//?autoPlay=true&mbr=true&MVPDid=Cablevision#playerurl=http%3A//tve-staging-usa.nbcuni.com/anywhere/show/Psych/26167364001/1/706%253A%2520Cirque%2520Du%2520Soul
+
+// empty vars
+$player_url = '';
+$feed_url = '';
+$platform_file_id = '';
+
+// mpx video
+if ($node->type == 'usa_video') {
+  $player_url = variable_get('usanetwork_theplatform_mpx_player_url');
+  $feed_url = variable_get('usanetwork_theplatform_mpx_feed_url');
+  $platform_file_id = _usanetwork_video_platform_get_file_id($guid, $feed_url);
+}
+// tve video
+if ($node->type == 'usa_tve_video') {
+//$player_url = variable_get('usanetwork_theplatform_tve_player_url');
+  // @todo: this is temporary only!
+  $player_url = 'http://player.theplatform.com/p/dCK2IC/usa-vod-stage';
+  $feed_url = variable_get('usanetwork_theplatform_tve_feed_url');
+  $platform_file_id = _usanetwork_video_platform_get_file_id($guid, $feed_url);
+}
+
 ?>
 
 <div class="<?php print $classes;?> video usa-video featured-asset premium-asset">
@@ -18,8 +46,17 @@
     </div>
   </div>
   <div class="video-player-wrapper">
-    <?php if ($guid): ?>
-    <div class="video-player"><?php print $guid; ?></div>
+    <?php if ($guid && $player_url): ?>
+    <div class="video-player">
+      <iframe
+      class="video-iframe"
+      style="width: 100%; height: 100%"
+      src="<?php print $player_url; ?>/embed/select/<?php print $platform_file_id; ?>"
+      frameborder="0"
+      allowfullscreen>
+      Your browser does not support iframes.
+      </iframe>
+    </div>
     <?php endif; ?>
   </div>
   <?php if ($tve_auth): ?><div class="tve-auth"><?php print $tve_auth; ?></div><?php endif; ?>

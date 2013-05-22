08f3087e60e71c5979da5d93134985f48e1c3e58
Mukesh <Mukesh.Yadav@nbcuni.com>
2013-05-22 15:26:01 -0400

    Added the Apache SOLR settings and enabled Fast 404

--- settings.php
+++ settings.php
@@ -61,6 +61,12 @@ switch ($_ENV['AH_SITE_ENVIRONMENT']) {
     $conf['cache_backends'][] = './profiles/all/modules/contrib/memcache/memcache.inc';
     $conf['cache_default_class'] = 'MemCacheDrupal';
     $conf['cache_class_form'] = 'DrupalDatabaseCache';
+
+    //Acquia Search settings
+    $conf["acquia_identifier"] = "GMWX-32384";
+    $conf["acquia_key"] = "1166f38ec6b5d664b8fb6b085fde8232";
+    $conf["apachesolr_path"] = "/solr/GMWX-32384";
+    $conf['apachesolr_read_only'] = "1";
     break;
 
   case 'test':
@@ -77,6 +83,12 @@ switch ($_ENV['AH_SITE_ENVIRONMENT']) {
     $conf['cache_backends'][] = './profiles/all/modules/contrib/memcache/memcache.inc';
     $conf['cache_default_class'] = 'MemCacheDrupal';
     $conf['cache_class_form'] = 'DrupalDatabaseCache';
+
+    //Acquia Search settings
+    $conf["acquia_identifier"] = "GMWX-32384";
+    $conf["acquia_key"] = "1166f38ec6b5d664b8fb6b085fde8232";
+    $conf["apachesolr_path"] = "/solr/GMWX-32384";
+    $conf['apachesolr_read_only'] = "0";
     break;
 
   case 'prod':
@@ -92,8 +104,27 @@ switch ($_ENV['AH_SITE_ENVIRONMENT']) {
     $conf['cache_backends'][] = './profiles/all/modules/contrib/memcache/memcache.inc';
     $conf['cache_default_class'] = 'MemCacheDrupal';
     $conf['cache_class_form'] = 'DrupalDatabaseCache';
+
+    //Acquia Search settings
+    $conf["acquia_identifier"] = "CGJK-32328";
+    $conf["acquia_key"] = "dc2bfa15286aedc061f759dfd20e2f9a";
+    $conf["apachesolr_path"] = "/solr/CGJK-32328";
+    $conf['apachesolr_read_only'] = "0";
     break;
 }
+
+/**
+ * By default, fast 404s are returned as part of the normal page request
+ * process, which will properly serve valid pages that happen to match and will
+ * also log actual 404s to the Drupal log. Alternatively you can choose to
+ * return a 404 now by uncommenting the following line. This will reduce server
+ * load, but will cause even valid pages that happen to match the pattern to
+ * return 404s, rather than the actual page. It will also prevent the Drupal
+ * system log entry. Ensure you understand the effects of this before enabling.
+ *
+ * To enable this functionality, remove the leading hash sign below.
+ */
+  drupal_fast_404();
 
 /**
  * Use the page_memory_limit module to increase the page memory limit on admin

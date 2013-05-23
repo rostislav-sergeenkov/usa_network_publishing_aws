b73c01af695b34d2983e59781173e0a74ad505cc
rebecca mullin <rebecca.mullin@nbcuni.com>
2013-05-23 12:59:14 -0400

    updating layout

--- modules/custom/usanetwork_layouts/plugins/layouts/usa_showlanding/usa-showlanding.tpl.php
+++ modules/custom/usanetwork_layouts/plugins/layouts/usa_showlanding/usa-showlanding.tpl.php
@@ -1,25 +1,25 @@
 <div class="panel-display panel-onecol-stacked clearfix">
   <div class="panel-wrapper">
-    <div class="panel-panel panel-row first-row a-spot-panel">
+    <div class="panel-panel panel-one panel-row first-row a-spot-panel">
     <?php print $content['one']; ?>
     </div>
     <div class="inner-wrapper panel-row video-ads-panel">
-      <div class="panel-panel panel-two">
+      <div class="panel-panel panel-two panel-col1">
         <?php print $content['two']; ?>
       </div>
-      <div class="panel-panel">
+      <div class="panel-panel panel-three panel-col2">
         <?php print $content['three']; ?>
       </div>
     </div>
-    <div class="panel-panel panel-row promo-panel">
+    <div class="panel-panel panel-four panel-row promo-panel">
       <?php print $content['four']; ?>
     </div>
-    <?php ($content['six']) ? $class = 'one-sidebar' : ''; ?>
+    <?php ($content['six']) ? $class = 'panel-two-cols' : ''; ?>
     <div class="inner-wrapper panel-row last-row social-panel <?php print $class; ?>">
-      <div class="panel-panel chatter-content">
+      <div class="panel-panel panel-five panel-col1">
         <?php print $content['five']; ?>
       </div>
-      <div class="panel-panel sidebar">
+      <div class="panel-panel panel-six panel-col2">
         <?php print $content['six']; ?>
       </div>
     </div>

4c89a5d961896efe13643f0d7f3ef01f5ea8cd38
rebecca mullin <rebecca.mullin@nbcuni.com>
2013-05-23 20:05:17 -0400

    updating show layout

--- modules/custom/usanetwork_layouts/plugins/layouts/usa_showlanding/usa-showlanding.tpl.php
+++ modules/custom/usanetwork_layouts/plugins/layouts/usa_showlanding/usa-showlanding.tpl.php
@@ -1,21 +1,46 @@
+<?php
+  $class = '';
+  if($content['six'])  {
+    $class = 'panel-two-cols';
+  } else {
+    $class = '';
+  }
+
+?>
 <div class="panel-display panel-onecol-stacked clearfix">
   <div class="panel-wrapper">
     <div class="panel-panel panel-one panel-row first-row a-spot-panel">
     <?php print $content['one']; ?>
     </div>
-    <div class="inner-wrapper panel-row video-ads-panel">
-      <div class="panel-panel panel-two panel-col1">
-        <?php print $content['two']; ?>
-      </div>
-      <div class="panel-panel panel-three panel-col2">
-        <?php print $content['three']; ?>
-      </div>
+    <div class="inner-wrapper panel-row video-ads-panel expandable-container">
+      <div class="expandable-content" style="height: 315px">
+        <div class="panel-panel panel-two panel-col1">
+          <?php print $content['two']; ?>
+        </div>
+        <div class="panel-panel panel-three panel-col2">
+          <?php print $content['three']; ?>
+        </div>
+       </div>
+       <div class="expandable-toggle-wrap">
+        <div class="expandable-toggle">
+              <div class="more">more</div>
+              <div class="less">close</div>
+          </div>
+        </div>
     </div>
-    <div class="panel-panel panel-four panel-row promo-panel">
-      <?php print $content['four']; ?>
+    <div class="panel-panel panel-four panel-row promo-panel expandable-container">
+      <div class="expandable-content">
+          <?php print $content['four']; ?>
+      </div>
+      <div class="expandable-toggle-wrap">
+        <div class="expandable-toggle">
+              <div class="more">more</div>
+              <div class="less">close</div>
+          </div>
+        </div>
     </div>
-    <?php ($content['six']) ? $class = 'panel-two-cols' : ''; ?>
     <div class="inner-wrapper panel-row last-row social-panel <?php print $class; ?>">
+      <?php print '<h2 class="pane-title">' . t('Social') . '</h2>'; ?>
       <div class="panel-panel panel-five panel-col1">
         <?php print $content['five']; ?>
       </div>

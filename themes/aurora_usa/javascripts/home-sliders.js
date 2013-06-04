f90d950014b9f945026b95529c99d7e4342fe6a0
Mason Wendell <mason@canarypromo.com>
2013-06-04 14:05:12 -0400

    wait for dom to be ready before initing the slideshow

--- themes/aurora_usa/javascripts/home-sliders.js
+++ themes/aurora_usa/javascripts/home-sliders.js
@@ -5,34 +5,37 @@
       $mainslider = $('#main-slider');
       $secondaryslider = $('.secondary-slider');
 
-      $mainslider.flexslider({
-        animation: 'slide',
-        controlNav: true,
-        directionNav: (!Modernizr.touch),
-        slideshow: false,
-        before: function(slider) {
-          var target = slider.animatingTo,
-            currentSlide = slider.currentSlide;
-          $secondaryslider.each(function (index, element) {
-            var flexslider = $(element).data('flexslider');
-            // Setting the animation direction of the secondary slider to be the
-            // same as the primary slider.
-            // but ONLY if we have more than one list item
-            // else the main slider breaks
-            if ($(this).find('li').length > 1) {
-              flexslider.direction = slider.direction;
-              flexslider.flexAnimate(target, true);
-            }
-          });
-        }
+      $(document).ready(function() {
+        $mainslider.flexslider({
+          animation: 'slide',
+          controlNav: true,
+          directionNav: (!Modernizr.touch),
+          slideshow: false,
+          before: function(slider) {
+            var target = slider.animatingTo,
+              currentSlide = slider.currentSlide;
+            $secondaryslider.each(function (index, element) {
+              var flexslider = $(element).data('flexslider');
+              // Setting the animation direction of the secondary slider to be the
+              // same as the primary slider.
+              // but ONLY if we have more than one list item
+              // else the main slider breaks
+              if ($(this).find('li').length > 1) {
+                flexslider.direction = slider.direction;
+                flexslider.flexAnimate(target, true);
+              }
+            });
+          }
+        });
+        $secondaryslider.flexslider({
+          animation: 'slide',
+          controlNav: false,
+          directionNav: false,
+          slideshow: false,
+          touch: false
+        });
       });
-      $secondaryslider.flexslider({
-        animation: 'slide',
-        controlNav: false,
-        directionNav: false,
-        slideshow: false,
-        touch: false
-      });
+
     },
   };
 

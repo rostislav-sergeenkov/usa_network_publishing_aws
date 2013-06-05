18a78b90b79a7769509a9412e6f1458d5ee65e17
suzi <suzi@thinkinkless.com>
2013-06-04 20:20:59 -0400

    updates to ie 8 modal

--- modules/custom/usanetwork_blocks/theme/usa-detect-ieversion.tpl.php
+++ modules/custom/usanetwork_blocks/theme/usa-detect-ieversion.tpl.php
@@ -7,10 +7,10 @@
  ** 
  */
  global $base_url;
-$image_path = drupal_get_path("module", "usanetwork_blocks")."/theme/images/";
+ $image_path = drupal_get_path("module", "usanetwork_blocks")."/theme/images/";
 ?>
 <!-- Error Modals -->
-<!--[if lte IE 8]> 
+<!--[if lte IE 8]>
 	<div id="lackOfFeaturesModal-main" style = "display:none;">
 	<div id="lackOfFeaturesModal" class="reveal-modal modal authen-modal"> 
 		<a class="lackOfFeaturesModal-close-modal">Close</a>
@@ -25,6 +25,8 @@ $image_path = drupal_get_path("module", "usanetwork_blocks")."/theme/images/";
 			<h2>Your browser needs a tune up...</h2>
 			<p>To play our content, your browser needs to be the latest version. <br>Please update your browser and reload the page.</p>
 			<div id="lackOfFeaturesModalIE" class="lackOfFeaturesModal-ietext">
+			  For IE8 users - IE8 is not currently supported. Please upgrade to a more recent version of Internet Explorer or use another browser.
+			  <br />
 				For IE9 users - IE9 is supported, but its Compatibility View feature is not. The fix is simple - turn off Compatibility View mode. Here's how&#58
 				<ol class="lackOfFeaturesModal-ietextol">
 					<li>Select Tools from the Menu bar (If it's not visible, press Alt on your keyboard)</li>
@@ -34,9 +36,10 @@ $image_path = drupal_get_path("module", "usanetwork_blocks")."/theme/images/";
 				</ol>
 			</div>
 			<div id="bullet-list">
-				<div id="lackOfFeaturesModal-bullet-list1"><img src="<?php echo $base_url."/".$image_path;?>firefox.png" alt="Firefox" /></div>
-				<div id="lackOfFeaturesModal-bullet-list2"><img src="<?php echo $base_url."/".$image_path;?>safari.png" alt="Safari" /></div>
-				<div id="lackOfFeaturesModal-bullet-list3"><img src="<?php echo $base_url."/".$image_path;?>chrome.png" alt="Chrome" /></div>
+				<div id="lackOfFeaturesModal-bullet-list1"><a href="http://www.mozilla.org/en-US/firefox/new/" target="_blank"><img src="<?php echo $base_url."/".$image_path;?>firefox_150.png" alt="Firefox" /></a></div>
+				<div id="lackOfFeaturesModal-bullet-list2"><a href=" http://www.apple.com/safari/" target="_blank"><img src="<?php echo $base_url."/".$image_path;?>safari_150.png" alt="Safari" /></a></div>
+				<div id="lackOfFeaturesModal-bullet-list3"><a href="https://www.google.com/intl/en/chrome/browser/" target="_blank"><img src="<?php echo $base_url."/".$image_path;?>chrome_150.png" alt="Chrome" /></a></div>
+				<div id="lackOfFeaturesModal-bullet-list4"><a href="http://windows.microsoft.com/en-us/internet-explorer/download-ie" target="_blank"><img src="<?php echo $base_url."/".$image_path;?>ielogo_150.png" alt="Chrome" /></a></div>
 			</div>
 		</div>
 		<div id="lackOfFeaturesModal-modal-footer-main">

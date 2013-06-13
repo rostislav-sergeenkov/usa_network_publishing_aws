1aedc430a4f3ab88632bc37f63a5971accdcf9ee
rebecca mullin <rebecca.mullin@nbcuni.com>
2013-06-13 12:50:48 -0400

    scroll js

---
+++ themes/aurora_usa/javascripts/social-sync-scroll.js
@@ -0,0 +1,21 @@
+// SELECT LIST-LIKE DROP DOWN MENUS
+(function ($) {
+  Drupal.behaviors.social_sync_scroll = {
+
+    attach: function (context, settings) {
+   
+      $(window).load(function(){
+          $(".usa-sync-scroll").mCustomScrollbar({
+            horizontalScroll: false,
+            autoDraggerLength: true,
+            autoHideScrollbar: true,
+            advanced: {
+              updateOnBrowserResize: true,
+              updateOnContentResize: false
+            }
+          });
+      });
+    },
+  };
+
+}(jQuery));

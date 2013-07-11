1f2fa5803c40e7c18a5e1a3806036774d2c7b945
rebecca mullin <rebecca.mullin@nbcuni.com>
2013-07-11 12:50:01 -0400

    personalization module

---
+++ modules/custom/usanetwork_personalization/theme/usa-personalization-content.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * @file
+ * usa-personalization-content.tpl.php
+ *
+ * Theme implementation for personalization content
+ *
+ * Available variables:
+ *
+ */
+
+?>
+
+<div class="usa-personalization personalization-content">
+  <div class="col">
+    col1 lorem ipsum
+  </div>
+  <div class="col">
+     col2 lorem ipsum
+  </div> 
+  <span class="close">close</span>
+</div>

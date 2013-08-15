c1e257cfc008e510cac1c895aff35f923da49ba8
rebecca mullin <rebecca.mullin@nbcuni.com>
2013-08-15 10:46:38 -0400

    using function so only happens on page load

--- themes/aurora_usa/javascripts/show-flexslider.js
+++ themes/aurora_usa/javascripts/show-flexslider.js
@@ -2,23 +2,32 @@
 (function ($) {
   Drupal.behaviors.showAspot = {
     attach: function (context, settings) {
-      $slideshow_selector = $('#show-aspot ul');
 
-      $slideshow = (settings.showAspot.slideshow !== null)? settings.showAspot.slideshow : false;
-      $slideshowSpeed = (settings.showAspot.slideshowSpeed !== null)? settings.showAspot.slideshowSpeed : 7000;
+      function create_show_flexslider() {
+        $slideshow_selector = $('#show-aspot ul');
 
-      $slideshow_selector
-        .addClass('slides')
-        .wrap('<div class="flexslider a-spot"></div>')
-        .parent()
-        .flexslider({
-          slideshow: $slideshow,
-          slideshowSpeed: $slideshowSpeed,
-          pauseOnHover: true,
-          animation: 'slide',
-          controlNav: true,
-          directionNav: (!Modernizr.touch)
-        });
+        $slideshow = (settings.showAspot.slideshow !== null)? settings.showAspot.slideshow : false;
+        $slideshowSpeed = (settings.showAspot.slideshowSpeed !== null)? settings.showAspot.slideshowSpeed : 7000;
+
+        $slideshow_selector
+          .addClass('slides')
+          .wrap('<div class="flexslider a-spot"></div>')
+          .parent()
+          .flexslider({
+            slideshow: $slideshow,
+            slideshowSpeed: $slideshowSpeed,
+            pauseOnHover: true,
+            animation: 'slide',
+            controlNav: true,
+            directionNav: (!Modernizr.touch)
+          });
+        }  
+
+      window.onload = function() {
+        create_show_flexslider();
+      };
+
+
     },
   };
 

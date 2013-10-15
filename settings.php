3808018bff871869495909903d65ee826a147c72
Donna Vaughan <donna.vaughan@nbcuni.com>
2013-10-15 13:22:16 -0500

    dev -- adding acceptance server config to settings.php

--- settings.php
+++ settings.php
@@ -108,6 +108,34 @@ switch ($_ENV['AH_SITE_ENVIRONMENT']) {
     $conf['apachesolr_read_only'] = "0";
     break;
 
+  case 'acceptance':
+    // Envronment indicator settings.
+    $conf['environment_indicator_overwritten_name'] = 'ACCEPTANCE';
+    $conf['environment_indicator_overwritten_color'] = '#009933';
+    $conf['environment_indicator_overwritten_position'] = 'top';
+    $conf['environment_indicator_overwritten_fixed'] = FALSE;
+
+    // File path settings. Acquia automatically figures our the public and tmp
+    // file paths, however we have to set the private path manually.
+    $conf['file_private_path'] = '/mnt/files/' . $_ENV["AH_SITE_GROUP"] . 'dev/sites/default/files-private';
+
+    // Memchache settings.
+    $conf['cache_backends'][] = './profiles/all/modules/contrib/memcache/memcache.inc';
+    $conf['cache_default_class'] = 'MemCacheDrupal';
+    $conf['cache_class_form'] = 'DrupalDatabaseCache';
+
+    //Acquia Search settings
+    $conf["acquia_identifier"] = "GMWX-32384";
+    $conf["acquia_key"] = "1166f38ec6b5d664b8fb6b085fde8232";
+    $conf["apachesolr_path"] = "/solr/GMWX-32384";
+    $conf['apachesolr_read_only'] = "0";
+
+    // Turn on display PHP errors
+    error_reporting(E_ALL);
+    ini_set('display_errors', TRUE);
+    ini_set('display_startup_errors', TRUE);
+    break;
+
   case 'prod':
     // Envronment indicator settings.
     $conf['environment_indicator_overwritten_name'] = 'LIVE';
@@ -134,6 +162,7 @@ switch ($_ENV['AH_SITE_ENVIRONMENT']) {
     default_site_request_handler();
 
     break;
+
 }
 
 /**

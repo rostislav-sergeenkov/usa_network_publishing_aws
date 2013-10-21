3986ed189066aa0b2ec4faf3fddaae20a66e87e8
unknown <710509@GDCWFDT710509.igategegdc.com>
2013-10-21 08:52:28 -0400

    item #105 changes

--- modules/custom/usanetwork_tv_schedule/js/usanetwork_social_on_now.js
+++ modules/custom/usanetwork_tv_schedule/js/usanetwork_social_on_now.js
@@ -1,11 +1,7 @@
 (function ($) {
   $('#navTwitterOnNow').live("click",function(){
-    $('#navTwitterOnNow span').removeClass('navTwitter').addClass('navTwitter-selected');
-    $('#navChatterOnNow span').removeClass('navChatter-selected').addClass('navChatter');
-    $('#navFbOnNow span').removeClass('navFb-selected').addClass('navFb');
-    $('#navPinterestOnNow span').removeClass('navPinterest-selected').addClass('navPinterest');
-    $('#navGetGlueOnNow span').removeClass('navGetGlue-selected').addClass('navGetGlue');
-    $('#navInstagramOnNow span').removeClass('navInstagram-selected').addClass('navInstagram');
+    $('.onnow_social_nav').removeClass('onnow_social_selected');
+    $('#navTwitterOnNow span').addClass('onnow_social_selected');
     $('#echo-stream').show();
     $('#pinterest').hide();
     $('#pinterest').empty();
@@ -17,12 +13,8 @@
     });
   });
   $('#navChatterOnNow').live("click",function(){
-    $('#navTwitterOnNow span').removeClass('navTwitter-selected').addClass('navTwitter');
-    $('#navChatterOnNow span').removeClass('navChatter').addClass('navChatter-selected');
-    $('#navFbOnNow span').removeClass('navFb-selected').addClass('navFb');
-    $('#navPinterestOnNow span').removeClass('navPinterest-selected').addClass('navPinterest');
-    $('#navGetGlueOnNow span').removeClass('navGetGlue-selected').addClass('navGetGlue');
-    $('#navInstagramOnNow span').removeClass('navInstagram-selected').addClass('navInstagram');
+    $('.onnow_social_nav').removeClass('onnow_social_selected');
+    $('#navChatterOnNow span').addClass('onnow_social_selected');
     var node_id = $(this).attr("name");
     $.ajax({ url: "/usa-on-now-social-block/"+node_id+"/"+"chatter"}).done(function ( data ) {
       $('#socialblock_on_now_content').html(data);
@@ -35,12 +27,8 @@
   });
 
   $('#navFbOnNow').live("click",function(){
-    $('#navTwitterOnNow span').removeClass('navTwitter-selected').addClass('navTwitter');
-    $('#navChatterOnNow span').removeClass('navChatter-selected').addClass('navChatter-selected');
-    $('#navFbOnNow span').removeClass('navFb').addClass('navFb-selected');
-    $('#navPinterestOnNow span').removeClass('navPinterest-selected').addClass('navPinterest');
-    $('#navGetGlueOnNow span').removeClass('navGetGlue-selected').addClass('navGetGlue');
-    $('#navInstagramOnNow span').removeClass('navInstagram-selected').addClass('navInstagram');
+    $('.onnow_social_nav').removeClass('onnow_social_selected');
+    $('#navFbOnNow span').addClass('onnow_social_selected');
     var node_id = $(this).attr("name");
     $.ajax({ url: "/usa-on-now-social-block/"+node_id+"/"+"facebook"}).done(function ( data ) {
       $('#socialblock_on_now_content').html(data);
@@ -53,12 +41,8 @@
   });
 
   $('#navPinterestOnNow').live("click",function(){
-    $('#navPinterestOnNow span').removeClass('navPinterest').addClass('navPinterest-selected');
-    $('#navChatterOnNow span').removeClass('navChatter-selected').addClass('navChatter');
-    $('#navTwitterOnNow span').removeClass('navTwitter-selected').addClass('navTwitter');
-    $('#navFbOnNow span').removeClass('navFb-selected').addClass('navFb');
-    $('#navGetGlueOnNow span').removeClass('navGetGlue-selected').addClass('navGetGlue');
-    $('#navInstagramOnNow span').removeClass('navInstagram-selected').addClass('navInstagram');
+    $('.onnow_social_nav').removeClass('onnow_social_selected');
+    $('#navPinterestOnNow span').addClass('onnow_social_selected');  
     var pinterestUrl = $(this).attr("name");
     $('#pinterest').html('<a data-pin-do="embedUser" href="'+pinterestUrl+'"></a></a><script type="text/javascript" src="//assets.pinterest.com/js/pinit.js"></script>');
     $('#echo-stream').hide();
@@ -73,12 +57,8 @@
     var getGlueArray = getGlueUrlAndShow.split("urlshowseperator");
     var getGlueUrl = getGlueArray[0];
     var getGlueShowName = getGlueArray[1];
-    $('#navGetGlueOnNow span').removeClass('navGetGlue').addClass('navGetGlue-selected');
-    $('#navChatterOnNow span').removeClass('navChatter-selected').addClass('navChatter');
-    $('#navTwitterOnNow span').removeClass('navTwitter-selected').addClass('navTwitter');
-    $('#navFbOnNow span').removeClass('navFb-selected').addClass('navFb');
-    $('#navPinterestOnNow span').removeClass('navPinterest-selected').addClass('navPinterest');
-    $('#navInstagramOnNow span').removeClass('navInstagram-selected').addClass('navInstagram');
+    $('.onnow_social_nav').removeClass('onnow_social_selected');
+    $('#navGetGlueOnNow span').addClass('onnow_social_selected');
     $('#sticker').html('<script>var s=document.createElement("script"); s.src="//widgets.getglue.com/checkin.js"; var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(s,n);</script><a class="glue-checkin-widget" href="' +getGlueUrl+ '" data-type="conversation" data-width="530" data-headerBgColor="#dddddd" data-bgColor="#ffffff" data-rolloverBgColor="#f9f9f9" data-borderColor="#d3d3d3" data-replyBgColor="#f1f2f4" data-linkColor="#2269b9" data-textColor="#000000" data-subtextColor="#ababab">'+getGlueShowName+'</a>');
     $('#echo-stream').hide();
     $('#pinterest').hide();
@@ -89,12 +69,8 @@
 
   $('#navInstagramOnNow').live("click",function(){
     var instagramShowId = $(this).attr("name");
-    $('#navInstagramOnNow span').removeClass('navInstagram').addClass('navInstagram-selected');
-    $('#navChatterOnNow span').removeClass('navChatter-selected').addClass('navChatter');
-    $('#navTwitterOnNow span').removeClass('navTwitter-selected').addClass('navTwitter');
-    $('#navFbOnNow span').removeClass('navFb-selected').addClass('navFb');
-    $('#navPinterestOnNow span').removeClass('navPinterest-selected').addClass('navPinterest');
-    $('#navGetGlueOnNow span').removeClass('navGetGlue-selected').addClass('navGetGlue');
+    $('.onnow_social_nav').removeClass('onnow_social_selected');
+    $('#navInstagramOnNow span').addClass('onnow_social_selected');   
     $('#instagram').html('<script src = "http://cdn.echoenabled.com/sdk/v3/loader.js"></script><div class="echo-canvas echo-canvas-'+instagramShowId+'" data-canvas-appkey="echo.echo.streamserver.usanetwork.prod" data-canvas-id="usanetwork/'+instagramShowId+'"></div><script type="text/javascript"> Echo.Loader.init(); </script>');
     $('#echo-stream').hide();
     $('#pinterest').hide();

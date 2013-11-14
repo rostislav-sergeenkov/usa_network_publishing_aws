cbf1f8d999eb8e0f63319382faaa981594f5ef8e
Siarhei Vaitehovich <swarog2002@mail.ru>
2013-11-14 16:06:23 +0300

    Added css and js fixes for IE8. #237

--- themes/aurora_usa/javascripts/ie8.js
+++ themes/aurora_usa/javascripts/ie8.js
@@ -15,7 +15,26 @@
         $('li:nth-of-type(4n+4)', panel).addClass('nth4n4');
         $('li:nth-of-type(4n+6)', panel).addClass('nth4n6');
         $('li:nth-of-type(4n+7)', panel).addClass('nth4n7');
+      };
+
+      if ($('.ie8 .node-type-catchall-page .tiles .upcoming-movie', context).length > 0) {
+        var movie = $('.ie8 .node-type-catchall-page .tiles').eq(0);
+        $('.upcoming-movie:nth-of-type(even)', movie).addClass('even');
+        $('.upcoming-movie:nth-of-type(odd)', movie).addClass('odd');
+        $('.upcoming-movie:nth-of-type(2)', movie).addClass('nth2');
+        $('.upcoming-movie:nth-of-type(3)', movie).addClass('nth3');
+        $('.upcoming-movie:nth-of-type(4)', movie).addClass('nth4');
+        $('.upcoming-movie:nth-of-type(2n+3)', movie).addClass('nth2n3');
+        $('.upcoming-movie:nth-of-type(2n+4)', movie).addClass('nth2n4');
+        $('.upcoming-movie:nth-of-type(3n+4)', movie).addClass('nth3n4');
+        $('.upcoming-movie:nth-of-type(3n+5)', movie).addClass('nth3n5');
+        $('.upcoming-movie:nth-of-type(3n+6)', movie).addClass('nth3n6');
+        $('.upcoming-movie:nth-of-type(4n+5)', movie).addClass('nth4n5');
+        $('.upcoming-movie:nth-of-type(4n+6)', movie).addClass('nth4n6');
+        $('.upcoming-movie:nth-of-type(4n+7)', movie).addClass('nth4n7');
+        $('.upcoming-movie:nth-of-type(4n+8)', movie).addClass('nth4n8');
       }
+
     },
 
   };

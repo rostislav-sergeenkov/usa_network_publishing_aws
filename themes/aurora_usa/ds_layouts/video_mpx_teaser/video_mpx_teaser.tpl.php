7d13d5698bafcd2abb53bccdc4937aeed911d587
Ina Shulha <Ina_Shulha@epam.com>
2013-12-18 17:29:19 +0300

    #300 - lock icon on thumbnails

--- themes/aurora_usa/ds_layouts/video_mpx_teaser/video_mpx_teaser.tpl.php
+++ themes/aurora_usa/ds_layouts/video_mpx_teaser/video_mpx_teaser.tpl.php
@@ -4,9 +4,12 @@ $url = drupal_lookup_path('alias',"file/" . $variables['file']->fid);
 
 $image = media_theplatform_mpx_file_formatter_image_view($file, array('settings'=> array('image_style'=>'300x169_video')), '');
 
+
+$field_mpx_entitlement = field_get_items('file', $file, 'field_mpx_entitlement');
+$lock_video = ($field_mpx_entitlement[0]['safe_value'] == 'auth') ? 'tve-video-auth' : '';
 ?>
 
-<div class="<?php print $classes;?>">
+<div class="<?php print $classes . ' ' . $lock_video; ?>">
   <a class="item-link" href="/<?php print $url; ?>">
    <?php if ($media): ?>
     <div class="asset-img">
@@ -19,7 +22,7 @@ $image = media_theplatform_mpx_file_formatter_image_view($file, array('settings'
         <div class="title"><?php print $title; ?></div>
        <?php endif; ?>
        <?php if ($caption && $caption != "&nbsp;"): ?>
-        <div class="caption"><?php print ($caption); ?></div>
+        <div class="caption"><?php print $caption; ?></div>
        <?php endif; ?>
       </div>
     </div>

21765b9826676eda6cbe7a7bcf175ddbb47d5520
Donna Vaughan <donna.vaughan@nbcuni.com>
2014-02-13 17:29:37 -0600

    376_global_cwf_fix: more updates to fix IE9 jquery POST problem

--- modules/custom/usanetwork_social/js/global-chat-with-fans.js
+++ modules/custom/usanetwork_social/js/global-chat-with-fans.js
@@ -91,34 +91,39 @@
 					// loop through each search result and get necessary data
 					for(var searchKey in data) {
 						var result = (typeof data[searchKey]["result"] == "string") ? data[searchKey]["result"] : null;
-						var result = data[searchKey]["result"];
+//						var result = data[searchKey]["result"];
 						var entries = (typeof data[searchKey]["entries"] == "object" && typeof data[searchKey]["entries"][0] == "object") ? data[searchKey]["entries"][0] : null;
 
-						// if there was an error, show the error in the console
-						if (result == "error") {
-							usa_debug("ERROR: "+ data[searchKey]["errorMessage"]);
-						}
-						// if successful, process data
-						else if (entries != null && typeof entries["object"]["content"] == "string" && entries["object"]["content"] != "")
-						{
-							var vars = new Array();
-							vars["showToken"] = searchKey.replace("search_", "");
-							vars["avatar"] = (typeof entries["actor"]["avatar"] != 'undefined' && entries["actor"]["avatar"] != '') ? entries["actor"]["avatar"] : "http://cdn.echoenabled.com/images/avatar-default.png";
-							vars["actor"] = entries["actor"]["title"];
-							vars["comment"] = entries["object"]["content"];
-							vars["timeStr"] = ago(entries["postedTime"]);
+            // some items contain empty entries, so skip those
+						if (entries != null) {
+              // if there was an error, show the error in the console
+              if (result == "error") {
+                usa_debug("ERROR: "+ data[searchKey]["errorMessage"]);
+              }
+              // if successful, process data
+              else if (entries != null && typeof entries["object"]["content"] == "string" && entries["object"]["content"] != "")
+              {
+                var vars = new Array();
+                vars["showToken"] = searchKey.replace("search_", "");
+                vars["avatar"] = (typeof entries["actor"]["avatar"] != 'undefined' && entries["actor"]["avatar"] != '') ? entries["actor"]["avatar"] : "http://cdn.echoenabled.com/images/avatar-default.png";
+                vars["actor"] = entries["actor"]["title"];
+                vars["comment"] = entries["object"]["content"];
+                vars["timeStr"] = ago(entries["postedTime"]);
 
-							var showArray = {};
-							for (var index in vars) {
-								showArray[index] = vars[index];
-							}
-							showData[vars["showToken"]] = showArray;
-						}
-						// we shouldn't get here
-						// if we do, find out why and fix it!
-						else
-						{
-							usa_debug(console.log("BUSTED"));
+                var showArray = {};
+                for (var index in vars) {
+                  showArray[index] = vars[index];
+                }
+                showData[vars["showToken"]] = showArray;
+              }
+              // we shouldn't get here
+              // if we do, find out why and fix it!
+              else
+              {
+                usa_debug("BUSTED: ");
+                usa_debug(result);
+                usa_debug(entries);
+              }
 						}
 					}
 					return showData;
@@ -185,25 +190,34 @@
 			 */
 			var getGlobalChatWithFansData = function()
 			{
-				jQuery.post(echoQuery, echoQueryParams, function(data) { usa_debug('POST SUCCESS'); })
-					.done(function(data){
-						if (typeof data == 'string') {
-              var newData = data.replace(/(\r\n|\n|\r|\t)/gm, ""); // remove all line breaks and tabs
-              newData = newData.replace(/^\({/, "{"); // replace "({" at the beginning of the string with "{"
-              newData = newData.replace(/}\);$/, "}"); // replace a string ending with "});" with "}"
-						  data = jQuery.parseJSON(newData);
-						}
-						showData = processGlobalChatWithFansData(data);
-						var showDataKeys = getKeys(showData);
-						if (showDataKeys.length > 0) {
-							outputGlobalChatWithFans(showData);
-						} else {
-							jQuery('#showList').html("No shows found. Please come back again soon.");
-						}
-					})
-					.fail(function() {
-					  alert('error');
-					});
+        jQuery.support.cors = true; // this is needed to fix an IE9 jQuery POST, cross-domain (CORS) problem
+				jQuery.ajax({
+          url: echoQuery,
+          data: echoQueryParams,
+          success: function(data) { usa_debug('POST SUCCESS'); },
+          dataType: 'text',
+          crossDomain: true,
+          type: 'POST'
+				})
+				.done(function(data){
+          if (typeof data == 'string') {
+            var newData = data.replace(/(\r\n|\n|\r|\t)/gm, ""); // remove all line breaks and tabs
+            newData = newData.replace(/^\({/, "{"); // replace "({" at the beginning of the string with "{"
+            newData = newData.replace(/}\);$/, "}"); // replace a string ending with "});" with "}"
+            data = jQuery.parseJSON(newData);
+          }
+          showData = processGlobalChatWithFansData(data);
+          var showDataKeys = getKeys(showData);
+          if (showDataKeys.length > 0) {
+            outputGlobalChatWithFans(showData);
+          } else {
+            jQuery('#showList').html("No shows found. Please come back again soon.");
+          }
+        })
+        .fail(function(jqXHR, textStatus, errorThrown) {
+          usa_debug('error');
+          usa_debug(errorThrown);
+        });
 
 /* COMMENTED ON FEB 12, 2014 BY DV BECAUSE THE QUERY PARAMS STRING WAS
 // NOW TOO LONG TO WORK AS A GET IN CHROME. NEW POST AJAX CODE ABOVE THIS.

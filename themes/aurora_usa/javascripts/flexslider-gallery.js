c60211d422076d855411ab5ea3bab154c4042a37
Ina Shulha <ina_shulha@epam.com>
2014-03-17 11:20:58 +0300

    #416 - new photo gallery template

--- themes/aurora_usa/javascripts/flexslider-gallery.js
+++ themes/aurora_usa/javascripts/flexslider-gallery.js
@@ -4,7 +4,7 @@
     attach: function (context, settings) {
       $('.media-gallery').once('mediagallery', function() {
         $slideshow_selector = $('.field-name-field-media-items');
-        $counter_selector = $('.slide-meta h1').after('<span class="counter"></span>').siblings('.counter');
+        $counter_selector = $('.media-gallery .navigation-buttons').append('<div class="counter"></div>').find('.counter');
         $count = $slideshow_selector.find('li').length;
         $counter = 1;
         $current = $counter;
@@ -18,22 +18,29 @@
             touch: true,
             smoothHeight: false,
             slideshow: false,
+            controlsContainer: $(".slider-navigation"),
             start: function(slider){
               update_counter(slider);
+              update_caption();
             },
             after: function(slider){
               update_counter(slider);
+              update_caption();
               update_ads_and_omniture();
             }
           });
         function update_counter(slider) {
           $current = slider.currentSlide + 1;
-          $counter_output = $current + "/" + $count;
+          $counter_output = $current + " of " + $count;
           $counter_selector.html($counter_output);
         }
         function update_ads_and_omniture() {
           clearTimeout(usa_adRefreshTO);
           usa_refreshBannerAd();
+        }
+        function update_caption(){
+          $currentCaption = $('.flexslider .flex-active-slide .field-name-field-caption').html();
+          $('.media-gallery .gallery-image-caption').html($currentCaption);
         }
       });
 

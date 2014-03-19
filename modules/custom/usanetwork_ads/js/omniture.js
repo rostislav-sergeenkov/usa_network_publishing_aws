4c366b18341d0842b841eb2b3d20657d8fd01b8d
Aliaksei Bykouski <Aliaksei_Bykouski@epam.com>
2014-03-19 16:09:35 +0300

    #410 #448 Omniture calls fixes

--- modules/custom/usanetwork_ads/js/omniture.js
+++ modules/custom/usanetwork_ads/js/omniture.js
@@ -44,96 +44,107 @@
       s.manageVars('clearVars', s.linkTrackVars, 1);
     },
     attach: function (context, settings) {
+      if (typeof s != 'object') {
+        return;
+      }
       //Click on "On Now" button
       $('#on-now').once('omniture-tracking', function() {
         $(this).on('click', function(){
-          s.linkTrackVars='events,eVar63,prop63';
-          s.linkTrackEvents='event63';
-          s.events='event63';
-          s.eVar63=s.prop63='On Now'
-          s.tl(this,'o','On Now Click');
-          s.manageVars("clearVars", s.linkTrackVars, 1);
+          if (typeof s == 'object' && typeof s.tl == 'function') {
+            s.linkTrackVars='events,eVar63,prop63';
+            s.linkTrackEvents='event63';
+            s.events='event63';
+            s.eVar63=s.prop63='On Now'
+            s.tl(this,'o','On Now Click');
+            s.manageVars("clearVars", s.linkTrackVars, 1);
+          }
         });
       });
 
       //Click on "Up Next"
       $('#jPanelMenu-menu .up-next .tab-wrapper, #jPanelMenu-menu .on-now .tab-wrapper').once('omniture-tracking', function() {
         $(this).on('click', function(){
-          $self = $(this);
-          var prop = '';
-          var descr = '';
-          if ($self.parents('.on-now').length > 0) {
-            prop = 'On Now';
-            descr = 'On Now Click';
+          if (typeof s == 'object' && typeof s.tl == 'function') {
+            var $self = $(this);
+            var prop = '';
+            var descr = '';
+            if ($self.parents('.on-now').length > 0) {
+              prop = 'On Now';
+              descr = 'On Now Click';
+            }
+            else {
+              prop = 'On Now - Up Next';
+              descr = 'Up Next Click';
+            }
+            s.linkTrackVars='events,eVar64,prop64';
+            s.linkTrackEvents='event64';
+            s.events='event64';
+            s.eVar64=s.prop64=prop;
+            s.tl(this,'o',descr);
+            s.manageVars("clearVars", s.linkTrackVars, 1);
           }
-          else {
-            prop = 'On Now - Up Next';
-            descr = 'Up Next Click';
-          }
-          s.linkTrackVars='events,eVar64,prop64';
-          s.linkTrackEvents='event64';
-          s.events='event64';
-          s.eVar64=s.prop64=prop;
-          s.tl(this,'o',descr);
-          s.manageVars("clearVars", s.linkTrackVars, 1);
         });
       });
 
       // Click on menu item
       $('#block-usanetwork-blocks-usa-meganav .mega-menu-items a, #logo a').once('omniture-tracking', function() {
         $(this).on('click', function(e){
-          var $self = $(this);
+          if (typeof s == 'object' && typeof s.tl == 'function') {
+            var $self = $(this);
 
-          // check if link is related to drawer
-          if ($self.attr('data-drawer-id')) {
-            var drawerId = $self.attr('data-drawer-id');
-            var $drawer = $('[data-drawer="' + drawerId + '"]');
-            if ($drawer.length > 0) {
-              $drawer.toggleClass('omniture-drawer-opened');
-              if (!$drawer.hasClass('omniture-drawer-opened')) {
-                // do not track when closing
-                return;
+            // check if link is related to drawer
+            if ($self.attr('data-drawer-id')) {
+              var drawerId = $self.attr('data-drawer-id');
+              var $drawer = $('[data-drawer="' + drawerId + '"]');
+              if ($drawer.length > 0) {
+                $drawer.toggleClass('omniture-drawer-opened');
+                if (!$drawer.hasClass('omniture-drawer-opened')) {
+                  // do not track when closing
+                  return;
+                }
               }
             }
-          }
 
-          s.linkTrackVars='events,eVar63,prop63';
-          s.linkTrackEvents='event63';
-          s.events='event63';
-          var menu_name = $self.text();
-          s.eVar63=s.prop63=menu_name;
-          if (!$self.hasClass('use-ajax') && $self.attr('href') != '#') {
-            e.preventDefault();
-            s.bcf = function() {
-              setTimeout(function() {
-                window.location = $self.attr('href');
-              }, 500);
-            };
+            s.linkTrackVars='events,eVar63,prop63';
+            s.linkTrackEvents='event63';
+            s.events='event63';
+            var menu_name = $self.text();
+            s.eVar63=s.prop63=menu_name;
+            if (!$self.hasClass('use-ajax') && $self.attr('href') != '#') {
+              e.preventDefault();
+              s.bcf = function() {
+                setTimeout(function() {
+                  window.location = $self.attr('href');
+                }, 500);
+              };
+            }
+            s.tl(this,'o','Global Menu Click');
+            s.manageVars("clearVars", s.linkTrackVars, 1);
           }
-          s.tl(this,'o','Global Menu Click');
-          s.manageVars("clearVars", s.linkTrackVars, 1);
         });
       });
 
       //Click on submenu item
       $('.mega-sub-nav .item-list li a, #tv-show-menu .item-list li a').once('omniture-tracking', function() {
         $(this).on('click', function(e){
-          e.preventDefault();
-          var $self = $(this);
-          s.linkTrackVars='events,eVar64,prop64';
-          s.linkTrackEvents='event64';
-          s.events='event64';
-          var sub_menu_name = $self.text();
-          s.eVar64=s.prop64=sub_menu_name;
-          if (!$self.hasClass('use-ajax') && !$self.hasClass('link-empty') && $self.attr('href') != '#') {
-            s.bcf = function() {
-              setTimeout(function() {
-                window.location = $self.attr('href');
-              }, 500);
-            };
+          if (typeof s == 'object' && typeof s.tl == 'function') {
+            e.preventDefault();
+            var $self = $(this);
+            s.linkTrackVars='events,eVar64,prop64';
+            s.linkTrackEvents='event64';
+            s.events='event64';
+            var sub_menu_name = $self.text();
+            s.eVar64=s.prop64=sub_menu_name;
+            if (!$self.hasClass('use-ajax') && !$self.hasClass('link-empty') && $self.attr('href') != '#') {
+              s.bcf = function() {
+                setTimeout(function() {
+                  window.location = $self.attr('href');
+                }, 500);
+              };
+            }
+            s.tl(this,'o','Global SubMenu Click');
+            s.manageVars('clearVars', s.linkTrackVars, 1);
           }
-          s.tl(this,'o','Global SubMenu Click');
-          s.manageVars('clearVars', s.linkTrackVars, 1);
         });
       });
 
@@ -145,48 +156,52 @@
       // A/B/C Spots
       $('.usa-home-aspot .node a, .usa-home-bspot .node a, .usa-home-cspot .node a').once('omniture-tracking', function() {
         $(this).on('click', function(e) {
-          e.preventDefault();
-          var $self = $(this);
-          var $node = $self.parents('.node');
-          var promoLocation = '';
-          if ($self.parents('.usa-home-aspot').length > 0) {
-            promoLocation = '1';
-          }
-          else if ($self.parents('.usa-home-bspot').length > 0) {
-            promoLocation = '2';
-          }
-          else if ($self.parents('.usa-home-cspot').length > 0) {
-            promoLocation = '3';
-          }
+          if (typeof s == 'object' && typeof s.tl == 'function') {
+            e.preventDefault();
+            var $self = $(this);
+            var $node = $self.parents('.node');
+            var promoLocation = '';
+            if ($self.parents('.usa-home-aspot').length > 0) {
+              promoLocation = '1';
+            }
+            else if ($self.parents('.usa-home-bspot').length > 0) {
+              promoLocation = '2';
+            }
+            else if ($self.parents('.usa-home-cspot').length > 0) {
+              promoLocation = '3';
+            }
 
-          // get carousel position
-          var $carouselItems = $self.parents('ul').children().not('.clone');
-          if ($carouselItems.length > 1) {
-            var index = $carouselItems.index($self.parents('li')) + 1;
-            promoLocation = 'C' + index + '-' + promoLocation;
-          }
+            // get carousel position
+            var $carouselItems = $self.parents('ul').children().not('.clone');
+            if ($carouselItems.length > 1) {
+              var index = $carouselItems.index($self.parents('li')) + 1;
+              promoLocation = 'C' + index + '-' + promoLocation;
+            }
 
-          // track it
-          Drupal.behaviors.omniture_tracking.trackPromo($node, promoLocation, {
-            href: $self.attr('href'),
-            target: $self.attr('target')
-          });
+            // track it
+            Drupal.behaviors.omniture_tracking.trackPromo($node, promoLocation, {
+              href: $self.attr('href'),
+              target: $self.attr('target')
+            });
+          }
         });
       });
       // Featured
       $('.field-name-field-hp-promos .node a').once('omniture-tracking', function() {
         $(this).on('click', function(e) {
-          e.preventDefault();
-          var $self = $(this);
-          var $node = $self.parents('.node');
-          var $items = $self.parents('.field-name-field-hp-promos').find('.node');
-          var promoLocation = $items.index($self.parents('.node')) + 4;
+          if (typeof s == 'object' && typeof s.tl == 'function') {
+            e.preventDefault();
+            var $self = $(this);
+            var $node = $self.parents('.node');
+            var $items = $self.parents('.field-name-field-hp-promos').find('.node');
+            var promoLocation = $items.index($self.parents('.node')) + 4;
 
-          // track it
-          Drupal.behaviors.omniture_tracking.trackPromo($node, promoLocation, {
-            href: $self.attr('href'),
-            target: $self.attr('target')
-          });
+            // track it
+            Drupal.behaviors.omniture_tracking.trackPromo($node, promoLocation, {
+              href: $self.attr('href'),
+              target: $self.attr('target')
+            });
+          }
         });
       });
 
@@ -194,39 +209,43 @@
       // A Spot
       $('.show-aspot .node a').once('omniture-tracking', function() {
         $(this).on('click', function(e) {
-          e.preventDefault();
-          var $self = $(this);
-          var $node = $self.parents('.node');
-          var promoLocation = '1';
+          if (typeof s == 'object' && typeof s.tl == 'function') {
+            e.preventDefault();
+            var $self = $(this);
+            var $node = $self.parents('.node');
+            var promoLocation = '1';
 
-          // get carousel position
-          var $carouselItems = $self.parents('ul').children().not('.clone');
-          if ($carouselItems.length > 1) {
-            var index = $carouselItems.index($self.parents('li')) + 1;
-            promoLocation = 'C' + index + '-' + promoLocation;
+            // get carousel position
+            var $carouselItems = $self.parents('ul').children().not('.clone');
+            if ($carouselItems.length > 1) {
+              var index = $carouselItems.index($self.parents('li')) + 1;
+              promoLocation = 'C' + index + '-' + promoLocation;
+            }
+
+            // track it
+            Drupal.behaviors.omniture_tracking.trackPromo($node, promoLocation, {
+              href: $self.attr('href'),
+              target: $self.attr('target')
+            });
           }
-
-          // track it
-          Drupal.behaviors.omniture_tracking.trackPromo($node, promoLocation, {
-            href: $self.attr('href'),
-            target: $self.attr('target')
-          });
         });
       });
       // Featured
       $('.field-name-field-usa-tv-promo .node a').once('omniture-tracking', function() {
         $(this).on('click', function(e) {
-          e.preventDefault();
-          var $self = $(this);
-          var $node = $self.parents('.node');
-          var $items = $self.parents('.field-name-field-usa-tv-promo').find('.node');
-          var promoLocation = $items.index($self.parents('.node')) + 2;
+          if (typeof s == 'object' && typeof s.tl == 'function') {
+            e.preventDefault();
+            var $self = $(this);
+            var $node = $self.parents('.node');
+            var $items = $self.parents('.field-name-field-usa-tv-promo').find('.node');
+            var promoLocation = $items.index($self.parents('.node')) + 2;
 
-          // track it
-          Drupal.behaviors.omniture_tracking.trackPromo($node, promoLocation, {
-            href: $self.attr('href'),
-            target: $self.attr('target')
-          });
+            // track it
+            Drupal.behaviors.omniture_tracking.trackPromo($node, promoLocation, {
+              href: $self.attr('href'),
+              target: $self.attr('target')
+            });
+          }
         });
       });
     }
--- modules/custom/usanetwork_tv_schedule/js/usanetwork_social_on_now.js
+++ modules/custom/usanetwork_tv_schedule/js/usanetwork_social_on_now.js
@@ -79,7 +79,7 @@
     var tab_id = $(this).parent().parent().parent().attr('id');
     $('.onnow_social_nav').removeClass('onnow_social_selected');
     $('#navGetGlueOnNow span').addClass('onnow_social_selected');
-    $('#'+tab_id+' #sticker').html('<script>var s=document.createElement("script"); s.src="//widgets.getglue.com/checkin.js"; var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(s,n);</script><a class="glue-checkin-widget" href="' +getGlueUrl+ '" data-type="conversation" data-width="530" data-height="889" data-headerBgColor="#dddddd" data-bgColor="#ffffff" data-rolloverBgColor="#f9f9f9" data-borderColor="#d3d3d3" data-replyBgColor="#f1f2f4" data-linkColor="#2269b9" data-textColor="#000000" data-subtextColor="#ababab">'+getGlueShowName+'</a>');
+    $('#'+tab_id+' #sticker').html('<script>(function() { var s=document.createElement("script"); s.src="//widgets.getglue.com/checkin.js"; var n=document.getElementsByTagName("script")[0];n.parentNode.insertBefore(s,n); })();</script><a class="glue-checkin-widget" href="' +getGlueUrl+ '" data-type="conversation" data-width="530" data-height="889" data-headerBgColor="#dddddd" data-bgColor="#ffffff" data-rolloverBgColor="#f9f9f9" data-borderColor="#d3d3d3" data-replyBgColor="#f1f2f4" data-linkColor="#2269b9" data-textColor="#000000" data-subtextColor="#ababab">'+getGlueShowName+'</a>');
     $('#'+tab_id+' #echo-stream').hide();
     $('#'+tab_id+' #pinterest').hide();
     $('#'+tab_id+' #pinterest').empty();

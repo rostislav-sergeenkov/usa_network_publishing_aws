1a7ea4a35be55d6d2ccfabe48542c29805f2e5e6
Aliaksei Bykouski <Aliaksei_Bykouski@epam.com>
2014-03-28 11:44:02 +0300

    #466 Gallery slider and Gigya share integration

--- themes/aurora_usa/javascripts/flexslider-gallery.js
+++ themes/aurora_usa/javascripts/flexslider-gallery.js
@@ -20,11 +20,13 @@
             slideshow: false,
             start: function(slider){
               update_counter(slider);
-              update_caption();
+              update_caption(slider);
+              update_gigya_sharebar(slider);
             },
             after: function(slider){
               update_counter(slider);
-              update_caption();
+              update_caption(slider);
+              update_gigya_sharebar(slider);
               update_ads_and_omniture();
             }
           });
@@ -37,13 +39,32 @@
           clearTimeout(usa_adRefreshTO);
           usa_refreshBannerAd();
         }
-        function update_caption(){
-          $currentCaption = $('.flexslider .flex-active-slide .field-name-field-caption').html();
-          $('.media-gallery .gallery-image-caption').html($currentCaption);
+        function update_caption(slider){
+          var $currentCaption = slider.find('.flex-active-slide .field-name-field-caption').html();
+          slider.parents('.media-gallery').find('.gallery-image-caption').html($currentCaption);
+        }
+        function update_gigya_sharebar(slider) {
+          if (typeof Drupal.gigya != 'undefined') {
+            var $sharebar = slider.parents('.media-gallery').find('.field-name-field-gigya-share-bar > div');
+            if ($sharebar.length > 0) {
+              var $currentDescription = slider.find('.flex-active-slide .field-name-field-caption').text();
+              if ($currentDescription == '' && $('meta[property="og:description"]').length > 0) {
+                $currentDescription = $('meta[property="og:description"]').attr('content');
+              }
+              var $currentImage = slider.find('.flex-active-slide .file-image img');
+              $.each(Drupal.settings.gigyaSharebars, function (index, sharebar) {
+                if (sharebar.gigyaSharebar.containerID == $sharebar.attr('id')) {
+                  sharebar.gigyaSharebar.ua.imageBhev = 'url';
+                  sharebar.gigyaSharebar.ua.imageUrl = $currentImage.attr('src');
+                  sharebar.gigyaSharebar.ua.description = $currentDescription;
+                  Drupal.gigya.showSharebar(sharebar);
+                }
+              });
+            }
+          }
         }
       });
-
-    },
+    }
   };
 
 }(jQuery));

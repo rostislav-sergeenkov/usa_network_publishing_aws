793374977424095db4d617f517a2d4809bb24ae7
Aliaksei Bykouski <Aliaksei_Bykouski@epam.com>
2014-04-08 18:25:35 +0300

    global nav changes

--- modules/custom/usanetwork_blocks/theme/usa-meganav.tpl.php
+++ modules/custom/usanetwork_blocks/theme/usa-meganav.tpl.php
@@ -14,8 +14,6 @@ $more_title = variable_get('usanetwork_global_nav_more_title');
 
 ?>
 
-
-<div class="mega-menu-items slide-menu-toggle"><span></span></div>
 <div data-module-type="NavDrawer" class="mega-menu-items shows-mega-menu-item active">
   <?php print l('Shows', 'globalnav_shows/nojs', array('absolute' => true, 'attributes' => array('class' => array('mega-nav-link', 'shows', 'use-ajax'), 'data-drawer-id' => 'main-nav-shows'))); ?>
   <div class="mega-sub-nav-container" data-drawer="main-nav-shows">

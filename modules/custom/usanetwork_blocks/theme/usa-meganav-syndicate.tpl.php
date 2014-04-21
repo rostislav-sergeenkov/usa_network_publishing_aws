057c3dc41bc67790c4b58820bf7224c99035096d
Aliaksei Bykouski <Aliaksei_Bykouski@epam.com>
2014-04-21 15:35:23 +0300

    Revert Global Nav changes

--- modules/custom/usanetwork_blocks/theme/usa-meganav-syndicate.tpl.php
+++ modules/custom/usanetwork_blocks/theme/usa-meganav-syndicate.tpl.php
@@ -11,8 +11,8 @@
 
 $show_title = variable_get('usanetwork_global_nav_show_title');
 $more_title = variable_get('usanetwork_global_nav_more_title');
-
 ?>
+
 
 
 <div data-module-type="NavDrawer" class="mega-menu-items shows-mega-menu-item active">
@@ -49,5 +49,4 @@ $more_title = variable_get('usanetwork_global_nav_more_title');
     </div>
   </div>
 </div>
-<div class="mega-menu-items slide-menu-close"><span>x</span></div>
 
--- modules/custom/usanetwork_blocks/theme/usa-meganav.tpl.php
+++ modules/custom/usanetwork_blocks/theme/usa-meganav.tpl.php
@@ -14,6 +14,8 @@ $more_title = variable_get('usanetwork_global_nav_more_title');
 
 ?>
 
+
+
 <div data-module-type="NavDrawer" class="mega-menu-items shows-mega-menu-item active">
   <?php print l('Shows', 'globalnav_shows/nojs', array('absolute' => true, 'attributes' => array('class' => array('mega-nav-link', 'shows', 'use-ajax'), 'data-drawer-id' => 'main-nav-shows'))); ?>
   <div class="mega-sub-nav-container" data-drawer="main-nav-shows">
@@ -48,4 +50,3 @@ $more_title = variable_get('usanetwork_global_nav_more_title');
     </div>
   </div>
 </div>
-<div class="mega-menu-items slide-menu-close"><span>x</span></div>

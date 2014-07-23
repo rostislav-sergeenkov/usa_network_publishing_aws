98f210070e8a9f97a3c1156105322ea9bd99e762
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2014-07-23 23:42:13 +0300

    #800 add animated intro field

--- themes/aurora_usa/javascripts/show-flexslider.js
+++ themes/aurora_usa/javascripts/show-flexslider.js
@@ -3,11 +3,11 @@
   Drupal.behaviors.showAspot = {
     attach: function (context, settings) {
 
-      $('body').once('showFlexslider', function() {
+      $('body').once('showFlexslider', function () {
         $slideshow_selector = $('#show-aspot ul');
 
-        $slideshow = (settings.showAspot.slideshow !== null)? settings.showAspot.slideshow : false;
-        $slideshowSpeed = (settings.showAspot.slideshowSpeed !== null)? settings.showAspot.slideshowSpeed : 7000;
+        $slideshow = (settings.showAspot.slideshow !== null) ? settings.showAspot.slideshow : false;
+        $slideshowSpeed = (settings.showAspot.slideshowSpeed !== null) ? settings.showAspot.slideshowSpeed : 7000;
 
         $slideshow_selector
           .addClass('slides')
@@ -22,66 +22,108 @@
             directionNav: (!Modernizr.touch)
           });
         $showSlideshow = $('#show-main-slider');
-       });
+      });
 
 
-        // A-SPOT VIDEOS
-        if (typeof aspotVideoEnabled != 'undefined' && aspotVideoEnabled && !usa_deviceInfo.smartphone && !usa_deviceInfo.mobileDevice) {
+      // A-SPOT VIDEOS
+      var isIE8 = 0;
+      var isSafari5 = 0;
+      var aspotVideoAgent = navigator.userAgent.toLowerCase();
+      if (aspotVideoAgent.indexOf('msie 8') != -1) {
+        isIE8 = 1;
+      }
+      if (aspotVideoAgent.indexOf('safari') != -1 && aspotVideoAgent.indexOf('version/5') != -1) {
+        isSafari5 = 1;
+      }
+      // Set default a-spot video values
+      var aspotVideoEnabled = 0;
+      var aspotVideoMp4VideoUrl = '';
+      var aspotVideoWebmVideoUrl = '';
 
-          var showAspotVideoPauseFlexslider = function() {
-            usa_debug('showAspotVideoPauseFlexslider()');
-            if (typeof $showSlideshow.flexslider === 'function') {
-              // if we don't put a timeout here, the flexslider control
-              // nav ("the dots") never appear
-              setTimeout(function(){
-                usa_debug("pausing flexslider");
-                $showSlideshow.flexslider("pause");
-                showAspotVideoResumeFlexsliderPlay();
-              }, 1000);
-            }
-            else {
-              setTimeout(showAspotVideoPauseFlexslider, 500);
-            }
+      // Exclude some browsers
+      if (!isIE8 && !isSafari5) {
+        function AspotVideo(mp4, webm) {
+          if (!(this instanceof arguments.callee)) {
+            return new AspotVideo(mp4, webm);
           }
+          this.mp4 = mp4;
+          this.webm = webm;
+        }
 
-          var showAspotVideoResumeFlexsliderPlay = function() {
-            usa_debug("showAspotVideoResumeFlexsliderPlay()");
-            $('#aspot-video').bind('ended', function() {
-              usa_debug("video ended");
-              $('#main-slider').css('opacity', 1);
-              $('#aspot-video-container').animate({'opacity': 0}, 400, function(){
-                $showSlideshow.flexslider('play');
-                $(this).remove();
-              })
-            });
+        var aspotVideos = {};
+        if (Drupal.settings.aspotSettings) {
+          var show = Drupal.settings.aspotSettings.show;
+          var mp4_url = Drupal.settings.aspotSettings.mp4_url;
+          var webm_url = Drupal.settings.aspotSettings.webm_url;
+          aspotVideos[show] = {};
+          aspotVideos[show] = AspotVideo(mp4_url, webm_url);
+        }
+        var aspotVideoBeingShown = '';
+        if (aspotVideos.hasOwnProperty(show)) {
+          var avTemp = aspotVideos[show];
+          if (avTemp.mp4 != '' && avTemp.webm != '') {
+            aspotVideoEnabled = 1;
+            aspotVideoMp4VideoUrl = avTemp.mp4;
+            aspotVideoWebmVideoUrl = avTemp.webm;
+            aspotVideoBeingShown = show;
           }
+        }
+        usa_debug('aspotVideoEnabled: ' + aspotVideoEnabled + '\naspotVideoBeingShown: ' + aspotVideoBeingShown + '\naspotVideoMp4VideoUrl: ' + aspotVideoMp4VideoUrl + '\naspotVideoWebmVideoUrl: ' + aspotVideoWebmVideoUrl);
+      }
+      if (typeof aspotVideoEnabled != 'undefined' && aspotVideoEnabled && !usa_deviceInfo.smartphone && !usa_deviceInfo.mobileDevice) {
 
-          var showAspotVideoDone = 0;
-          var showAspotVideoShow = function() {
-            usa_debug('showAspotVideoShow()');
-            if (!showAspotVideoDone) {
-              var aspotVideoTag = '<video id="aspot-video" width="100%" autoplay><source src="' + aspotVideoMp4VideoUrl + '" type="video/mp4"><source src="' + aspotVideoWebmVideoUrl + '" type="video/webm">Your browser does not support the video tag.</video>';
-              if (aspotVideoAgent.indexOf('msie') != -1) {
-                var aspotVideoWidth = $('#main-slider').width();
-                var aspotVideoHeight = $('#main-slider').height();
-                aspotVideoTag = '<video id="aspot-video" width="' + aspotVideoWidth + '" height="' + aspotVideoHeight + '" autoplay><source src="' + aspotVideoMp4VideoUrl + '" type="video/mp4"><source src="' + aspotVideoWebmVideoUrl + '" type="video/webm">Your browser does not support the video tag.</video>';
-              }
-              $('#aspot-video-container').html(aspotVideoTag).show();
-              showAspotVideoDone = 1
-            }
-            else {
-              setTimeout(showAspotVideoShow, 500);
-            }
+        var showAspotVideoPauseFlexslider = function () {
+          usa_debug('showAspotVideoPauseFlexslider()');
+          if (typeof $showSlideshow.flexslider === 'function') {
+            // if we don't put a timeout here, the flexslider control
+            // nav ("the dots") never appear
+            setTimeout(function () {
+              usa_debug("pausing flexslider");
+              $showSlideshow.flexslider("pause");
+              showAspotVideoResumeFlexsliderPlay();
+            }, 1000);
           }
+          else {
+            setTimeout(showAspotVideoPauseFlexslider, 500);
+          }
+        }
 
-          $(document).ready(function(){
-            $('#main-slider').css('opacity', 0);
-            showAspotVideoShow();
-            showAspotVideoPauseFlexslider();
+        var showAspotVideoResumeFlexsliderPlay = function () {
+          usa_debug("showAspotVideoResumeFlexsliderPlay()");
+          $('#aspot-video').bind('ended', function () {
+            usa_debug("video ended");
+            $('#main-slider').css('opacity', 1);
+            $('#aspot-video-container').animate({'opacity': 0}, 400, function () {
+              $showSlideshow.flexslider('play');
+              $(this).remove();
+            })
           });
-        } // A-SPOT VIDEOS
+        }
 
+        var showAspotVideoDone = 0;
+        var showAspotVideoShow = function () {
+          usa_debug('showAspotVideoShow()');
+          if (!showAspotVideoDone) {
+            var aspotVideoTag = '<video id="aspot-video" width="100%" autoplay><source src="' + aspotVideoMp4VideoUrl + '" type="video/mp4"><source src="' + aspotVideoWebmVideoUrl + '" type="video/webm">Your browser does not support the video tag.</video>';
+            if (aspotVideoAgent.indexOf('msie') != -1) {
+              var aspotVideoWidth = $('#main-slider').width();
+              var aspotVideoHeight = $('#main-slider').height();
+              aspotVideoTag = '<video id="aspot-video" width="' + aspotVideoWidth + '" height="' + aspotVideoHeight + '" autoplay><source src="' + aspotVideoMp4VideoUrl + '" type="video/mp4"><source src="' + aspotVideoWebmVideoUrl + '" type="video/webm">Your browser does not support the video tag.</video>';
+            }
+            $('#aspot-video-container').html(aspotVideoTag).show();
+            showAspotVideoDone = 1
+          }
+          else {
+            setTimeout(showAspotVideoShow, 500);
+          }
+        }
 
-    },
+        $(document).ready(function () {
+          $('#main-slider').css('opacity', 0);
+          showAspotVideoShow();
+          showAspotVideoPauseFlexslider();
+        });
+      } // A-SPOT VIDEOS
+    }
   };
 }(jQuery));

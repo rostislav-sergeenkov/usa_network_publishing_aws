2972fc2ef6c4d48ddb58039126d24fcd766ef330
maksim-tarhunakau <--global>
2014-11-06 15:26:18 +0300

    Applying layout changes + global block

--- docroot/sites/usanetwork/themes/aurora_usa/layouts/usanetwork-show-menu/usanetwork-show-menu.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/layouts/usanetwork-show-menu/usanetwork-show-menu.tpl.php
@@ -1,31 +1,23 @@
-<header>
-  <div class="panel-display usanetwork-show-menu-panel clearfix" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
-    <div class="ad ad-728x90">
-      <?php print $content['reg_ad']; ?>
-    </div>
-
-    <div class="header-nav-bar">
-      <?php print $content['reg_menu']; ?>
-    </div>
-
-    <div class="header-small-menu menu">
-      <?php print $content['reg_mobile_menu']; ?>
-    </div>
-
-    <div class="tab-item shows-tab">
-      <?php print $content['reg_tab_shows']; ?>
-    </div>
-
-    <div class="tab-item full-episode-tab">
-      <?php print $content['reg_tab_full_episodes']; ?>
-    </div>
-
-    <div class="tab-item schedule-tab">
-      <?php print $content['reg_tab_schedule']; ?>
-    </div>
-
-    <div class="tab-item user-profile-tab">
-      <?php print $content['reg_tab_user_profile']; ?>
-    </div>
+<div class="panel-display usanetwork-show-menu-panel clearfix" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
+  <div class="ad ad-728x90">
+    <?php print $content['reg_ad']; ?>
   </div>
-</header>
+  <div class="header-nav-bar">
+    <?php print $content['reg_menu']; ?>
+  </div>
+  <div class="header-small-menu menu">
+    <?php print $content['reg_mobile_menu']; ?>
+  </div>
+  <div class="tab-item shows-tab">
+    <?php print $content['reg_tab_shows']; ?>
+  </div>
+  <div class="tab-item full-episode-tab">
+    <?php print $content['reg_tab_full_episodes']; ?>
+  </div>
+  <div class="tab-item schedule-tab">
+    <?php print $content['reg_tab_schedule']; ?>
+  </div>
+  <div class="tab-item user-profile-tab">
+    <?php print $content['reg_tab_user_profile']; ?>
+  </div>
+</div>

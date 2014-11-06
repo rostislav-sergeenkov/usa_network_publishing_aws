93c1c37f6090361942a54d06ea2ab60dc5059598
maksim-tarhunakau <--global>
2014-11-06 16:53:48 +0300

    Template update

--- docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-smnav-img-show.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-smnav-img-show.tpl.php
@@ -19,3 +19,24 @@
     <div class="cta-text"><?php print $cta_text; ?></div>
   <?php endif; ?>
 </div>
+
+<div class="node node-usanetwork-promo">
+  <?php if (!empty($image)): ?>
+    <a href="#" class="play-icon">
+      <div class="asset-img show-color-border show-rush">
+        <img src="<?php print $image; ?>" alt="">
+      </div>
+    </a>
+  <?php endif; ?>
+  <div class="title-overlay meta">
+    <?php if (!empty($title)): ?>
+      <div class="title"><?php print $title; ?></div>
+    <?php endif; ?>
+    <?php if (!empty($type) && !empty($time)): ?>
+      <div class="type-and-time"><span><?php print $type; ?></span> <?php print $time; ?></div>
+    <?php endif; ?>
+    <?php if (!empty($cta_text)): ?>
+       <div class="caption"><?php print $cta_text; ?></div>
+    <?php endif; ?>
+  </div>
+</div>

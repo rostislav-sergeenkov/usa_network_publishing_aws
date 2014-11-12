1a4aa01a370c5b4e3f167e48df7eaa37f10f48b9
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2014-11-12 17:26:43 +0300

    Theme header and footer of new site design

--- docroot/sites/usanetwork/themes/aurora_usa/layouts/usanetwork-show-menu/usanetwork-show-menu.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/layouts/usanetwork-show-menu/usanetwork-show-menu.tpl.php
@@ -1,20 +1,23 @@
 <div class="panel-display usanetwork-show-menu-panel clearfix" <?php if (!empty($css_id)): print "id=\"$css_id\""; endif; ?>>
-  <div class="header-nav-bar">
+  <div class="show-title-wrapper show-border tertiary"></div>
+  <div class="nav-bar-tabs">
     <?php print $content['reg_menu']; ?>
   </div>
   <div class="header-small-menu menu">
     <?php print $content['reg_mobile_menu']; ?>
   </div>
-  <div class="tab-item shows-tab">
-    <?php print $content['reg_tab_shows']; ?>
-  </div>
-  <div class="tab-item full-episode-tab">
-    <?php print $content['reg_tab_full_episodes']; ?>
-  </div>
-  <div class="tab-item schedule-tab">
-    <?php print $content['reg_tab_schedule']; ?>
-  </div>
-  <div class="tab-item user-profile-tab">
-    <?php print $content['reg_tab_user_profile']; ?>
+  <div class="tab-content">
+    <div class="tab-item shows-tab">
+      <?php print $content['reg_tab_shows']; ?>
+    </div>
+    <div class="tab-item full-episode-tab">
+      <?php print $content['reg_tab_full_episodes']; ?>
+    </div>
+    <div class="tab-item schedule-tab">
+      <?php print $content['reg_tab_schedule']; ?>
+    </div>
+    <div class="tab-item user-profile-tab">
+      <?php print $content['reg_tab_user_profile']; ?>
+    </div>
   </div>
 </div>

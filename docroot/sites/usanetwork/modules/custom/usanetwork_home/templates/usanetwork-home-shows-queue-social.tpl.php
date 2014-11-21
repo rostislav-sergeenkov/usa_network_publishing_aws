8a8114069ea087c6bc2b6c3e18548f9a32cbcdea
maksim-tarhunakau <--global>
2014-11-21 15:35:28 +0300

    Intermediate commit

--- docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-shows-queue-social.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-shows-queue-social.tpl.php
@@ -6,7 +6,7 @@
 <div class="node-wrapper social">
   <div class="node node-usanetwork-promo show-color-border<?php if (!empty($show_class)): print ' ' . $show_class; endif; ?> social-twitter">
     <a href="javascript:void(0)">
-      <img alt="" src="http://placehold.it/600x500">
+      <img alt="" src="http://placehold.it/500x500">
     </a>
   </div>
 </div>

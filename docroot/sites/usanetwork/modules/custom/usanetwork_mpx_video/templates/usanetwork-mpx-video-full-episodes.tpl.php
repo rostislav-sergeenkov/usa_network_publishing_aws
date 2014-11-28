9fe7d9d8ee76fc576751014c8891bec02953146e
Sergii Pavlenko <Sergii_Pavlenko1@epam.com>
2014-11-28 14:13:22 +0200

    Full episodes block for video page

---
+++ docroot/sites/usanetwork/modules/custom/usanetwork_mpx_video/templates/usanetwork-mpx-video-full-episodes.tpl.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * 
+ * @see usanetwork_mpx_full_episodes()
+ */
+?>
+<div class="consumptionator-full-episodeconsumptionator-full-episodess carousel-block carousel-block-left">
+  <div class="carousel-description-item start">
+    <a href="javascript:void(0)" class="description-button"></a>
+    <div class="description-wrapper">
+      <div class="description-block">
+        <div class="caption"><?php print $caption; ?></div>
+        <div class="title"><?php print $title; ?></div>
+        <div class="additional-text"><?php print $additional_text; ?></div>
+      </div>
+    </div>
+  </div>
+  <?php print $elements; ?>
+</div>

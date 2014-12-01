d40917c6ea688847b9239e2e72183f0c807ec7c9
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2014-12-01 13:20:28 +0300

    fix js in aspot

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/variables.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/variables.js
@@ -1,4 +1,20 @@
 var window_size_tablet_portrait = 769;
 var window_size_mobile = 481;
 
+var USAN = USAN || {};
+
+var waitForFinalEvent = (function () {
+  var timers = {};
+
+  return function (callback, ms, uniqueId) {
+    if (!uniqueId) {
+      uniqueId = "Don't call this twice without a uniqueId";
+    }
+    if (timers[uniqueId]) {
+      clearTimeout (timers[uniqueId]);
+    }
+    timers[uniqueId] = setTimeout(callback, ms);
+  };
+})();
+
 

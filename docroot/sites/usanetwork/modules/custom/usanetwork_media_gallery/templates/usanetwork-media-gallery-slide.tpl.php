b27aaec794d69f80eb804d9fd9ffc6d1c6ff500c
Sergii Pavlenko <Sergii_Pavlenko1@epam.com>
2014-12-03 14:25:01 +0200

    add gigya social bar and end card slide

--- docroot/sites/usanetwork/modules/custom/usanetwork_media_gallery/templates/usanetwork-media-gallery-slide.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_media_gallery/templates/usanetwork-media-gallery-slide.tpl.php
@@ -4,7 +4,7 @@
  */
 ?>
 <div class="slide">
-  <div class="node node-gallery">
+  <div class="node node-gallery<?php if ($gigya_id): ?> end-cart<?php endif; ?>">
     <div class="asset-img">
       <?php if (!empty($image)): ?><?php print $image; ?><?php endif; ?>
     </div>
@@ -12,18 +12,16 @@
       <div class="description">
         <?php if (isset($info['description'])): ?><?php print $info['description']; ?><?php endif; ?>
       </div>
+      <?php if ($gigya_id): ?>
       <div class="social-bar">
         <div class="social-title">
           Share this image
         </div>
-        <div class="social-icons icons-block">
-          <a href="javascript:void(0)" class="facebook"></a>
-          <a href="javascript:void(0)" class="twitter"></a>
-          <a href="javascript:void(0)" class="tumblr"></a>
-          <a href="javascript:void(0)" class="pinterest"></a>
-          <a href="javascript:void(0)" class="plus-icon"></a>
+        <div class="social-icons icons-block" id="<?php print $gigya_id; ?>">
+          <?php print print_r($field_gigya_share_bar); ?>
         </div>
       </div>
+      <?php endif; ?>
       <div class="slider-counter"></div>
     </div>
   </div>

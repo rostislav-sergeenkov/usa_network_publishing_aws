d24a73cdfe776c3e005058dcd635c44e224f2ec0
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2014-12-16 15:23:54 +0300

    added iframe resize functionality

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/consumptionator-video.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/consumptionator-video.js
@@ -2,7 +2,16 @@
 
   Drupal.behaviors.consumptionator_video = {
     attach: function (context, settings) {
-   
+
+      var current_iframe = $('.consumptionator-video-page .video-player-wrapper iframe');
+      var iframe_height = current_iframe.width()*9/16;
+      current_iframe.height(iframe_height);
+
+      $(window).bind('resize', function () {
+        iframe_height = current_iframe.width()*9/16;
+        current_iframe.height(iframe_height);
+        current_iframe.src = current_iframe.src;
+      });
 
     }
   };

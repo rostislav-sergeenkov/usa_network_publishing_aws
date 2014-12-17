d80a1b882b3bf312fadee8b2cd19659f10026fbd
maksim-tarhunakau <--global>
2014-12-17 10:35:33 +0300

    Changing menu

--- docroot/sites/usanetwork/modules/custom/usanetwork_widget/theme/syndicate-navbar-menu.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_widget/theme/syndicate-navbar-menu.tpl.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
 <head>
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-  <?php print $styles; ?> 
-  <?php if ($show_id): ?>
-  <script type="text/javascript"> var $show_id = <?php print $show_id; ?>; </script>
+
+  <?php if (!empty($styles)): ?>
+    <?php foreach ($styles as $style): ?>
+      <?php print $style; ?>
+    <?php endforeach; ?>
   <?php endif; ?>
-  <script>var $env = "<?php print _usanetwork_widget_get_environment(); ?>";</script>
-  <script src="<?php print $library_js_path; ?>"></script>
-  <script src="<?php print $widget_js_path; ?>"></script>
+
+  <?php if ($show_id): ?>
+    <script type="text/javascript"> var $show_id = <?php print $show_id; ?>; </script>
+  <?php endif; ?>
+
+  <?php if (!empty($scripts)): ?>
+    <?php foreach ($scripts as $script): ?>
+      <?php print $script; ?>
+    <?php endforeach; ?>
+  <?php endif; ?>
 </head>
 <!--[if IE 9]>
   <div class="ie ie9">
  <![endif]-->
-<h3>page body goes here..</h3>
-<div id="usanetwork-main-menu"></div>
-<!--[if IE 9]>
-</div>
+  <header>
+    <?php print $menu; ?>
+  </header>
+  <!--[if IE 9]>
+  </div>
 <![endif]-->
 </html>

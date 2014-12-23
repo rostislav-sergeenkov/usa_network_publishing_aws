2433d647e181d7b37ad8df839b52c602e7fc93ac
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2014-12-23 10:01:30 +0300

    theme full episodes page

--- docroot/sites/usanetwork/modules/custom/usanetwork_mpx_video/templates/usanetwork-mpx-video-show-full-episodes-carousel.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_mpx_video/templates/usanetwork-mpx-video-show-full-episodes-carousel.tpl.php
@@ -6,17 +6,17 @@
     <div class="description-wrapper">
       <div class="description-block">
         <div class="caption">access all full episodes</div>
-        <div class="title"><?php print $show_title; ?></div>
+        <div class="title show-color show-color-border"><?php print $show_title; ?></div>
         <div class="additional-text"><?php print $videos_count; ?></div>
       </div>
     </div>
   </div>
-  <div class="carousel carousel-left start" data-carousel-id="2" data-jcarousel="true">
+  <div class="carousel carousel-left start" data-carousel-id="2">
     <ul>
       <?php print $carousel_items; ?>
     </ul>
-    <a href="javascript:void(0)" class="jcarousel-controls jcarousel-control-prev" data-jcarouselcontrol="true"></a>
-    <a href="javascript:void(0)" class="jcarousel-controls jcarousel-control-next inactive" data-jcarouselcontrol="true"></a>
+    <a href="javascript:void(0)" class="jcarousel-controls jcarousel-control-prev"></a>
+    <a href="javascript:void(0)" class="jcarousel-controls jcarousel-control-next"></a>
   </div>
   <a class="get-app" href="">Get the app to watch</a>
 </div>

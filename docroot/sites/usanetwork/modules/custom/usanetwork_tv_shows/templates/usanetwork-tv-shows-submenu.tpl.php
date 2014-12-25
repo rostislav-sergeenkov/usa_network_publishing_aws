876e9d4b3e3712b7abca90f358c48ecca57f0135
Sergii Pavlenko <Sergii_Pavlenko1@epam.com>
2014-12-25 18:07:41 +0200

    fix tv show submenu notice amend

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-submenu.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-submenu.tpl.php
@@ -15,34 +15,36 @@
 </div>
 <div class="show-menu-tab" style="display: none;">
   <?php if ($show_menu_video_url): ?>
-  <div class="show-menu-video">
-    <div class="node node-usanetwork-promo">
-      <a href="<?php print $show_menu_video_url; ?>" class="play-icon">
-        <div class="asset-img">
-          <?php print $show_menu_video_img; ?>
-        </div>
-      </a>
+    <div class="show-menu-video">
+      <div class="node node-usanetwork-promo">
+        <a href="<?php print $show_menu_video_url; ?>" class="play-icon">
+          <div class="asset-img">
+            <?php print $show_menu_video_img; ?>
+          </div>
+        </a>
 
-      <div class="title-overlay meta">
-        <div class="title"><?php print $show_menu_video_title; ?></div>
-        <div class="caption"><?php print $show_menu_video_caption; ?></div>
+        <div class="title-overlay meta">
+          <div class="title"><?php print $show_menu_video_title; ?></div>
+          <div class="caption"><?php print $show_menu_video_caption; ?></div>
+        </div>
       </div>
     </div>
-  </div>
   <?php endif; ?>
   <ul class="show-menu menu">
-    <?php $i = 0; 
-    foreach ($show_menu_tab_items as $show_menu_tab_item): ?>
-      <?php if (isset($show_menu_tab_item['child']) && $show_menu_tab_item['child']): ?>
-        <li class="<?php if (count($show_menu_tab_item['child']) > 0): ?>expanded<?php endif; ?> <?php if ($i == 0): ?> active<?php endif; ?>"><?php print $show_menu_tab_item['main_link']; ?>
+    <?php
+    $i = 0;
+    foreach ($show_menu_tab_items as $show_menu_tab_item):
+      ?>
+      <li class="<?php if (isset($show_menu_tab_item['child']) && count($show_menu_tab_item['child']) > 0): ?>expanded<?php endif; ?> <?php if ($i == 0): ?> active<?php endif; ?>"><?php print $show_menu_tab_item['main_link']; ?>
+          <?php if (isset($show_menu_tab_item['child']) && $show_menu_tab_item['child']): ?>
           <ul class="menu<?php if ($i == 0): ?> active<?php endif; ?>">
-            <?php foreach ($show_menu_tab_item['child'] as $show_menu_tab_item_child):?>
-            <li><?php print $show_menu_tab_item_child; ?></li>
-            <?php endforeach; ?>
+            <?php foreach ($show_menu_tab_item['child'] as $show_menu_tab_item_child): ?>
+              <li><?php print $show_menu_tab_item_child; ?></li>
+          <?php endforeach; ?>
           </ul>
-        </li>
       <?php endif; ?>
-      <?php  $i++; ?>
-    <?php endforeach; ?>
+      </li>
+      <?php $i++; ?>
+<?php endforeach; ?>
   </ul>
 </div>

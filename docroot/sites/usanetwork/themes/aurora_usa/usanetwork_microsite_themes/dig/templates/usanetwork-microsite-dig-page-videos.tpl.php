5b9c86fc25edb2c6283f6fbc9e3bdea033bf9ffa
Donna Vaughan <donna.vaughan@nbcuni.com>
2015-01-06 22:21:05 -0600

    dig / microsites -- more updates to video code, switched url handling from hashes to window.history.pushState, EPAM team fixes for a-spot

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/templates/usanetwork-microsite-dig-page-videos.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/templates/usanetwork-microsite-dig-page-videos.tpl.php
@@ -9,12 +9,46 @@
 <?php if (!empty($background_url)): ?>
 <div class="microsite-section-container" data-bg-url="<?php print $background_url; ?>">
 <?php endif; ?>
-  <div class="left-pane">
-    LEFT PANE CONTENT GOES HERE
-  </div>
-  <div class="right-pane">
-    RIGHT PANE CONTENT GOES HERE
-    <div class="ad300x250"><?php print render($ad300x250); ?></div>
+  <div class="full-pane">
+    <div class="video-container">
+      <?php if (!empty($in_player['rendered'])): ?>
+      <div class="video-player">
+        <?php print $in_player['rendered']; ?>
+      </div>
+      <?php endif; ?>
+
+      <?php if (!empty($in_player['title'])): ?>
+      <div id="video-title"><?php print $in_player['title']; ?></div>
+      <?php endif; ?>
+
+      <?php if (!empty($in_player['description'])): ?>
+      <div id="video-description"><?php print $in_player['description']; ?></div>
+      <?php endif; ?>
+
+      <?php if (!empty($in_player['season_number']) || !empty($in_player['episode_number']) || !empty($in_player['airdate'])): ?>
+      <div id="video-info">
+        <?php if (!empty($in_player['season_number'])): ?>
+        <span id="video-season">S<?php print $in_player['season_number']; ?></span>
+        <?php endif; ?>
+        <?php if (!empty($in_player['episode_number'])): ?>
+        <span id="video-episode">E<?php print $in_player['episode_number']; ?></span>
+        <?php endif; ?>
+        <?php if (!empty($in_player['airdate'])): ?>
+        <span id="video-airdate">Air Date: <?php print $in_player['airdate']; ?></span>
+        <?php endif; ?>
+      </div>
+      <?php endif; ?>
+    </div>
+
+    <ul>
+    <?php foreach ($videos as $video): ?>
+      <?php if (!empty($video['url']) && !empty($video['rendered'])): ?>
+      <li data-video-url="<?php print $video['url']; ?>">
+        <?php print $video['rendered']; ?>
+      </li>
+      <?php endif; ?>
+    <?php endforeach; ?>
+    </ul>
   </div>
 <?php if (!empty($background_url)): ?>
 </div>

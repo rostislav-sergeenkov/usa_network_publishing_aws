290dc6c84feafbdddc724375f590854164a66726
Ina Shulha <ina_shulha@epam.com>
2015-01-15 18:55:43 +0300

    Companion Ad for Microsites

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/templates/usanetwork-microsite-dig-page-videos.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/templates/usanetwork-microsite-dig-page-videos.tpl.php
@@ -10,11 +10,12 @@
 <?php if (!empty($background_url)): ?>
   <div class="microsite-section-container" data-bg-url="<?php print $background_url; ?>">
 <?php endif; ?>
-  <div class="full-pane">
+  <div class="full-pane" data-tve-player>
     <div id="video-container">
       <?php if (!empty($in_player)): ?>
         <?php print $in_player; ?>
       <?php endif; ?>
+      <div id="ad_300x60_1"></div>
     </div>
     <?php if (!empty($videos)): ?>
       <div id="block-usanetwork-mpx-video-usa-mpx-video-views" class="block block-usanetwork-mpx-video">
@@ -25,12 +26,12 @@
                 <div class="item-list">
                   <ul>
                     <?php foreach ($videos as $v_key => $video): ?>
-                      <?php if ($v_key == 2): ?><li class="ad300x250 views-row grid-item"></li><?php endif; ?>
                       <?php if (!empty($video['url']) && !empty($video['rendered'])): ?>
                         <li class="views-row grid-item" data-video-url="<?php print $video['url']; ?>" data-fid="<?php print $video['fid']; ?>">
                           <?php print $video['rendered']; ?>
                         </li>
                       <?php endif; ?>
+                      <?php if ($v_key == 1): ?><li class="views-row grid-item"><div id="ad_300x250_1"></div></li><?php endif; ?>
                     <?php endforeach; ?>
                   </ul>
                 </div>

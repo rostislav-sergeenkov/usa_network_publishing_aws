bb4f907eb76844144c61f735161c8b7f18e09e68
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-01-15 14:14:59 +0300

    add video trailer functional

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/js/microsite_flexslider.js
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/js/microsite_flexslider.js
@@ -3,8 +3,17 @@
   Drupal.behaviors.micrositeAspot = {
     attach: function (context, settings) {
 
+      $('#aspot-video-container').hide();
+
+      $('.aspot-node-55161 .aspot-link').click(function(e){
+        e.preventDefault();
+        $('#show-aspot-microsite').css('opacity', 0);
+        var showAspotVideoDone = 0;
+        showAspotVideoShow(showAspotVideoDone);
+        showAspotVideoPauseFlexslider();
+      });
+
       $('body').once('micrositeFlexslider', function () {
-        console.log("flexslider_a-spot");
         $slideshow_selector = $('.microsite-section-container #show-aspot-microsite ul');
         $touch = true;
         if ($slideshow_selector.find('li').length <= 1){
@@ -56,8 +65,6 @@
           var show = Drupal.settings.aspotSettings.show;
           var mp4_url = Drupal.settings.aspotSettings.mp4_url;
           var webm_url = Drupal.settings.aspotSettings.webm_url;
-          var mp4_url = 'http://a248.g.akamai.net/7/1697/141550/0s/usavideo1.download.akamai.com/141550/video/NBCU_USA_Network/697/791/SUITS_RM_SUMMER_FINALE.mp4';
-          var webm_url = 'http://a248.g.akamai.net/7/1697/141550/0s/usavideo1.download.akamai.com/141550/mezzanine/NBCU_USA_Network/697/791/SUITS_RM_SUMMER_FINALE.webmhd.webm';
           aspotVideos[show] = {};
           aspotVideos[show] = AspotVideo(mp4_url, webm_url);
         }
@@ -73,11 +80,8 @@
         }
       }
       if (typeof aspotVideoEnabled != 'undefined' && aspotVideoEnabled && !usa_deviceInfo.smartphone && !usa_deviceInfo.mobileDevice) {
-
         var showAspotVideoPauseFlexslider = function () {
           if (typeof $showSlideshow.flexslider === 'function') {
-            // if we don't put a timeout here, the flexslider control
-            // nav ("the dots") never appear
             setTimeout(function () {
               $showSlideshow.flexslider("pause");
               showAspotVideoResumeFlexsliderPlay();
@@ -90,16 +94,13 @@
 
         var showAspotVideoResumeFlexsliderPlay = function () {
           $('#aspot-video').bind('ended', function () {
-            $('#main-slider').css('opacity', 1);
-            $('#aspot-video-container').animate({'opacity': 0}, 400, function () {
-              $showSlideshow.flexslider('play');
-              $(this).remove();
-            })
+            $('#show-aspot-microsite').css('opacity', 1);
+            $('#aspot-video-container').hide().html('');
+            $showSlideshow.flexslider('play');
           });
         }
 
-        var showAspotVideoDone = 0;
-        var showAspotVideoShow = function () {
+        var showAspotVideoShow = function (showAspotVideoDone) {
           if (!showAspotVideoDone) {
             var aspotVideoTag = '<video id="aspot-video" width="100%" autoplay><source src="' + aspotVideoMp4VideoUrl + '" type="video/mp4"><source src="' + aspotVideoWebmVideoUrl + '" type="video/webm">Your browser does not support the video tag.</video>';
             if (aspotVideoAgent.indexOf('msie') != -1) {
@@ -114,15 +115,6 @@
             setTimeout(showAspotVideoShow, 500);
           }
         }
-
-        $(document).ready(function () {
-//          $('#activeContent #main-slider').css('opacity', 0);
-         // showAspotVideoShow();
-         // showAspotVideoPauseFlexslider();
-
-          $('#aspot-video-container').css('display', 'none');
-
-        });
       } // A-SPOT VIDEOS
     }
   };

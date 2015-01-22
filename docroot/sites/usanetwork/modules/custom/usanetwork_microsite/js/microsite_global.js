d29d55a139203462c470d82d6fdc0dce378bb9ef
YuryKaminski <Yury_Kaminski@epam.com>
2015-01-22 11:55:58 +0300

    TA3358505-Fix-double-load-ad-on-dig

--- docroot/sites/usanetwork/modules/custom/usanetwork_microsite/js/microsite_global.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_microsite/js/microsite_global.js
@@ -5,17 +5,58 @@
   var urlPath = window.location.pathname;
   var testData;
 
+  var activeSection = 'home';
   Drupal.behaviors.microsite_scroll = {
-    attach: function (context, settings) {
 
+
+
+  // Usa_refreshMicrositeAdsBySection.
+  usa_refreshMicrositeAdsBySection: function (adContainer) {
+    usa_debug('usa_refreshMicrositeAdsBySection(' + adContainer + ')');
+    jQuery(adContainer + ' iframe').attr('src', jQuery(adContainer + ' iframe').attr('src'));
+  },
+
+  // 300x250 -- not for video companion ads!!
+  create300x250Ad: function(section) {
+    usa_debug('create300x250Ad(' + section + ')');
+    if (activeSection != 'videos') {
+
+      // check to see if there's already an ad
+      if (jQuery('.dart-name-300x250_ifr_reload_' + section + ' iframe').length) {
+        Drupal.behaviors.microsite_scroll.usa_refreshMicrositeAdsBySection('.dart-name-300x250_ifr_reload_' + section);
+      }
+      else if (jQuery('.dart-name-220x60_ifr_reload_' + section + ' iframe').length) {
+        Drupal.behaviors.microsite_scroll.usa_refreshMicrositeAdsBySection('.dart-name-220x60_ifr_reload_' + section);
+      }
+      else {
+        iframeQueue = [];
+        Drupal.DART.tag('{"machinename":"300x250_ifr_reload_' + section + '","name":"300x250 script","pos":"7","sz":"300x250","block":"1","settings":{"overrides":{"site":"","zone":"","slug":""},"options":{"scriptless":0,"method":"adi"},"key_vals":[]},"table":"dart_tags","type":"Overridden","export_type":3,"disabled":false,"export_module":"usanetwork_ads","key_vals":{"pos":[{"val":"7","eval":false}],"sz":[{"val":"300x250","eval":false}],"site":[{"val":"usa","eval":0}],"sect":[{"val":"Drupal.settings.USA.DART.values.sect || \u0027\u0027","eval":1}],"sub":[{"val":"Drupal.settings.USA.DART.values.sub || \u0027\u0027","eval":1}],"sub2":[{"val":"Drupal.settings.USA.DART.values.sub2 || \u0027\u0027","eval":1}],"genre":[{"val":"Drupal.settings.USA.DART.values.genre || \u0027\u0027","eval":1}],"daypart":[{"val":"Drupal.settings.USA.DART.values.genre || \u0027\u0027","eval":1}],"!c":[{"val":"usa","eval":0},{"val":"Drupal.settings.USA.DART.values.sect || \u0027\u0027","eval":1},{"val":"Drupal.settings.USA.DART.values.sub || \u0027\u0027","eval":1}],"tandomad":[{"val":"eTandomAd","eval":1}],"\u003Cnone\u003E":[{"val":"top.__nbcudigitaladops_dtparams || \u0027\u0027","eval":1}],"tile":[{"val":"tile++","eval":true}],"ord":[{"val":"ord","eval":true}]},"prefix":"nbcu","site":"usa","zone":"default","slug":"","network_id":"","noscript":{"src":"http:\/\/ad.doubleclick.net\/ad\/nbcu.usa\/default;pos=7;sz=300x250;site=usa;!c=usa;tile=25;ord='+ord+'?","href":"http:\/\/ad.doubleclick.net\/jump\/nbcu.usa\/default;pos=7;sz=300x250;site=usa;!c=usa;tile=25;ord='+ord+'?"}}');
+        // write iframe ad units to page
+        if (iframeQueue.length) {
+          for (var i=0, iframeQueueLength = iframeQueue.length; i < iframeQueueLength; i++) {
+            // 300x250 second
+            if (iframeQueue[i].tag.indexOf('300x250') != '-1') {
+              jQuery('.dart-name-' + iframeQueue[i].tag).html(iframeQueue[i].html);
+            }
+            // 220x60 last
+            if (iframeQueue[i].tag.indexOf('220x60') != '-1') {
+              jQuery('.dart-name-' + iframeQueue[i].tag).html(iframeQueue[i].html);
+            }
+          }
+        }
+      }
+    }
+  },
+
+    attach: function (context, settings) {
 
 
       // set defaults
       var siteName = Drupal.settings.microsites_settings.title,
           basePath = Drupal.settings.microsites_settings.base_path,
           basePageName = siteName + ' | USA Network',
-          activeSection = 'home',
-          activeItem = '';
+          activeItem = '',
+          self = this;
 
       // usa_debug
       var hostname = window.location.hostname,
@@ -29,15 +70,6 @@
         }
       }
 
-
-      // ADS
-      // usa_refreshMicrositeAdsBySection
-      function usa_refreshMicrositeAdsBySection(adContainer) {
-        usa_debug('usa_refreshMicrositeAdsBySection(' + adContainer + ')');
-        jQuery(adContainer + ' iframe').attr('src', jQuery(adContainer + ' iframe').attr('src'));
-      }
-
-
       // createAds
       // there is a race condition if we try to create both the 728x90
       // and the 300x250 at about the same time, so we create the 728x90
@@ -48,8 +80,8 @@
         usa_debug('createAds(' + section + ')');
 
         // check to see if there is an ad already there
-        if (jQuery('.dart-name-728x90_ifr_reload_' + section + ' iframe').length > 0) {
-          usa_refreshMicrositeAdsBySection('.dart-name-728x90_ifr_reload_' + section);
+        if (jQuery('.dart-name-728x90_ifr_reload_' + section + ' iframe').length) {
+            Drupal.behaviors.microsite_scroll.usa_refreshMicrositeAdsBySection('.dart-name-728x90_ifr_reload_' + section);
         }
         // if no 728x90 ad in this section yet, create it
         else {
@@ -63,7 +95,7 @@
           Drupal.DART.tag('{"machinename":"728x90_ifr_reload_' + section + '","name":"728x90 script","pos":"7","sz":"728x90","block":"1","settings":{"overrides":{"site":"","zone":"","slug":""},"options":{"scriptless":0,"method":"adi"},"key_vals":[]},"table":"dart_tags","type":"Overridden","export_type":3,"disabled":false,"export_module":"usanetwork_ads","key_vals":{"pos":[{"val":"7","eval":false}],"sz":[{"val":"728x90","eval":false}],"site":[{"val":"usa","eval":0}],"sect":[{"val":"Drupal.settings.USA.DART.values.sect || \u0027\u0027","eval":1}],"sub":[{"val":"Drupal.settings.USA.DART.values.sub || \u0027\u0027","eval":1}],"sub2":[{"val":"Drupal.settings.USA.DART.values.sub2 || \u0027\u0027","eval":1}],"genre":[{"val":"Drupal.settings.USA.DART.values.genre || \u0027\u0027","eval":1}],"daypart":[{"val":"Drupal.settings.USA.DART.values.genre || \u0027\u0027","eval":1}],"!c":[{"val":"usa","eval":0},{"val":"Drupal.settings.USA.DART.values.sect || \u0027\u0027","eval":1},{"val":"Drupal.settings.USA.DART.values.sub || \u0027\u0027","eval":1}],"tandomad":[{"val":"eTandomAd","eval":1}],"\u003Cnone\u003E":[{"val":"top.__nbcudigitaladops_dtparams || \u0027\u0027","eval":1}],"tile":[{"val":"tile++","eval":true}],"ord":[{"val":"ord","eval":true}]},"prefix":"nbcu","site":"usa","zone":"default","slug":"","network_id":"","noscript":{"src":"http:\/\/ad.doubleclick.net\/ad\/nbcu.usa\/default;pos=7;sz=728x90;site=usa;!c=usa;tile=25;ord='+ord+'?","href":"http:\/\/ad.doubleclick.net\/jump\/nbcu.usa\/default;pos=7;sz=728x90;site=usa;!c=usa;tile=25;ord='+ord+'?"}}');
 
           // write iframe ad units to page
-          if (iframeQueue.length > 0) {
+          if (iframeQueue.length) {
             for (var i=0, iframeQueueLength = iframeQueue.length; i < iframeQueueLength; i++) {
               // 728x90
               if (iframeQueue[i].tag.indexOf('728x90') != '-1') {
@@ -72,48 +104,13 @@
             }
           }
         }
-        // end 728x90
-
-        // now create the 300x250
-        create300x250Ad(section);
-      }
-
-      // 300x250 -- not for video companion ads!!
-      function create300x250Ad(section) {
-        usa_debug('create300x250Ad(' + section + ')');
-        if (activeSection != 'videos') {
-
-          // check to see if there's already an ad
-          if (jQuery('.dart-name-300x250_ifr_reload_' + section + ' iframe').length > 0) {
-            usa_refreshMicrositeAdsBySection('.dart-name-300x250_ifr_reload_' + section);
-          }
-          else if (jQuery('.dart-name-220x60_ifr_reload_' + section + ' iframe').length > 0) {
-            usa_refreshMicrositeAdsBySection('.dart-name-220x60_ifr_reload_' + section);
-          }
-          else {
-            iframeQueue = new Array();
-
-            Drupal.DART.tag('{"machinename":"300x250_ifr_reload_' + section + '","name":"300x250 script","pos":"7","sz":"300x250","block":"1","settings":{"overrides":{"site":"","zone":"","slug":""},"options":{"scriptless":0,"method":"adi"},"key_vals":[]},"table":"dart_tags","type":"Overridden","export_type":3,"disabled":false,"export_module":"usanetwork_ads","key_vals":{"pos":[{"val":"7","eval":false}],"sz":[{"val":"300x250","eval":false}],"site":[{"val":"usa","eval":0}],"sect":[{"val":"Drupal.settings.USA.DART.values.sect || \u0027\u0027","eval":1}],"sub":[{"val":"Drupal.settings.USA.DART.values.sub || \u0027\u0027","eval":1}],"sub2":[{"val":"Drupal.settings.USA.DART.values.sub2 || \u0027\u0027","eval":1}],"genre":[{"val":"Drupal.settings.USA.DART.values.genre || \u0027\u0027","eval":1}],"daypart":[{"val":"Drupal.settings.USA.DART.values.genre || \u0027\u0027","eval":1}],"!c":[{"val":"usa","eval":0},{"val":"Drupal.settings.USA.DART.values.sect || \u0027\u0027","eval":1},{"val":"Drupal.settings.USA.DART.values.sub || \u0027\u0027","eval":1}],"tandomad":[{"val":"eTandomAd","eval":1}],"\u003Cnone\u003E":[{"val":"top.__nbcudigitaladops_dtparams || \u0027\u0027","eval":1}],"tile":[{"val":"tile++","eval":true}],"ord":[{"val":"ord","eval":true}]},"prefix":"nbcu","site":"usa","zone":"default","slug":"","network_id":"","noscript":{"src":"http:\/\/ad.doubleclick.net\/ad\/nbcu.usa\/default;pos=7;sz=300x250;site=usa;!c=usa;tile=25;ord='+ord+'?","href":"http:\/\/ad.doubleclick.net\/jump\/nbcu.usa\/default;pos=7;sz=300x250;site=usa;!c=usa;tile=25;ord='+ord+'?"}}');
-
-            // write iframe ad units to page
-            if (iframeQueue.length > 0) {
-              for (var i=0, iframeQueueLength = iframeQueue.length; i < iframeQueueLength; i++) {
-                // 300x250 second
-                if (iframeQueue[i].tag.indexOf('300x250') != '-1') {
-                  jQuery('.dart-name-' + iframeQueue[i].tag).html(iframeQueue[i].html);
-                }
-                // 220x60 last
-                if (iframeQueue[i].tag.indexOf('220x60') != '-1') {
-                  jQuery('.dart-name-' + iframeQueue[i].tag).html(iframeQueue[i].html);
-                }
-              }
-            }
-          }
+        if (Drupal.behaviors.microsite_carousel.carousel_inited) {
+          self.create300x250Ad(section);
         }
+
       }
 
-
-      // URL HANDLING
+     // URL HANDLING
       // toTitleCase
       function toTitleCase(str) {
         return str.replace(/\w\S*/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});

a9ef94d08179ccb681ae11ada494192a86481699
Ihar Karavatski <Ihar_Karavatski@epam.com>
2015-01-27 15:01:44 +0000

    dig / microsite -- Endcard. Fix for aspot.

--- docroot/sites/usanetwork/modules/custom/usanetwork_microsite/js/usanetwork_video_endcard.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_microsite/js/usanetwork_video_endcard.js
@@ -10,7 +10,7 @@
     if (typeof tpController !== 'undefined') {
       tpController.addEventListener('OnEndcardCountdownEnd', Drupal.usanetwork_video_endcard.OnCountdownEnd);
     }
-    var previewItem = $('#block-usanetwork-mpx-video-usa-mpx-video-views .item-list ul li');
+    var previewItem = $('#block-usanetwork-mpx-video-usa-mpx-video-views .item-list ul li, #show-aspot-microsite .aspot-link');
     previewItem.click(function(e){
       tpController.addEventListener('OnEndcardCountdownEnd', Drupal.usanetwork_video_endcard.OnCountdownEnd);
     });

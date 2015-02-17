f60140b08b8eca5eecdda5539cfd065d452224f2
Donna Vaughan <donna.vaughan@nbcuni.com>
2015-02-17 17:16:21 -0600

    dig / microsite -- galleries: started adding gallery navigation

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/js/microsite_galleries_bxslider.js
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/js/microsite_galleries_bxslider.js
@@ -1,10 +1,10 @@
-// BXSLIDER for characters
+// BXSLIDER for galleries
 (function ($) {
-  Drupal.behaviors.micrositeBxSliders = {
+  Drupal.behaviors.micrositeGalleriesBxSliders = {
 
 
     micrositeReloadBxSlider: function() {
-      if ($('#microsite #home #characters-cast-list').length) $('#microsite #home .characters-cast #characters-cast-list').css({ 'opacity': 0 });
+      if ($('#microsite #galleries .galleries-bxslider').length) $('#microsite #galleries #characters-galleries-list').css({ 'opacity': 0 });
       var wwidth = $(window).width(),
           transitionWidth = 640,
           numSlides = (wwidth > transitionWidth) ? 5 : 3,
@@ -33,29 +33,45 @@
       // set defaults
       var wwidth = $(window).width(),
           transitionWidth = 640,
-          numSlides = (wwidth > transitionWidth) ? 5 : 3,
-          slideWidth = (wwidth > transitionWidth) ? 242 : 100,
+          numSlides = (wwidth > transitionWidth) ? 2 : 2,
+          slideWidth = (wwidth > transitionWidth) ? 140 : 140,
           slideMargin = 10,
           self = this;
 
 
-      self.charactersCastBxSlider = $('.characters-cast-bxslider').bxSlider({
+      self.epGalleryBxSlider = $('#microsite #galleries #ep-galleries .galleries-bxslider').bxSlider({
         slideWidth: slideWidth,
         minSlides: numSlides,
         maxSlides: numSlides,
         slideMargin: slideMargin,
-        nextSelector: '#characters-cast-next',
-        prevSelector: '#characters-cast-prev',
+        nextSelector: '#ep-galleries-next',
+        prevSelector: '#ep-galleries-prev',
         nextText: 'Next',
         prevText: 'Previous',
         infiniteLoop: false,
         hideControlOnEnd: true,
         onSliderLoad: function(){
-          $('#microsite #home #characters-cast-list').animate({ 'opacity': 1 }, 1000, 'jswing');
+          $('#microsite #galleries #ep-galleries .galleries-bxslider').animate({ 'opacity': 1 }, 1000, 'jswing');
         }
       });
 
-      $('#microsite #home .characters-cast li a').click(function(e){
+      self.characterGalleryBxSlider = $('#microsite #galleries #character-galleries .galleries-bxslider').bxSlider({
+        slideWidth: slideWidth,
+        minSlides: numSlides,
+        maxSlides: numSlides,
+        slideMargin: slideMargin,
+        nextSelector: '#character-galleries-next',
+        prevSelector: '#character-galleries-prev',
+        nextText: 'Next',
+        prevText: 'Previous',
+        infiniteLoop: false,
+        hideControlOnEnd: true,
+        onSliderLoad: function(){
+          $('#microsite #galleries #character-galleries .galleries-bxslider').animate({ 'opacity': 1 }, 1000, 'jswing');
+        }
+      });
+
+      $('#microsite #galleries .galleries-bxslider li a').click(function(e){
         var anchorFull = this.href,
             anchorPathParts = Drupal.behaviors.microsite_scroll.micrositeGetUrlPath(anchorFull);
 
@@ -74,9 +90,11 @@
           anchor = anchorPathParts[1];
           item = (typeof anchorPathParts[2] != 'undefined') ? anchorPathParts[2] : '';
 
-          if (anchor == 'characters') {
+/*
+          if (anchor == 'galleries') {
             if (item != '') Drupal.behaviors.microsite_characters.micrositeSwitchCharacters('nav-' + item, 10);
           }
+*/
         }
       });
 
--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/templates/usanetwork-microsite-dig-page-galleries.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/templates/usanetwork-microsite-dig-page-galleries.tpl.php
@@ -4,10 +4,20 @@
  *
  * Variables:
  * - $background_url - the URL of page background
- * - $galleries - array of galleries composed of the following for each gallery:
+ * - $gallery -  the url-specified gallery info:
  * - - title - the title of the gallery
  * - - description - the description of the gallery
  * - - rendered - the rendered view of the gallery
+ * - episodic_galleries - array of episodic galleries
+ * - - episodic_galleries[n]['url'] - the machine-readable url path
+ * - - episodic_galleries[n]['gallery_nid'] - the node id
+ * - - episodic_galleries[n]['title'] - the title
+ * - - episodic_galleries[n]['cover_img'] - the cover image url
+ * - character_galleries - array of non-episodic galleries
+ * - - character_galleries[n]['url'] - the machine-readable url path
+ * - - character_galleries[n]['gallery_nid'] - the node id
+ * - - character_galleries[n]['title'] - the title
+ * - - character_galleries[n]['cover_img'] - the cover image url
  * - $ad300x250 - the code to render the 300 x 250 ad
  */
 ?>
@@ -34,14 +44,34 @@
 
   <div class="right-pane">
     <?php if (!empty($episodic_galleries)): ?>
-      <div class="episodic-galleries">
-        <?php print $episodic_galleries; ?>
-      </div>
+    <div id="ep-galleries" class="galleries-nav">
+      <h3>Episodic</h3>
+      <div id="ep-galleries-prev" class="prev btns"><span class="screen-reader-text">Previous</span></div>
+      <ul id="ep-galleries-list" class="galleries-bxslider">
+      <?php foreach ($episodic_galleries as $ep_gallery): ?>
+        <?php if (!empty($ep_gallery['url']) && !empty($ep_gallery['gallery_nid']) && !empty($ep_gallery['title']) && !empty($ep_gallery['cover_img'])): ?>
+        <?php /* @TODO: Dynamically create the /dig/characters part of the following url */ ?>
+        <li data_node_id="<?php print $ep_gallery['gallery_nid']; ?>"><a href="/dig/characters/<?php print $ep_gallery['url']; ?>"><img src="<?php print $ep_gallery['cover_img']; ?>" alt="<?php print $ep_gallery['title']; ?>"><div class="gallery-title"><?php print $ep_gallery['title']; ?></div></a></li>
+        <?php endif; ?>
+      <?php endforeach; ?>
+      </ul>
+      <div id="ep-galleries-next" class="next btns"><span class="screen-reader-text">Next</span></div>
+    </div>
     <?php endif; ?>
     <?php if (!empty($character_galleries)): ?>
-      <div class="character-galleries">
-        <?php print $character_galleries; ?>
-      </div>
+    <div id="character-galleries" class="galleries-nav">
+      <h3>Cast, Crew & Set</h3>
+      <div id="character-galleries-prev" class="prev btns"><span class="screen-reader-text">Previous</span></div>
+      <ul id="character-galleries-list" class="galleries-bxslider">
+      <?php foreach ($character_galleries as $char_gallery): ?>
+        <?php if (!empty($char_gallery['url']) && !empty($char_gallery['gallery_nid']) && !empty($char_gallery['title']) && !empty($char_gallery['cover_img'])): ?>
+        <?php /* @TODO: Dynamically create the /dig/characters part of the following url */ ?>
+        <li data_node_id="<?php print $char_gallery['gallery_nid']; ?>"><a href="/dig/characters/<?php print $char_gallery['url']; ?>"><img src="<?php print $char_gallery['cover_img']; ?>" alt="<?php print $char_gallery['title']; ?>"><div class="gallery-title"><?php print $char_gallery['title']; ?></div></a></li>
+        <?php endif; ?>
+      <?php endforeach; ?>
+      </ul>
+      <div id="character-galleries-next" class="next btns"><span class="screen-reader-text">Next</span></div>
+    </div>
     <?php endif; ?>
     <div class="ad300x250 dart-tag dart-name-300x250_ifr_reload_galleries"></div>
   </div>

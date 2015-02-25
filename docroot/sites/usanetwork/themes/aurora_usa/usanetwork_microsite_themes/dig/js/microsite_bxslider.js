8d2bd44bc13542c77c836dd361850499121cd97c
Nikolai Verbitskii <verbitskiinikolai@gmail.com>
2015-02-25 17:44:40 +0300

    dig / microsite: fix for ie9 visibility THE CAST & CREW gallery

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/js/microsite_bxslider.js
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/js/microsite_bxslider.js
@@ -15,7 +15,7 @@
         slideWidth: slideWidth,
         minSlides: numSlides,
         maxSlides: numSlides,
-//        useCSS: true,
+        useCSS: true,
         slideMargin: slideMargin,
         nextSelector: '#characters-cast-next',
         prevSelector: '#characters-cast-prev',
@@ -24,7 +24,7 @@
         infiniteLoop: false,
         hideControlOnEnd: true,
         onSliderLoad: function(){
-          //$('#microsite #home .characters-cast').animate({ 'opacity': 1 }, 1000, 'jswing');
+          $('#microsite #home .characters-cast').animate({ 'opacity': 1 }, 1000, 'jswing');
         }
       })
     },
@@ -45,7 +45,7 @@
         slideWidth: slideWidth,
         minSlides: numSlides,
         maxSlides: numSlides,
-//        useCSS: true,
+        useCSS: true,
         slideMargin: slideMargin,
         nextSelector: '#characters-cast-next',
         prevSelector: '#characters-cast-prev',
@@ -54,7 +54,7 @@
         infiniteLoop: false,
         hideControlOnEnd: true,
         onSliderLoad: function(){
-          //$('#microsite #home .characters-cast').css('overflow', 'visible').animate({ 'opacity': 1 }, 1000, 'jswing');
+          $('#microsite #home .characters-cast').css('overflow', 'visible').animate({ 'opacity': 1 }, 1000, 'jswing');
         }
       });
 

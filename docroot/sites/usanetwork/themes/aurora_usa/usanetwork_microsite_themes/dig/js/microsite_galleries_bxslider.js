1c742f8c41a4c80d1d101f7e0848f93c64c9fea9
Alexander Belko <snoooplex@gmail.com>
2015-02-27 11:16:46 +0300

    Fix page reload bug

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/js/microsite_galleries_bxslider.js
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/js/microsite_galleries_bxslider.js
@@ -288,7 +288,7 @@
           anchorPathParts = Drupal.behaviors.microsite_scroll.micrositeGetUrlPath(anchorFull);
 
         // Unbind click while selected gallery loading
-        $('#microsite #galleries .galleries-bxslider li a').unbind('click');
+        $('#microsite #galleries .galleries-bxslider li > div').unbind('click');
         // if this is an internal microsite url
         // prevent the default action
         // and show the correct microsite item without a page reload
@@ -303,13 +303,13 @@
 
           var nid = $(this).parent().attr('data-node-id');
           self.switchGallery(nid, function() {
-            $('#microsite #galleries .galleries-bxslider li a').bind('click', changeGalleryHandler);
+            $('#microsite #galleries .galleries-bxslider li > div').bind('click', changeGalleryHandler);
           });
           history.pushState({"state": anchorFull}, anchorFull, anchorFull);
         }
       };
 
-      $('#microsite #galleries .galleries-bxslider li a').bind('click', changeGalleryHandler);
+      $('#microsite #galleries .galleries-bxslider li > div').bind('click', changeGalleryHandler);
 
       $(window).bind('resize', function () {
         self.micrositeReloadSliders();

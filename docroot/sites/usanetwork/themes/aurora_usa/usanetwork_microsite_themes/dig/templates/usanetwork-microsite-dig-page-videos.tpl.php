c408aa9f4ffd3830dcf4522288f0f75491c3a09c
Nikolai Verbitskii <verbitskiinikolai@gmail.com>
2015-03-09 10:47:30 +0300

    fix files filteres

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/templates/usanetwork-microsite-dig-page-videos.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/dig/templates/usanetwork-microsite-dig-page-videos.tpl.php
@@ -12,25 +12,28 @@
     <div class="video-container-ajaxed" data-tve-player>
       <div class="video-auth-player-wrapper file-video-mpx video usa-video featured-asset premium-asset">
         <div class="tve-help">
-          <div class="tve-msg">By signing in with your TV provider you get access to full<br/>episodes the day after they
+          <div class="tve-msg">By signing in with your TV provider you get access to full<br/>episodes the day after
+            they
             air!
           </div>
-          <div class="tve-download">To unlock full episodes you can select an episode to sign in<br/>- or -<br/>DOWNLOAD THE
+          <div class="tve-download">To unlock full episodes you can select an episode to sign in<br/>- or -<br/>DOWNLOAD
+            THE
             USA NOW APP
           </div>
           <div class="tve-download-link">
             <a href="https://play.google.com/store/apps/details?id=com.usanetwork.watcher"><img
-                src="/sites/usanetwork/themes/aurora_usa/images/googleplay.png" alt=""/></a>
+                  src="/sites/usanetwork/themes/aurora_usa/images/googleplay.png" alt=""/></a>
             <a href="http://www.usanetwork.com/usanow"><img
-                src="/sites/usanetwork/themes/aurora_usa/images/usanow.png" alt=""/></a>
+                  src="/sites/usanetwork/themes/aurora_usa/images/usanow.png" alt=""/></a>
             <a href="https://itunes.apple.com/us/app/usa-now/id661695783?mt=8"><img
-                src="/sites/usanetwork/themes/aurora_usa/images/appstore.png" alt=""/></a>
+                  src="/sites/usanetwork/themes/aurora_usa/images/appstore.png" alt=""/></a>
           </div>
           <div class="tve-close"><img src="/sites/usanetwork/themes/aurora_usa/images/close.png" alt=""/>Close</div>
         </div>
         <div class="video-player-wrapper" data-ng-if="!global.isAuthN">
           <div class="locked-msg">
-            <span class="first-line"><?php print t('Please sign in with your TV provider to unlock this episode.'); ?></span>
+            <span
+                class="first-line"><?php print t('Please sign in with your TV provider to unlock this episode.'); ?></span>
           </div>
           <div id="player">
             <a href="javascript:void(0)" class="loginButton clean" data-ng-if="!global.isAuthN"
@@ -40,7 +43,8 @@
         </div>
         <div class="video-player video-player-wrapper" data-ng-show="global.isAuthN">
           <div class="custom-play"></div>
-          <div class="ds-1col file file-mpx-video-1 file-video-mpx view-mode-inline_content view-mode-inline_content clearfix ">
+          <div
+              class="ds-1col file file-mpx-video-1 file-video-mpx view-mode-inline_content view-mode-inline_content clearfix ">
           </div>
         </div>
         <div class="tve-help-link signIn">
@@ -56,16 +60,17 @@
       <div class="video-no-auth-player-wrapper">
         <div class="video-player no-auth">
           <div class="custom-play"></div>
-          <div class="ds-1col file file-mpx-video-1 file-video-mpx view-mode-inline_content view-mode-inline_content clearfix ">
-           <iframe class="base-iframe"></iframe>
+          <div
+              class="ds-1col file file-mpx-video-1 file-video-mpx view-mode-inline_content view-mode-inline_content clearfix ">
+            <iframe class="base-iframe"></iframe>
           </div>
         </div>
       </div>
       <div id="ad_300x60_1"></div>
       <div class="video-player-desc">
         <?php if (!empty($video_desc)): ?>
-					<?php print $video_desc; ?>
-				<?php endif; ?>
+          <?php print $video_desc; ?>
+        <?php endif; ?>
       </div>
     </div>
   </div>
@@ -81,13 +86,14 @@
               <?php print $filter_list; ?>
             </div>
           </div>
-          <div id="thumbnail-list" class="view view-usa-mpx-video view-id-usa_mpx_video view-display-id-category_mpx_videos expandable-container view-dom-id-be381e47058d0c21dac9092cb8220a69 jquery-once-2-processed">
+          <div id="thumbnail-list"
+               class="view view-usa-mpx-video view-id-usa_mpx_video view-display-id-category_mpx_videos expandable-container view-dom-id-be381e47058d0c21dac9092cb8220a69 jquery-once-2-processed">
             <div class="view-content">
               <div class="item-list">
                 <ul>
                   <?php
-                      $video_count = count($videos);
-                      foreach ($videos as $v_key => $video): ?>
+                  $video_count = count($videos);
+                  foreach ($videos as $v_key => $video): ?>
                     <?php if (!empty($video['url']) && !empty($video['rendered'])): ?>
                       <li class="thumbnail views-row grid-item <?php print $video['state']; ?>"
                           data-video-url="<?php print $video['url']; ?>"
@@ -99,20 +105,23 @@
                         <?php print $video['rendered']; ?>
                       </li>
                     <?php endif; ?>
-                    <?php if ($v_key == 1 || $video_count == 1): ?><li class="thumbnail views-row grid-item ad"><div id="ad_300x250_1"></div></li><?php endif; ?>
+                    <?php if ($v_key == 1 || $video_count == 1): ?>
+                      <li class="thumbnail views-row grid-item ad">
+                        <div id="ad_300x250_1"></div>
+                      </li><?php endif; ?>
                   <?php endforeach; ?>
                 </ul>
               </div>
             </div>
-						<div class="expandable-toggle-wrap <?php if (!empty($more_videos)): ?>active<?php endif; ?>">
-							<div class="expandable-toggle">
-								<div class="item-list">
-									<ul class="pager pager-load-more">
-										<li class="pager-next first last more">more</li>
-									</ul>
-								</div>
-							</div>
-						</div>
+            <div class="expandable-toggle-wrap <?php if (!empty($more_videos)): ?>active<?php endif; ?>">
+              <div class="expandable-toggle">
+                <div class="item-list">
+                  <ul class="pager pager-load-more">
+                    <li class="pager-next first last more">more</li>
+                  </ul>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>

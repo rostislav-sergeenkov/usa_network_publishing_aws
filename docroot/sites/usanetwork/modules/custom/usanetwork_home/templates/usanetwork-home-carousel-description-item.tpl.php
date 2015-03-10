9d62d7981a37bbc71dddd75a1728d353cf457fbf
maksim-tarhunakau <--global>
2015-03-10 16:59:17 +0300

    Recommit

--- docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-carousel-description-item.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-carousel-description-item.tpl.php
@@ -5,6 +5,9 @@
  *  $additional_text
  */
 ?>
+<?php if (!empty($background)): ?>
+  <div class="hidden background-image" data-url="<?php print $background; ?>"></div>
+<?php endif; ?>
 <div class="carousel-description-item start">
   <a href="javascript:void(0)" class="description-button"></a>
   <div class="description-wrapper">
@@ -12,11 +15,9 @@
       <div class="caption"><?php print $caption; ?></div>
       <div class="title"><?php print $title; ?></div>
       <div class="additional-text"><?php print $additional_text; ?></div>
+      <?php if (!empty($logo)): ?>
+        <div class="logo"><img src="<?php print $logo; ?>" /></div>
+      <?php endif; ?>
     </div>
   </div>
 </div>
-
-
-
-
-

105cb1da3f93d9a20b5c192ce713d9373236723e
Ina Shulha <ina_shulha@epam.com>
2015-03-11 16:09:29 +0300

    Umbel: script changes

--- docroot/sites/usanetwork/modules/custom/usanetwork_umbel/js/usanetwork_umbel.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_umbel/js/usanetwork_umbel.js
@@ -1,13 +1,14 @@
 /* 
- * Unbel Scripts
+ * Umbel Scripts
  */
+(function ($) {
+  $('form.webform-client-form').bind('submit', function() {
+    var email = $('form.webform-client-form input[type="email"]').val();
+    _umbel.push({"type": "set", "name": "profile.email", "value": email});
+  });
 
-$('form.webform-client-form input[type="submit"]').bind('click', function() {
-  var email = $('form.webform-client-form input[type="email"]').val();
-  _umbel.push({"type": "set", "name": "profile.email", "value": email});
-});
-
-$('form#usanetwork-lyris-newsletter-subscription input[type="submit"]').bind('click', function() {
-  var email = $('form#usanetwork-lyris-newsletter-subscription input[name="email"]').val();
-  _umbel.push({"type": "set","name": "profile.email","value": email});
-});
+  $('form#usanetwork-lyris-newsletter-subscription').bind('submit', function() {
+    var email = $('form#usanetwork-lyris-newsletter-subscription input[name="email"]').val();
+    _umbel.push({"type": "set","name": "profile.email","value": email});
+  });
+})(jQuery);

e4285f7906c74051c8c447b816d459cda21be8d7
Ihar Karavatski <Ihar_Karavatski@epam.com>
2015-03-20 15:20:56 +0000

    mrrobot / microsite -- Changes for multiple microsites.

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/mrrobot/templates/usanetwork-microsite-mrrobot-page-home.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/mrrobot/templates/usanetwork-microsite-mrrobot-page-home.tpl.php
@@ -18,8 +18,7 @@
 <div class="ad-leaderboard dart-tag dart-name-728x90_ifr_reload_home"></div>
 
 <?php
-$imagePath = '/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/mrrobot/images/';
-$sitePath = '/node/56352/microsite';
+$imagePath = '/' . $directory . '/usanetwork_microsite_themes/mrrobot/images/';
 ?>
 <style>
 html, body {

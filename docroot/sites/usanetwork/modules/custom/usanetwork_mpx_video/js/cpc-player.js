61848fbef27533f59e12f6bd5a8fdb5918ae93c4
Nikolai Verbitskii <verbitskiinikolai@gmail.com>
2015-03-25 09:47:30 +0300

    stage: fix js live player

--- docroot/sites/usanetwork/modules/custom/usanetwork_mpx_video/js/cpc-player.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_mpx_video/js/cpc-player.js
@@ -9,33 +9,33 @@
 
     $injector.invoke(['$cookies', 'tveConfig', 'tveModal', 'authService', function ($cookies, tveConfig, tveModal, authService) {
 
-      var nbcu_user_settings, mvpdId;
-
       if ($cookies['nbcu_ap_loginpending']) {
         authService.promise.then(function() {
-          initLivePlayer();
+          initLivePlayer($cookies);
         });
       }
 
-      nbcu_user_settings = JSON.parse($cookies.nbcu_user_settings);
-      mvpdId = nbcu_user_settings.selectedProvider;
-
       if (authService.isAuthN()) {
-        initLivePlayer(mvpdId);
+        initLivePlayer($cookies);
       }
 
     }]);
+  });
 
-    function initLivePlayer(mvpdId) {
-      var contentInitObj = new NBCUniCPC.ContentInitializationObject();
-      contentInitObj.videoId = "LIVE";
-      contentInitObj.mediaPid = "LIVE";
-      contentInitObj.fullEpisode = true;
-      var parameters = new NBCUniCPC.PlayerParameters();
-      parameters.autoPlay = false;
-      parameters.mvpdId = mvpdId;
-      $cpc = NBCUniCPC.load("pdk-player", NBCUniCPC.Account.USA, contentInitObj, parameters);
-    }
-  })
+  function initLivePlayer($cookies) {
 
+    var nbcu_user_settings = JSON.parse($cookies.nbcu_user_settings),
+        mvpdId = nbcu_user_settings.selectedProvider;
+
+    var contentInitObj = new NBCUniCPC.ContentInitializationObject();
+    contentInitObj.videoId = "LIVE";
+    contentInitObj.mediaPid = "LIVE";
+    contentInitObj.fullEpisode = true;
+
+    var parameters = new NBCUniCPC.PlayerParameters();
+    parameters.autoPlay = false;
+    parameters.mvpdId = mvpdId;
+
+    $cpc = NBCUniCPC.load("pdk-player", NBCUniCPC.Account.USA, contentInitObj, parameters);
+  }
 })(angular, jQuery);

9ee387be9806312e1936602fe3df6563041117e7
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-03-27 16:46:12 +0300

    made styles for show-card block

--- docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-shows-queue-promo.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-shows-queue-promo.tpl.php
@@ -5,7 +5,7 @@
 ?>
 <div class="node-wrapper promo">
   <div class="node node-usanetwork-promo show-color-border<?php if (!empty($show_class)): print ' ' . $show_class; endif; ?>">
-    <a class="slide-overlay-btn" href="javascript:void(0)">
+    <a href="javascript:void(0)">
       <div class="asset-img">
         <?php if (!empty($latet_episode_image_url)): ?>
           <img alt="" src="<?php print $latet_episode_image_url; ?>">
@@ -13,16 +13,18 @@
       </div>
       <div class="meta-back"></div>
       <div class="meta-wrapper">
-        <div class="meta-icon play-icon"></div>
-        <div class="title-overlay meta">
-          <?php if (!empty($episode_title)): ?>
-            <div class="title"><?php print $episode_title; ?></div>
-          <?php endif; ?>
-          <?php if (!empty($episode_duration) && !empty($episode_duration)): ?>
-            <div class="additional">
-              <span><?php print t('Latest full episode'); ?></span><?php print $episode_duration; ?>
-            </div>
-          <?php endif; ?>
+        <div class="meta-wrapper-inner">
+          <div class="meta-icon play-icon"></div>
+          <div class="meta">
+            <?php if (!empty($episode_title)): ?>
+              <div class="title"><?php print $episode_title; ?></div>
+            <?php endif; ?>
+            <?php if (!empty($episode_duration) && !empty($episode_duration)): ?>
+              <div class="additional">
+                <span><?php print t('Latest full episode'); ?></span><?php print $episode_duration; ?>
+              </div>
+            <?php endif; ?>
+          </div>
         </div>
       </div>
     </a>

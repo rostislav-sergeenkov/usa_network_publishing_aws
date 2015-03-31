48e0557a02db12c560471d81ce7084484cb8cb84
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-03-31 17:55:56 +0300

    made template for best of

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-best-of.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-best-of.tpl.php
@@ -3,16 +3,60 @@
  *
  */
 ?>
-<div class="example">
-  <?php
-  if (!empty($promos) && is_array($promos)):
-     foreach($promos as $promo):
-        foreach($promo as $key => $element):
-          print '$promo[' . $key . '] ='. $element . '<br>';
-        endforeach;
-      endforeach;
-  endif;
-
-  ?>
-
+<div class="best-of-block show-border">
+  <h2 class="section-title">
+    <span class="section-title-wrapper show-border secondary">Best of chrisley</span>
+  </h2>
+  <?php if (!empty($promos) && is_array($promos)):?>
+  <ul>
+    <?php $second_item = true;?>
+    <?php foreach ($promos as $promo): ?>
+    <?php if ($promo['is_first']): ?>
+    <li class="first">
+      <div class="node node-usanetwork-promo <?php print (!empty($promo['class']))? $promo['class']: ''; ?>">
+        <a href="<?php print (!empty($promo['url']))? $promo['url']: '#'; ?>">
+          <div class="meta-wrapper">
+            <div class="meta-wrapper-inner">
+              <div class="meta-icon <?php print (!empty($promo['icon_type']))? $promo['icon_type']: ''; ?>"></div>
+              <div class="meta">
+                <div class="caption"><?php print (!empty($promo['caption']))? $promo['caption']: ''; ?></div>
+                <div class="title"><?php print (!empty($promo['title']))? $promo['title']: ''; ?></div>
+                <div class="additional"><?php print (!empty($promo['description']))? $promo['description']: ''; ?></div>
+              </div>
+            </div>
+          </div>
+          <div class="asset-img">
+            <?php print (!empty($promo['image']))? $promo['image']: ''; ?>
+          </div>
+        </a>
+      </div>
+    </li>
+      <?php else: ?>
+        <?php if ($second_item == true): ?>
+          <li class="last">
+          <?php $second_item = false; ?>
+        <?php endif; ?>
+          <div class="node node-usanetwork-promo <?php print (!empty($promo['class']))? $promo['class']: ''; ?>">
+            <a href="<?php print (!empty($promo['url']))? $promo['url']: '#'; ?>">
+              <div class="meta-wrapper">
+                <div class="meta-wrapper-inner">
+                  <div class="meta-icon <?php print (!empty($promo['icon_type']))? $promo['icon_type']: ''; ?>"></div>
+                  <div class="meta">
+                    <div class="caption"><?php print (!empty($promo['caption']))? $promo['caption']: ''; ?></div>
+                    <div class="title"><?php print (!empty($promo['title']))? $promo['title']: ''; ?></div>
+                    <div class="additional"><?php print (!empty($promo['description']))? $promo['description']: ''; ?></div>
+                  </div>
+                </div>
+              </div>
+              <div class="asset-img">
+                <?php print (!empty($promo['image']))? $promo['image']: ''; ?>
+              </div>
+            </a>
+          </div>
+      <?php endif; ?>
+      <?php endforeach; ?>
+      <?php if ($second_item == false): ?>
+        </li>
+      <?php endif; ?>
+      <?php endif; ?>
 </div>

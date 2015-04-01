e92e4e5c1f5a84e1c0b9aed2eba8f30521919ca1
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-04-01 16:21:53 +0300

    made styles for show-latest block

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-best-of.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-best-of.tpl.php
@@ -82,3 +82,124 @@
       <?php endif; ?>
       <?php endif; ?>
 </div>
+
+<div class="show-latest-block show-border">
+  <h2 class="section-title">
+    <span class="section-title-wrapper show-border secondary">The latest</span>
+  </h2>
+  <ul class="odd">
+    <li class="first">
+      <div class="node node-usanetwork-promo three-line-video">
+        <a href="/benched/cast/carlos">
+          <div class="meta-wrapper">
+            <div class="meta-wrapper-inner">
+              <div class="meta-icon play-icon"></div>
+              <div class="meta">
+                <div class="caption">Show Name or Violator</div>
+                <div class="title">Title</div>
+                <div class="additional">Description</div>
+              </div>
+            </div>
+          </div>
+          <div class="asset-img" data-picture="" data-alt="" data-class="tile-img">
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/431x350/public/profiles_hero_760x879_carlos.jpg?itok=qxVneIsg"></div>
+            <div data-media="(min-width: 481px)" data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/928x714/public/profiles_hero_760x879_carlos.jpg?itok=r0UgOGdK"></div>
+            <!--[if (IE 8) & (!IEMobile)]>
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/928x714/public/profiles_hero_760x879_carlos.jpg?itok=r0UgOGdK"></div>
+            <![endif]-->
+            <noscript>&lt;img src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/928x714/public/profiles_hero_760x879_carlos.jpg?itok=r0UgOGdK" alt="" title="" /&gt;</noscript>
+            <img alt="" class="tile-img" src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/928x714/public/profiles_hero_760x879_carlos.jpg?itok=r0UgOGdK"></div>
+        </a>
+      </div>
+      <div class="node node-usanetwork-promo three-line-video">
+        <a href="/benched/cast/carlos">
+          <div class="meta-wrapper">
+            <div class="meta-wrapper-inner">
+              <div class="meta-icon play-icon"></div>
+              <div class="meta">
+                <div class="caption">Show Name or Violator</div>
+                <div class="title">Title</div>
+                <div class="additional">Description</div>
+              </div>
+            </div>
+          </div>
+          <div class="asset-img" data-picture="" data-alt="" data-class="tile-img">
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/212x172/public/profiles_hero_760x879_carlos.jpg?itok=gV8L9kB2"></div>
+            <div data-media="(min-width: 481px)" data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+            <!--[if (IE 8) & (!IEMobile)]>
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+            <![endif]-->
+            <noscript>&lt;img src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA" alt="" title="" /&gt;</noscript>
+            <img alt="" class="tile-img" src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+        </a>
+      </div>
+      <div class="node node-usanetwork-promo three-line-video">
+        <a href="/benched/cast/carlos">
+          <div class="meta-wrapper">
+            <div class="meta-wrapper-inner">
+              <div class="meta-icon play-icon"></div>
+              <div class="meta">
+                <div class="caption">Show Name or Violator</div>
+                <div class="title">Title</div>
+                <div class="additional">Description</div>
+              </div>
+            </div>
+          </div>
+          <div class="asset-img" data-picture="" data-alt="" data-class="tile-img">
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/212x172/public/profiles_hero_760x879_carlos.jpg?itok=gV8L9kB2"></div>
+            <div data-media="(min-width: 481px)" data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+            <!--[if (IE 8) & (!IEMobile)]>
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+            <![endif]-->
+            <noscript>&lt;img src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA" alt="" title="" /&gt;</noscript>
+            <img alt="" class="tile-img" src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+        </a>
+      </div>
+    </li>
+    <li class="last">
+      <div class="node node-usanetwork-promo three-line-video">
+        <a href="/benched/cast/carlos">
+          <div class="meta-wrapper">
+            <div class="meta-wrapper-inner">
+              <div class="meta-icon play-icon"></div>
+              <div class="meta">
+                <div class="caption">Show Name or Violator</div>
+                <div class="title">Title</div>
+                <div class="additional">Description</div>
+              </div>
+            </div>
+          </div>
+          <div class="asset-img" data-picture="" data-alt="" data-class="tile-img">
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/212x172/public/profiles_hero_760x879_carlos.jpg?itok=gV8L9kB2"></div>
+            <div data-media="(min-width: 481px)" data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+            <!--[if (IE 8) & (!IEMobile)]>
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+            <![endif]-->
+            <noscript>&lt;img src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA" alt="" title="" /&gt;</noscript>
+            <img alt="" class="tile-img" src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+        </a>
+      </div>
+      <div class="node node-usanetwork-promo three-line-video-large">
+        <a href="/benched/cast/carlos">
+          <div class="meta-wrapper">
+            <div class="meta-wrapper-inner">
+              <div class="meta-icon play-icon"></div>
+              <div class="meta">
+                <div class="caption">Show Name or Violator</div>
+                <div class="title">Title</div>
+                <div class="additional">Description</div>
+              </div>
+            </div>
+          </div>
+          <div class="asset-img" data-picture="" data-alt="" data-class="tile-img">
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/212x172/public/profiles_hero_760x879_carlos.jpg?itok=gV8L9kB2"></div>
+            <div data-media="(min-width: 481px)" data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+            <!--[if (IE 8) & (!IEMobile)]>
+            <div data-src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+            <![endif]-->
+            <noscript>&lt;img src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA" alt="" title="" /&gt;</noscript>
+            <img alt="" class="tile-img" src="http://usanetwork.local.usanetwork.com/sites/usanetwork/files/public/styles/457x351/public/profiles_hero_760x879_carlos.jpg?itok=gT-9V7GA"></div>
+        </a>
+      </div>
+    </li>
+  </ul></div>

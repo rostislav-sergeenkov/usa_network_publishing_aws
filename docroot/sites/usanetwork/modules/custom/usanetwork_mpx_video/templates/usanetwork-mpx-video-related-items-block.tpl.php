1421ef46d414dbbb843fad12017e3990a1b459e1
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-04-07 17:15:49 +0300

    made changes for related block

--- docroot/sites/usanetwork/modules/custom/usanetwork_mpx_video/templates/usanetwork-mpx-video-related-items-block.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_mpx_video/templates/usanetwork-mpx-video-related-items-block.tpl.php
@@ -2,7 +2,7 @@
 /*add class "first" for first li and class "last" for last li */
 ?>
 <?php if (!empty($ad)): ?>
-  <div class="latest-ad"></div>
+  <div class="related-ad"></div>
 <?php endif; ?>
 <ul>
   <?php if (!empty($related_items)): ?>

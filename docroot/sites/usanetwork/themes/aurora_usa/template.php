387e944941038bf9a009628b0fff327811ee703c
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-04-08 10:01:51 +0300

    changed viewport settings

--- docroot/sites/usanetwork/themes/aurora_usa/template.php
+++ docroot/sites/usanetwork/themes/aurora_usa/template.php
@@ -53,7 +53,7 @@ function aurora_usa_preprocess_html(&$vars) {
     '#tag' => 'meta',
     '#attributes' => array(
       'name' => 'viewport',
-      'content' => 'width=device-width, initial-scale=1, minimal-ui',
+      'content' => 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
     ),
   );
   drupal_add_html_head($viewport, 'viewport');

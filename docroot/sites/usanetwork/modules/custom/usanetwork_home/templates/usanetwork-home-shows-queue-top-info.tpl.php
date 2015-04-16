04276e44efa46e42ef12bc0770953a3d5db5049a
YuryKaminski <Yury_Kaminski@epam.com>
2015-04-16 17:11:38 +0300

    TA134-Print-tune-in-display-date

--- docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-shows-queue-top-info.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_home/templates/usanetwork-home-shows-queue-top-info.tpl.php
@@ -8,9 +8,9 @@
     <?php if (!empty($show_title)): ?>
       <div class="title"><?php print $show_title; ?></div>
     <?php endif; ?>
-    <?php if (!empty($schedule_day) && !empty($schedule_time)): ?>
+    <?php if (!empty($tune_in_date)): ?>
       <div class="time">
-        <span><?php print $schedule_day; ?></span><?php print $schedule_time; ?>
+        <span><?php print $tune_in_date; ?></span>
       </div>
     <?php endif; ?>
   </div>

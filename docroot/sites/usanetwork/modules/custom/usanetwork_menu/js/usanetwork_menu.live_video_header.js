98c80d65d990ab291a9edaec973b2def1b961727
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-04-27 11:14:11 +0300

    made parse ajax for live page header

--- docroot/sites/usanetwork/modules/custom/usanetwork_menu/js/usanetwork_menu.live_video_header.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_menu/js/usanetwork_menu.live_video_header.js
@@ -16,7 +16,20 @@
 
             if (typeof message.on_now_vars != 'undefined') {
               var on_now_vars = JSON.parse(message.on_now_vars);
+              headerBlock.find('.show-name a').attr('href', on_now_vars.show_url);
+              headerBlock.find('.show-name a span').text(on_now_vars.show_name);
+              headerBlock.find('h1 a').text(on_now_vars.episode.title);
+              headerBlock.find('.info-tab .asset-img img').attr('src' , on_now_vars.episode.image_url);
+              headerBlock.find('.info-tab .caption').text(on_now_vars.episode.video_type);
+              headerBlock.find('.info-tab .title').text(on_now_vars.episode.title);
+              var additional = '<span>S' + on_now_vars.episode.season_number + ' episode ' + on_now_vars.episode.episode_number + '</span> ' + on_now_vars.episode.running_time;
+              headerBlock.find('.info-tab .additional').html(additional);
+              headerBlock.find('.info-tab .description').text(on_now_vars.episode.description);
             }
+
+          },
+          error: function () {
+            console.info('error');
           }
         });
       }

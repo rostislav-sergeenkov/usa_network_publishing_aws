836dd8feead10d2b17ed4cc37b14b382862043ce
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-05-07 09:40:52 +0300

    added styles for show video landing

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-landing-videos.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-landing-videos.tpl.php
@@ -3,13 +3,7 @@
  *
  */
 ?>
-<div class="videos-landing-page-container ajax-load-block"
-  <?php if (!empty($show_nid)): print ' data-show-nid="' . $show_nid . '"'; endif;?>
-  <?php if (!empty($items_per_page_limit)): print ' data-show-items-limit="' . $items_per_page_limit . '"'; endif; ?>
-  <?php print ' data-filter-tid="' . $filter_tid . '"'; ?>
-  <?php if (!empty($sorting_column)): print ' data-sorting-column="' . $sorting_column . '"'; endif; ?>
-  <?php if (!empty($sorting_order)): print ' data-sorting-order="' . $sorting_order . '"'; endif; ?>
-  >
+<div class="videos-landing-page-container">
   <h2 class="section-title">
     <span class="section-title-wrapper show-border secondary"><?php print t('All videos'); ?></span>
   </h2>
@@ -17,10 +11,10 @@
     <div class="all-items-filter item-filter">
       <?php if (!empty($video_filters)): ?>
         <div class="filter-label">test</div>
-        <ul class="filter-menu">
+        <ul class="filter-menu transform-filter">
           <?php foreach ($video_filters as $video_filter): ?>
             <li class="filter-item<?php if ($video_filter['active'] == TRUE): print ' active'; endif; ?>">
-              <a href="<?php print $video_filter['url']; ?>" data-type="<?php print $video_filter['tid']; ?>">
+              <a href="<?php print $video_filter['url']; ?>" data-type="<?php print $video_filter['tid']; ?>" class="no-ajax">
                 <span class="title"><?php print $video_filter['name']; ?></span> <span class="items-in">(<?php print $video_filter['items_count']; ?>)</span>
               </a>
             </li>
@@ -34,7 +28,7 @@
         <ul class="filter-menu">
           <?php foreach ($video_sorters as $video_sorter): ?>
             <li class="filter-item sorter-item<?php if (!empty($video_sorter['order'])): print ' order-' . $video_sorter['order']; endif; ?><?php if ($video_sorter['active'] == TRUE): print ' active'; endif; ?>">
-              <a href="<?php print $video_sorter['url']; ?>" data-type="<?php print $video_sorter['data_type']; ?>">
+              <a href="<?php print $video_sorter['url']; ?>" data-type="<?php print $video_sorter['data_type']; ?>" class="no-ajax">
                 <span class="title"><?php print $video_sorter['title']; ?></span>
               </a>
             </li>
@@ -43,13 +37,17 @@
       <?php endif; ?>
     </div>
   </div>
-  <div class="video-items-blocks show-border ajax-load-block"><!--this tag with class "ajax-load-block" for data-attributes for ajax load><-->
+  <div class="video-items-blocks show-border ajax-load-block"
+  <?php if (!empty($show_nid)): print ' data-show-nid="' . $show_nid . '"'; endif;?>
+  <?php if (!empty($items_per_page_limit)): print ' data-show-items-limit="' . $items_per_page_limit . '"'; endif; ?>
+  <?php print ' data-filter-tid="' . $filter_tid . '"'; ?>
+  <?php if (!empty($sorting_column)): print ' data-sorting-column="' . $sorting_column . '"'; endif; ?>
+  <?php if (!empty($sorting_order)): print ' data-sorting-order="' . $sorting_order . '"'; endif; ?>>
     <?php if (!empty($videos_block)): ?>
       <?php print $videos_block; ?>
     <?php endif; ?>
-    <?php if (!empty($load_more_link)): ?>
+
       <div class="load-more-link"><a href="javascript:void(0)"><?php print t('Load more'); ?></a></div>
-    <?php endif; ?>
+
   </div>
-  <div class="load-more-link"><a href="javascript:void(0)"><?php print t('Load more'); ?></a></div>
 </div>

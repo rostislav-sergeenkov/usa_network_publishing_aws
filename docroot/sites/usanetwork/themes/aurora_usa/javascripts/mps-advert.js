f7cdd0d0f27c547a2d0de5dd30e062128823b255
Nikolai Verbitskii <verbitskiinikolai@gmail.com>
2015-05-11 14:48:20 +0300

    redesign: mps quiz

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/mps-advert.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/mps-advert.js
@@ -85,10 +85,6 @@
     attach: function (context, settings) {
       // init ad
       //Drupal.behaviors.mpsAdvert.consumSidebar();
-
-      if('body'.hasClass('node-type-quiz')) {
-        Drupal.behaviors.mpsAdvert.mpsLoadAd('#topbanner', Drupal.behaviors.mpsAdvert.mpsNameAD.topbox);
-      }
     }
   };
 }(jQuery));

fc5e63c671dcb391d35f8c6dc1771f8c8f04a229
Nikolai Verbitskii <verbitskiinikolai@gmail.com>
2015-05-11 18:22:04 +0300

    redesign: sponsorship main-menu full-episodes

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/mps-sponsorship.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/mps-sponsorship.js
@@ -22,22 +22,29 @@
 
     attach: function (context, settings) {
 
+      // main menu
+      var nodePromo = $('header .full-episodes-list .node-usanetwork-promo');
+      nodePromo.each(function (index, element) {
+        var sponsor = $(element).find('.sponsored');
+        if(sponsor.length) {
+          var fileId =sponsor.data('mpspath'),
+              style = Drupal.behaviors.mpsSponsorship.styleName.dark_stacked;
+
+          Drupal.behaviors.mpsSponsorship.insertLogo(mps._select(sponsor), style, fileId);
+        }
+      });
+
       // page-videos
       if($('.page-videos')) {
-
         var carouselBlock = $('.carousel-wrapper .carousel-block');
-
         carouselBlock.each(function (index, element) {
-
           var sponsor = $(element).find('.sponsored');
-
           if(sponsor.length) {
             var fileId =sponsor.data('mpspath'),
                 style = Drupal.behaviors.mpsSponsorship.styleName.dark_stacked;
 
-            mps.usa.insertLogo(mps._select(sponsor), style, fileId);
+            Drupal.behaviors.mpsSponsorship.insertLogo(mps._select(sponsor), style, fileId);
           }
-
         });
       }
     }

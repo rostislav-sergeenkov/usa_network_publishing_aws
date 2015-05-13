e1714723cbab3d9a6bcc049326d5cbc4d7194441
Ihar Karavatsky <Ihar_Karavatski@epam.com>
2015-05-13 14:52:04 +0300

    Microsite / Graceland Catchup -- Fix Endcard.

---
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/gracelandcatchup/js/ms_video_endcard.js
@@ -0,0 +1,38 @@
+(function ($) {
+	Drupal.usanetwork_video_endcard = {
+		'GetAnchors': function (data) {
+			var next_url_splited = data.data.next_url.split('/'),
+				anchor = next_url_splited[next_url_splited.length - 2],
+				dataVideoUrl = next_url_splited[next_url_splited.length - 1],
+				anchorFull = Drupal.settings.microsites_settings.base_path + '/' + anchor + '/' + dataVideoUrl;
+
+      return [anchor, anchorFull, dataVideoUrl];
+    },
+    'SelectActiveThumb': function(data) {
+      $('#block-usanetwork-mpx-video-usa-mpx-video-views .item-list ul li').removeClass('active');
+      $('#block-usanetwork-mpx-video-usa-mpx-video-views .item-list ul li[data-video-url="' +
+      Drupal.usanetwork_video_endcard.GetAnchors(data)[2] + '"]').addClass('active');
+    },
+    'OnCountdownEnd': function(data) {
+      Drupal.behaviors.ms_videos.micrositeSetVideoPlayer(true, null, data);
+      Drupal.behaviors.ms_global.changeUrl(Drupal.usanetwork_video_endcard.GetAnchors(data)[0], Drupal.usanetwork_video_endcard.GetAnchors(data)[1]);
+      Drupal.usanetwork_video_endcard.SelectActiveThumb(data);
+    },
+    'OnYmalitemnewClick': function(data) {
+      Drupal.behaviors.ms_videos.micrositeSetVideoPlayer(true, null, data);
+      Drupal.behaviors.ms_global.changeUrl(Drupal.usanetwork_video_endcard.GetAnchors(data)[0], Drupal.usanetwork_video_endcard.GetAnchors(data)[1]);
+      Drupal.usanetwork_video_endcard.SelectActiveThumb(data);
+    }
+  };
+
+	$(window).load(function () {
+		if (typeof tpController !== 'undefined') {
+			tpController.addEventListener('OnEndcardCountdownEnd', Drupal.usanetwork_video_endcard.OnCountdownEnd);
+			tpController.addEventListener('OnYmalitemnewClick', Drupal.usanetwork_video_endcard.OnYmalitemnewClick);
+		}
+		var previewItem = $('#block-usanetwork-mpx-video-usa-mpx-video-views .item-list ul li, #show-aspot-microsite .aspot-link');
+		previewItem.click(function (e) {
+			tpController.addEventListener('OnEndcardCountdownEnd', Drupal.usanetwork_video_endcard.OnCountdownEnd);
+		});
+	});
+})(jQuery);

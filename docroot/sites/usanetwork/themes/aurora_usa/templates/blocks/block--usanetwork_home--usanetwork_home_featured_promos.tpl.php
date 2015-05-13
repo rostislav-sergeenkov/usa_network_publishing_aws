20cb363c976bac9f7eff08bf920113cebde2c857
maksim-tarhunakau <--global>
2015-05-13 17:15:25 +0300

    Fixing

--- docroot/sites/usanetwork/themes/aurora_usa/templates/blocks/block--usanetwork_home--usanetwork_home_featured_promos.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/templates/blocks/block--usanetwork_home--usanetwork_home_featured_promos.tpl.php
@@ -1,7 +1,11 @@
-<div class="featured-block carousel-block carousel-block-left"<?php print !empty($attributes_array['data-url']) ? (' data-url="' . $attributes_array['data-url'] . '"') : ''; ?>>
-  <?php print $content; ?>
-  <a href="javascript:void(0)" class="more-button more">
-    <span class="more-text"><?php print t('More action packed moments'); ?></span>
-    <span class="close-text"><?php print t('Close'); ?></span>
-  </a>
-</div>
+<?php if (!empty($content)): ?>
+  <div class="featured-block carousel-block carousel-block-left"<?php print !empty($attributes_array['data-url']) ? (' data-url="' . $attributes_array['data-url'] . '"') : ''; ?>>
+    <?php print $content; ?>
+    <?php if (!empty($items_count) && $items_count > 3): ?>
+      <a href="javascript:void(0)" class="more-button more">
+        <span class="more-text"><?php print t('More action packed moments'); ?></span>
+        <span class="close-text"><?php print t('Close'); ?></span>
+      </a>
+    <?php endif; ?>
+  </div>
+<?php endif; ?>

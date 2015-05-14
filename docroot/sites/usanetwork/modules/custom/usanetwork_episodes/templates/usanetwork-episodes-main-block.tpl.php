20df4df8197828e7eb5b47ceaab17aaf478aeaa4
YuryKaminski <Yury_Kaminski@epam.com>
2015-05-14 16:55:26 +0300

    TA612-Episode-tv-cons-main-block

---
+++ docroot/sites/usanetwork/modules/custom/usanetwork_episodes/templates/usanetwork-episodes-main-block.tpl.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* $sharebar - html placeholder for JS, for sharebar rendering.
+* $episode_title - string
+* $body - html
+* $season_number - integer
+* $episode_number - integer
+* $episode_video_link - link for "Wath the episode" button.
+* $image_mobile - URL for img.
+* $image_desktop - URL for img.
+* $air_date_text - string
+*/
+?>
+<?php print $sharebar; ?> <br>
+<?php print $episode_title; ?> <br>
+<?php print $body; ?> <br>
+<?php print $season_number; ?> <br>
+<?php print $episode_number; ?> <br>
+<?php print $episode_video_link; ?> <br>
+<?php print $image_mobile; ?> <br>
+<?php print $image_desktop; ?> <br>
+<?php print $air_date_text; ?> <br>

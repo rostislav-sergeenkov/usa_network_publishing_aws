50b9e9f9df6633e0e13592519e6a5c01f4588ee2
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-05-19 11:09:30 +0300

    add js for episode-landing

--- docroot/sites/usanetwork/modules/custom/usanetwork_episodes/js/usanetwork_episodes.autoloader.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_episodes/js/usanetwork_episodes.autoloader.js
@@ -4,18 +4,12 @@
       var showNid = $('.episode-landing-list-items-seasons').data('show-nid');
 
       if (showNid > 0) {
-        var seasonBlocks = $('.episode-landing-list-items-season');
+        Drupal.settings.lastSeasonNumber = $('.episode-landing-list-items-seasons .episode-landing-list-items-season').last().data('season-number');
 
-        if (Array.isArray(seasonBlocks)) {
-
+        if (Drupal.settings.lastSeasonNumber > 1) {
+          Drupal.settings.newSeasonNumber = Drupal.settings.lastSeasonNumber - 1;
         }
-        else {
-          Drupal.settings.lastSeasonNumber = seasonBlocks.data('season-number');
 
-          if (Drupal.settings.lastSeasonNumber > 1) {
-            Drupal.settings.newSeasonNumber = Drupal.settings.lastSeasonNumber - 1;
-          }
-        }
         console.log(window.location);
         var serviceUrl = '/ajax/usanetwork-tv-show-episodes/get-related/' + showNid + '/' + Drupal.settings.newSeasonNumber;
 
@@ -25,7 +19,17 @@
           url: serviceUrl,
           dataType: 'json',
           success: function (data) {
-            $('.episode-landing-list-items-seasons').append(data.rendered);
+            console.info(data);
+            $('.ajax-load-block .load-more-link').before(data.rendered);
+            $('.ajax-load-block .load-more-link .load-more-loader').remove();
+
+            if (typeof window.picturefill != 'undefined') {
+              window.picturefill();
+            }
+
+            if (Drupal.settings.newSeasonNumber != 1) {
+              $('.ajax-load-block .load-more-link a').removeClass('disabled');
+            }
           },
           error: function () {
             console.info('error');
@@ -34,9 +38,7 @@
       }
     },
     attach: function(context, settings) {
-      $(document).ready(function() {
-        //@TODO: write action trigger for Drupal.behaviors.usanetwork_episodes_autoloader.loadPageItems();
-      });
+
     }
   }
 })(jQuery);

7914722fe730e87ac0491f6bb60fd7998ecb5fd2
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-05-21 15:37:09 +0300

    fixed #844

--- docroot/sites/usanetwork/modules/custom/usanetwork_episodes/templates/usanetwork-episodes-gallery-rec.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_episodes/templates/usanetwork-episodes-gallery-rec.tpl.php
@@ -4,5 +4,11 @@
 * $block_title - string
 */
 ?>
-<?php print $block_title; ?><br>
-<?php print $gallery_html; ?>
+<div class="gallery-recap-block">
+  <h2 class="section-title">
+    <span class="section-title-wrapper show-border secondary"><?php print !empty($block_title) ? $block_title : ''; ?></span>
+  </h2>
+  <?php print $gallery_html; ?>
+</div>
+
+

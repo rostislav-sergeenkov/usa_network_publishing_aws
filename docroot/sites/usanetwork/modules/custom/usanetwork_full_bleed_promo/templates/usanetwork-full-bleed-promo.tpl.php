b88736c87f82a70b88d80e5457be88a6c3c5ff56
Nikolai Verbitskii <verbitskiinikolai@gmail.com>
2015-05-21 09:37:33 +0300

    fix for full bleed promo

--- docroot/sites/usanetwork/modules/custom/usanetwork_full_bleed_promo/templates/usanetwork-full-bleed-promo.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_full_bleed_promo/templates/usanetwork-full-bleed-promo.tpl.php
@@ -6,29 +6,31 @@
 
 <?php if (!empty($desktop_url) || !empty($mobile_url)): ?>
   <div id="full-bleed-promo" class="show-border">
-    <div class="block-advert">
-      <?php if (!empty($advert_block)): ?>
-        <?php print $advert_block; ?>
-      <?php endif; ?>
-    </div>
-    <div class="block-content">
-      <?php if (!empty($content_id)) : ?>
-        <div class="sponsored" data-mpspath="<?php print $content_id; ?>" data-scalemps="1"></div>
-      <?php else : ?>
-        <?php if (!empty($url)): ?>
-          <a href="<?php print $url; ?>">
-        <?php else: ?>
-          <a class="empty-link">
+    <div class="full-bleed-wrapper">
+      <div class="block-advert">
+        <?php if (!empty($advert_block)): ?>
+          <?php print $advert_block; ?>
         <?php endif; ?>
-          <?php if (!empty($desktop_url)): ?>
-            <div class="block-content-image hidden desktop-content" data-url="<?php print $desktop_url;?>"></div>
+      </div>
+      <div class="block-content">
+        <?php if (!empty($content_id)) : ?>
+          <div class="sponsored" data-mpspath="<?php print $content_id; ?>" data-scalemps="1"></div>
+        <?php else : ?>
+        <?php if (!empty($url)): ?>
+        <a href="<?php print $url; ?>">
+          <?php else: ?>
+          <a class="empty-link">
+            <?php endif; ?>
+            <?php if (!empty($desktop_url)): ?>
+              <div class="block-content-image hidden desktop-content" data-url="<?php print $desktop_url;?>"></div>
+            <?php endif; ?>
+            <?php if (!empty($mobile_url)): ?>
+              <div class="block-content-image hidden mobile-content" data-url="<?php print $mobile_url; ?>"></div>
+            <?php endif; ?>
+            <img class="responsive-image" />
+          </a>
           <?php endif; ?>
-          <?php if (!empty($mobile_url)): ?>
-            <div class="block-content-image hidden mobile-content" data-url="<?php print $mobile_url; ?>"></div>
-          <?php endif; ?>
-          <img class="responsive-image" />
-        </a>
-      <?php endif; ?>
+      </div>
     </div>
   </div>
 <?php endif; ?>

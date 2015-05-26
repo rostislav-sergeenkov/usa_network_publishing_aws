dd5bfc579b8f9e27682760d3f6d0fcbc3dbd5a4c
Nikolai Verbitskii <verbitskiinikolai@gmail.com>
2015-05-26 15:13:35 +0300

    fix video rotation

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/consumptionator-video.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/consumptionator-video.js
@@ -15,26 +15,9 @@
       var iframe_height = current_iframe.width()*9/16;
       current_iframe.height(iframe_height);
 
-      $(window).bind('resize', function () {
-        iframe_height = current_iframe.width()*9/16;
-        current_iframe.height(iframe_height);
-        var current_src = current_iframe.attr('src');
-        current_iframe.attr('src', '');
-        current_iframe.attr('src', current_src);
-      });
-
-      Drupal.behaviors.consumptionator_video.mobileModal();
-
-      //init custom scrollbar for social block
-      //var container = $('#block-usanetwork-mpx-video-usanetwork-social-content .content .show-border');
-
-      //if(container.length > 0) {
-      //  $(container).mCustomScrollbar({
-      //    axis: "y",
-      //    theme: "light",
-      //    scrollEasing: "easeOut"
-      //  });
-      //}
+      if($('.consum-sidebar .full-video').length > 0) {
+        Drupal.behaviors.consumptionator_video.mobileModal();
+      }
     }
   };
 

93d45fade964e2b24681540b8851d07209b2ddf7
Nikolai Verbitskii <verbitskiinikolai@gmail.com>
2015-05-28 17:49:06 +0300

    new init player

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/sign_in_out.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/sign_in_out.js
@@ -12,13 +12,11 @@
       if ($cookies['nbcu_ap_loginpending']) {
         initHeader();
         authService.promise.then(function () {
-          Drupal.behaviors.consumptionator_video.initPlayerBind();
         });
       }
 
       if (authService.isAuthN()) {
         initHeader();
-        Drupal.behaviors.consumptionator_video.initPlayerBind();
       }
 
     }]);

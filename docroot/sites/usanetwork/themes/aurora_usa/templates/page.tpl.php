c649e75530282ca5f820298a24caa541bec8735b
YuryKaminski <Yury_Kaminski@epam.com>
2015-06-01 15:45:36 +0300

    TA976-Remove-old-tool-bar

--- docroot/sites/usanetwork/themes/aurora_usa/templates/page.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/templates/page.tpl.php
@@ -87,27 +87,7 @@
     <!-- /header -->
   </header>
 
-  <!-- TOP TITLE AND TOOLS BAR -->
-  <div id="utilities" class="clearfix <?php print $util_classes; ?>">
-    <div id="utilities-wrap" class="clearfix">
-    <?php if ($page['search']) : ?>
-      <div class="utilties-main-group">
-    <?php endif; ?>
-    <?php print render($title_prefix); ?>
-    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
-    <?php if ($page['catchall_seo_title']): ?><h2 class="title" id="page-title"><?php print $page['catchall_seo_title']; ?></h2><?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php if ($page['head_show']): ?><div id="head-show"><?php print render($page['head_show']); ?></div><?php endif; ?>
-    <?php if ($page['head_general']): ?><div id="head-general"><?php print render($page['head_general']); ?></div><?php endif; ?>
-    <?php if ($page['sponsored']): ?><div id="head-sponsored"><?php print render($page['sponsored']); ?></div><?php endif; ?>
-    <?php if ($page['search']): ?>
-      </div>
-    <?php endif; ?>
-    <?php if ($page['search']): ?><div id="head-search"><?php print render($page['search']); ?></div><?php endif; ?>
-    </div>
-  </div>
 <?php endif; ?>
-<!-- /TOP TITLE AND TOOLS BAR -->
 
 <div class="usa-wrap"><?php // this wrapper is intended for ad rails response please do not theme against it ?>
   <!-- MAIN CONTENT -->

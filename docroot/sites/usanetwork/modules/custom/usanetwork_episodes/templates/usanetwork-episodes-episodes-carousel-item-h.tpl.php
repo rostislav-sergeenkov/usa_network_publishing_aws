4005c2b548ddb00e6ad0763a7a30ed3f71cb3ad4
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-06-12 17:00:28 +0300

    fixed #939

--- docroot/sites/usanetwork/modules/custom/usanetwork_episodes/templates/usanetwork-episodes-episodes-carousel-item-h.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_episodes/templates/usanetwork-episodes-episodes-carousel-item-h.tpl.php
@@ -5,7 +5,18 @@
     <?php if (!empty($url)): ?>
       <a href="<?php print $url; ?>">
         <?php if (!empty($image)): ?>
-          <div class="asset-img"><img src="<?php print $image; ?>" alt="" title="<?php print $title; ?>" /></div>
+          <div class="asset-img" data-picture data-alt="" data-class="tile-img">
+            <?php if (!empty($image_mobile)): ?>
+              <div data-src="<?php print $image_mobile; ?>"></div>
+            <?php endif; ?>
+            <?php if (!empty($image)): ?>
+              <div data-media="(min-width: 641px)" data-src="<?php print $image; ?>"></div>
+              <!--[if (IE 8) & (!IEMobile)]>
+              <div data-src="<?php print $image_url; ?>"></div>
+              <![endif]-->
+              <noscript><img src="<?php print $image; ?>" width="633" height="356" alt="" title="" /></noscript>
+            <?php endif; ?>
+          </div>
         <?php endif; ?>
         <div class="meta-wrapper">
           <div class="meta-back"></div>

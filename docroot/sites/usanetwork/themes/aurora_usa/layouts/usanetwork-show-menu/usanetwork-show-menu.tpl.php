4fb6ab8fda6294c29bded77899b680fec121e934
Nikolai Verbitskii <verbitskiinikolai@gmail.com>
2015-06-12 09:40:35 +0300

    prof icon style

--- docroot/sites/usanetwork/themes/aurora_usa/layouts/usanetwork-show-menu/usanetwork-show-menu.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/layouts/usanetwork-show-menu/usanetwork-show-menu.tpl.php
@@ -7,7 +7,7 @@
     <div class="nav-bar-tabs">
       <?php print $content['reg_menu']; ?>
     </div>
-    <div id="tve-help-link">
+    <div id="tve-help-link-wrapper">
       <?php print $content['profile']; ?>
     </div>
     <div class="main-menu-open"><a href="#" class="link-color-reset"></a></div>

89b476683c5b0a2ba50993d27ab2de1bea85824d
NikolaiVerbitskiicd <verbitskiinikolai@gmail.com>
2015-07-02 10:59:21 +0300

    fix sponsored for five-promo-item-block

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/mps-sponsorship.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/mps-sponsorship.js
@@ -17,6 +17,7 @@
         var sponsoredBlock = $(element).find('.sponsored');
         if (sponsoredBlock.length) {
           var fileId = sponsoredBlock.data('mpspath');
+          sponsoredBlock.addClass('active');
           Drupal.behaviors.mpsSponsorShip.insertLogo(mps._select(sponsoredBlock), styleBlock, fileId);
         }
       });

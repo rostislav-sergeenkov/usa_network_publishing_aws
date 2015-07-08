6b598c0feeec9cdc59388c304759d64942c6a45a
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-07-08 15:12:14 +0300

    made changes for all movies block

--- docroot/sites/usanetwork/modules/custom/usanetwork_movie/templates/usanetwork-movies-schedule.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_movie/templates/usanetwork-movies-schedule.tpl.php
@@ -3,7 +3,7 @@
  *
  */
 ?>
-<div class="landing-page-container all-shows-landing-page-container page-schedule schedule-wrapper">
+<div class="landing-page-container page-schedule schedule-wrapper">
   <h2 class="section-title">
     <span class="section-title-wrapper show-border secondary"><?php print !empty($block_title) ? $block_title : t('Movies Schedule'); ?></span>
   </h2>

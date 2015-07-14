76b9812e7073f3601592626af9e6fbfdb0030ab1
NikolaiVerbitskiicd <verbitskiinikolai@gmail.com>
2015-07-14 16:55:24 +0300

    show card animation fix

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/variables-and-functions.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/variables-and-functions.js
@@ -91,4 +91,33 @@ $(document).ready(function(){
 
 });
 
+// detect browser
+function browserDetect() {
+
+  var browserName = '';
+
+  //Check if browser is IE or not
+  if (navigator.userAgent.search("MSIE") >= 0) {
+    browserName = 'msie';
+  }
+  //Check if browser is Chrome or not
+  else if (navigator.userAgent.search("Chrome") >= 0) {
+    browserName = 'chrome';
+  }
+  //Check if browser is Firefox or not
+  else if (navigator.userAgent.search("Firefox") >= 0) {
+    browserName = 'firefox';
+  }
+  //Check if browser is Safari or not
+  else if (navigator.userAgent.search("Safari") >= 0 && navigator.userAgent.search("Chrome") < 0) {
+    browserName = 'safari';
+  }
+  //Check if browser is Opera or not
+  else if (navigator.userAgent.search("Opera") >= 0) {
+    browserName = 'opera';
+  }
+
+  return browserName;
+}
+
 

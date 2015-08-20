60c0d3371fbaa37b4e147405baba82fb617973a2
Donna Vaughan <donna.vaughan@nbcuni.com>
2015-08-20 09:47:43 -0500

    mrrobot / microsite -- adding infographic back in with new image

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/mrrobot/templates/usanetwork-microsite-mrrobot-page-characters.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/mrrobot/templates/usanetwork-microsite-mrrobot-page-characters.tpl.php
@@ -42,11 +42,9 @@ $themePath = '/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/mr
 <?php endif; ?>
 
 <div id="characters-container">
-<? /* ?>
   <div id="character-infographic">
-    <a><img src="<?php print $themePath; ?>/images/MrRobot_Infographic_Cover_v1.jpg" alt="Mr. Robot infographic cover image"></a>
+    <a><img src="<?php print $themePath; ?>/images/MrRobot_CharacterMap_preview_1601.jpg" alt="Mr. Robot infographic preview"></a>
   </div>
-<? */ ?>
   <?php if (!empty($people)): ?>
   <div id="character-wrapper">
     <!-- character navigation -->

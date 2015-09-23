63de39deb37e4969ea3b1fcef9c47e490f453839
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-09-23 17:49:55 +0300

    fixed #1522

--- docroot/sites/usanetwork/modules/custom/usanetwork_quiz/js/quiz_edit.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_quiz/js/quiz_edit.js
@@ -4,12 +4,58 @@
 
     attach: function(context, settings) {
 
+      function checkHide(){
+        var hide = true;
+        $('.question-handling').each(function () {
+          if(!$(this).hasClass('hide')){
+            hide = false;
+            return false;
+          }
+        });
+        return hide;
+      }
+      function checkShow(){
+        var show = true;
+        $('.question-handling').each(function () {
+          if($(this).hasClass('hide')){
+            show = false;
+            return false;
+          }
+        });
+        return show;
+      }
+
+      function collapseTest(){
+        if(checkHide()) {
+          if(!$('.questions-collapse').hasClass('hide')){
+            $('.questions-collapse').addClass('hide');
+          }
+        } else {
+          if($('.questions-collapse').hasClass('hide')){
+            $('.questions-collapse').removeClass('hide');
+          }
+        }
+      }
+      function expandTest(){
+        if(checkShow()) {
+          if(!$('.questions-expand').hasClass('hide')){
+            $('.questions-expand').addClass('hide');
+          }
+        } else {
+          if($('.questions-expand').hasClass('hide')){
+            $('.questions-expand').removeClass('hide');
+          }
+        }
+      }
+
       $('.question-expand').click(function(e){
         e.preventDefault();
         var handling = $(this).closest('.question-handling');
         if(handling.hasClass('hide')){
           handling.removeClass('hide');
         }
+        collapseTest();
+        expandTest();
       });
 
       $('.question-collapse').click(function(e){
@@ -18,6 +64,8 @@
         if(!handling.hasClass('hide')){
           handling.addClass('hide');
         }
+        collapseTest();
+        expandTest();
       });
 
       $('.questions-expand').click(function(e){

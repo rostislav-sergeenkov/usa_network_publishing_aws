1363c40a0d02a4c54d4c55aab3fd133b810f62cd
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-09-24 17:06:14 +0300

    made styles changes for top3

--- docroot/sites/usanetwork/modules/custom/usanetwork_top3/templates/usanetwork-top3-main-block.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_top3/templates/usanetwork-top3-main-block.tpl.php
@@ -1,9 +1,12 @@
+<?php
+
+?>
 <div id="slider-container">
 
   <div class="slider-wrapper">
     <?php foreach ($slides as $key => $slide) : ?>
       <div class="slide">
-        <div class="play-button">play-button</div>
+        <div class="play-button"></div>
         <div class="slide-content">
           <div class="slide-content-inner" data-slide-id="<?php print $key; ?>">
             <div class="img-wrapper">
@@ -21,15 +24,34 @@
   </div>
 
 
-  <div class="counter">
+  <div id="counter">
     <span class="current-slide"></span>
     /
     <span class="total-slides"></span>
   </div>
 
-  <div class="info">info</div>
+  <div id="info"></div>
+  <div id="info-close"></div>
   <div class="container-message">This slide has already chosen!</div>
-  <div id="compare-button">Compare</div>
+  <div id="info-block">
+    <div class="top-text">Select your top 3 favorite Todd-isms and then share them with your friends and family!</div>
+    <div class="color-text show-color show-font">How it works</div>
+    <div class="bottom-text">Use the icon shown below to click & drag the thumbnail into the ranking boxes. Fill all 3 boxes and share your personalized page.</div>
+    <div class="help-image"></div>
+  </div>
+  <div id="share-block-preview">
+    <div class="first-text-line">Click and drag the thumbnails over a different box to change the order</div>
+    <div class="first">
+
+    </div>
+    <div class="second">
+
+    </div>
+    <div class="third">
+
+    </div>
+    <div class="second-text-line">Generate your custom share link</div>
+  </div>
   <div id="share-block">
     <div class="first">
 
@@ -42,13 +64,12 @@
 
       </div>
     </div>
-    <div id="share-button">Share</div>
   </div>
   <div id="share-img"></div>
-  <div class="prev control-button">prev</div>
-  <div class="next control-button">next</div>
+  <div class="prev control-button"></div>
+  <div class="next control-button"></div>
 
-  <div id="drop-area" class="drop-area show" style="z-index: 1000; margin: 120px 0px 0px;">
+  <div id="drop-area" class="drop-area show">
     <div class="numGroup" style="display: none;">
       <ul>
         <li class="num1">1</li>
@@ -57,13 +78,22 @@
       </ul>
     </div>
     <div class="drag-group">
-      <div class="first-wrapper"></div>
-      <div class="second-wrapper"></div>
-      <div class="third-wrapper"></div>
-      <div class="drop-area__item" id="one"></div>
-      <div class="drop-area__item" id="two"></div>
-      <div class="drop-area__item" id="three"></div>
+      <div class="drop-area__item show-color" id="one"></div>
+      <div class="drop-area__item show-color" id="two"></div>
+      <div class="drop-area__item show-color" id="three"></div>
     </div>
   </div>
 
+</div>
+<div class="right-sidebar">
+  <div class="choose-top3-img">
+  </div>
+  <div id="share-button">Share</div>
+  <div id="share-button-temp">Share</div>
+  <div class="node-wrapper advert">
+    <div class="advertisement">
+      <div id="topbox">
+      </div
+    </div>
+  </div>
 </div>

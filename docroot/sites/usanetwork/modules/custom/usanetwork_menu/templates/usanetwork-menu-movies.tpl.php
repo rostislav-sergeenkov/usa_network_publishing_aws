d30f58aecc3b075760095fe60ff397cd8963ca28
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-10-02 15:19:40 +0300

    made styles for movie tab

--- docroot/sites/usanetwork/modules/custom/usanetwork_menu/templates/usanetwork-menu-movies.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_menu/templates/usanetwork-menu-movies.tpl.php
@@ -6,16 +6,16 @@
   <div class="movies-list">
     <?php foreach ($items as $item) :?>
       <div class="node node-usanetwork-promo header-full-episodes-promo<?php print (!empty($item['sponsored']))? ' sponsored-enable': '';?>">
-        <a href="<?php print $item['url']; ?>" class="movies-link" data-name="<?php print $item['file_name']; ?>">
+        <a href="<?php print $item['url']; ?>" class="movies-link">
           <div class="meta-wrapper">
             <div class="meta-wrapper-inner">
               <div class="meta">
-                <div class="meta-icon <?php print !empty($item['icon_type']) ? $item['icon_type'] : 'video-icon'; ?>"></div>
-                <?php if (!empty($item['logo'])): ?>
-                  <div class="logo"><?php print $item['logo']; ?></div>
+                <div class="meta-icon full-video-icon-default"></div>
+                <?php if (!empty($item['title'])): ?>
+                  <div class="title"><?php print $item['title']; ?></div>
                 <?php endif; ?>
                 <?php if (!empty($item['sponsored'])) : ?>
-                  <div class="sponsored" data-mpspath="<?php print $item['file_path']; ?>" data-scalemps="1"></div>
+                  <div class="sponsored" data-scalemps="1"></div>
                 <?php endif; ?>
               </div>
             </div>

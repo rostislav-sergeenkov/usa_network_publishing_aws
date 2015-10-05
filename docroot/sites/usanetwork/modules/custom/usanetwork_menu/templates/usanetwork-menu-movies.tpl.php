9f557100ada17db3ea9e0e30166b722827f0d9c9
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2015-10-05 18:02:53 +0300

    made changes for #1565

--- docroot/sites/usanetwork/modules/custom/usanetwork_menu/templates/usanetwork-menu-movies.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_menu/templates/usanetwork-menu-movies.tpl.php
@@ -10,7 +10,7 @@
           <div class="meta-wrapper">
             <div class="meta-wrapper-inner">
               <div class="meta">
-                <div class="meta-icon full-video-icon-default"></div>
+                <div class="meta-icon movie-play-icon"></div>
                 <?php if (!empty($item['title'])): ?>
                   <div class="title"><?php print $item['title']; ?></div>
                 <?php endif; ?>

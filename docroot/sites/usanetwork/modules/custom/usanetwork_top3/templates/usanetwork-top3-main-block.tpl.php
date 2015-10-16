3b62a98a41d9fac4bcc0a10eb72c35460817377d
Ina Shulha <ina_shulha@epam.com>
2015-10-16 15:32:53 +0300

    TOP3 - All needed changes

--- docroot/sites/usanetwork/modules/custom/usanetwork_top3/templates/usanetwork-top3-main-block.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_top3/templates/usanetwork-top3-main-block.tpl.php
@@ -39,15 +39,15 @@
   </div>
 
   <div id="info"></div>
-  <div class="container-message">Sorry you have already added that item.</div>
+  <div class="container-message"><?php print t('Sorry you have already added that item.'); ?></div>
   <div id="info-block">
     <div id="info-close" class="close-button"></div>
     <div class="info-block-wrapper">
       <div class="info-text">
-        <div class="top-text">Select your top 3 favorite Todd-isms and then share them with your friends and family!</div>
-        <div class="color-text show-color show-font">How it works</div>
-        <div class="bottom-text">Use the icon shown below to click & drag the thumbnail into the ranking boxes. Fill all 3 boxes and share your personalized page.</div>
-        <div id="start-button" class="show-color">Start now!</div>
+        <div class="top-text"><?php print t('Select your top 3 favorite ') . $top3_title . t(' and then share them with your friends and family!'); ?></div>
+        <div class="color-text show-color show-font"><?php print t('How it works'); ?></div>
+        <div class="bottom-text"><?php print t('Use the icon shown below to click & drag the thumbnail into the ranking boxes. Fill all 3 boxes and share your personalized page.'); ?></div>
+        <div id="start-button" class="show-color"><?php print t('Start now!'); ?></div>
       </div>
       <div class="help-image"></div>
       <div class="info-drag-group">
@@ -62,7 +62,7 @@
   <div id="share-block-preview">
     <div id="share-preview-close" class="close-button"></div>
     <div class="share-block-preview-wrapper show-color">
-      <div class="first-text-line">CLICK AND DRAG THE THUMBNAILS OVER A DIFFERENT BOX TO CHANGE THE ORDER OR CLOSE TO CONTINUE PICKING YOUR TOP 3</div>
+      <div class="first-text-line"><?php print t('CLICK AND DRAG THE THUMBNAILS OVER A DIFFERENT BOX TO CHANGE THE ORDER OR CLOSE TO CONTINUE PICKING YOUR TOP 3'); ?></div>
       <div class="preview-items-block">
         <div class="first preview-item" id="preview-one" data-id="one">
 
@@ -74,7 +74,7 @@
 
         </div>
       </div>
-      <div class="last-text-line">Generate your custom share link</div>
+      <div class="last-text-line"><?php print t('Generate your custom share link'); ?></div>
     </div>
   </div>
   <div id="share-block" class="show-color">
@@ -108,18 +108,21 @@
 <div class="right-sidebar">
 
   <div class="choose-top3-img">
-    <div class="top-choose-top3"><div class="first-line">Choose</div> your top <span>3</span></div>
+    <div class="top-choose-top3"><div class="first-line"><?php print t('Choose'); ?></div><?php print t(' your top '); ?><span>3</span></div>
     <?php if (!empty($logo)) : ?>
       <img src="<?php print $logo; ?>" alt="">
     <?php endif; ?>
-    <div class="bottom-choose-top3">& share them with your friends</div>
+    <div class="bottom-choose-top3"><?php print t('& share them with your friends'); ?></div>
   </div>
 
-  <div id="share-button" class="show-color">Share</div>
+  <div id="share-button" class="show-color"><?php print t('Share'); ?></div>
   <div id="gigya-share-top3"></div>
   <div class="node-wrapper advert">
     <div class="advertisement">
       <div id="topbox">
+        <?php if (!empty($advert_block)): ?>
+          <?php print $advert_block; ?>
+        <?php endif; ?>
       </div
     </div>
   </div>

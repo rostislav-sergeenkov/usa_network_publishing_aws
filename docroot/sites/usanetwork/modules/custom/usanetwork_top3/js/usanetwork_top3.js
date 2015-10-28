42eb983a534b4dd8b7f35dbdd4899f3cb0a56f1b
NikolaiVerbitskii <verbitskiinikolai@gmail.com>
2015-10-28 17:26:55 +0300

    fix device video player

--- docroot/sites/usanetwork/modules/custom/usanetwork_top3/js/usanetwork_top3.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_top3/js/usanetwork_top3.js
@@ -1,7 +1,5 @@
 (function ($) {
 
-  $(document).ready(function () {
-
     var body = document.body,
         dropArea = document.getElementById( 'drop-area' ),
         //dropAreaPreview = document.getElementById( 'share-block-preview' ),
@@ -110,6 +108,7 @@
       playerStatus: false, // default value
       mediaLoadStatus: false, // default value
       mediaPlayStatus: false, // default value
+      hideSliderWrapper: false, // default value
 
       createPlayer: function () {
             var src = videoBlock.data('player-src'),
@@ -137,7 +136,6 @@
           return;
         }
 
-
         $pdk.bind('pdk-player');
         $pdk.controller.addEventListener('OnMediaStart', _onMediaStart);
         $pdk.controller.addEventListener('OnMediaPause', _onMediaPause);
@@ -149,15 +147,19 @@
           playerService.mediaLoadStatus = false;
           playerService.mediaPlayStatus = true;
           // change status play button
-          playButton.attr('data-player-status', 'start');
+          playButton
+              .addClass('play')
+              .attr('data-player-status', 'start');
         }
 
         function _onMediaPause(pdkEvent) {
           playButton.removeClass('play');
+          playerService.mediaPlayStatus = false;
         }
 
         function _onMediaUnpause(pdkEvent) {
           playButton.addClass('play');
+          playerService.mediaPlayStatus = true;
         }
 
         function _onMediaLoadStart(pdkEvent) {
@@ -171,14 +173,13 @@
         }
       },
       playPlayer: function () {
-        $pdk.controller.clickPlayButton(true);
-        $pdk.controller.pause(false);
+        $pdk.controller.clickPlayButton();
+        $pdk.controller.pause();
       },
       pausePlayer: function () {
-        $pdk.controller.clickPlayButton(false);
         $pdk.controller.pause(true);
       },
-      showPlayer: function () {
+      setPlayer: function () {
         var activeSlide = $('#slider-container .slick-active'),
             srcLink = activeSlide.find('.video-data').data('src-link'),
             src = activeSlide.find('.video-data').data('src'),
@@ -198,10 +199,35 @@
           });
         }
       },
+      loadPlayer: function () {
+        var activeSlide = $('#slider-container .slick-active'),
+            srcLink = activeSlide.find('.video-data').data('src-link'),
+            src = activeSlide.find('.video-data').data('src'),
+            neighborBlock = activeSlide.find('.slide-content-inner');
+
+        if (videoBlock.hasClass('active')) {
+
+          //change player status
+          playerService.playerStatus = true;
+
+          // change video in player
+          $pdk.controller.loadReleaseURL(srcLink, true);
+
+          neighborBlock.addClass('inactive');
+          videoBlock.velocity("fadeIn", {
+            duration: 200
+          });
+        }
+      },
       hidePlayer: function (el) {
         var activeSlide = $('#slider-container .slick-active'),
             neighborBlock = activeSlide.find('.slide-content-inner'),
             playButton = $('#play-button');
+
+        if (playerService.hideSliderWrapper) {
+          playerService.hideSliderWrapper = false;
+          $('#slider-container .slider-wrapper').show();
+        }
 
         if (playerService.playerStatus) {
 
@@ -259,12 +285,20 @@
         // add click on play button in slide
         $('#play-button').on('click', function () {
 
-          var playBtn = $(this);
+          var playBtn = $(this),
+              isMobileDevice = usa_deviceInfo.mobileDevice;
 
           if (!playBtn.hasClass('inactive')) {
-            playBtn.addClass('inactive play');
+            // added class and change status button
+            playBtn.addClass('inactive');
             // show player
-            playerService.showPlayer();
+            if (isMobileDevice) {
+              playerService.loadPlayer();
+              playerService.hideSliderWrapper = true;
+              $('#slider-container .slider-wrapper').hide();
+            } else {
+              playerService.setPlayer();
+            }
           } else if(playBtn.data('player-status') === 'start') {
             if (playBtn.hasClass('play')) {
               playerService.pausePlayer();
@@ -272,6 +306,20 @@
               playerService.playPlayer();
             }
           }
+
+          //if (!playBtn.hasClass('inactive')) {
+          //  playBtn.addClass('inactive play');
+          //  // show player
+          //  playerService.loadPlayer();
+          //  $('.slider-wrapper').hide();
+          //
+          //} else if(playBtn.data('player-status') === 'start') {
+          //  if (playBtn.hasClass('play')) {
+          //    playerService.pausePlayer();
+          //  } else if (!playBtn.hasClass('play')){
+          //    playerService.playPlayer();
+          //  }
+          //}
         });
 
         /*$("#loading").fadeIn();
@@ -913,6 +961,8 @@
       }
 
     };
+
+  $(document).ready(function () {
     // create player block
     playerService.createPlayer();
     top3Usanetwork.init();

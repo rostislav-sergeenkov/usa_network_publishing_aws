dc7c74d72cfd96a3aa15dfc21b6b12e1cd686227
Ina Shulha <ina_shulha@epam.com>
2015-11-02 14:57:33 +0300

    Top3: Changes for Shared Page

--- docroot/sites/usanetwork/modules/custom/usanetwork_top3/templates/usanetwork-top3-shared.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_top3/templates/usanetwork-top3-shared.tpl.php
@@ -12,7 +12,7 @@
 *  $top3_title: Gallery's title
 */
 ?>
-<div id="shared-container">
+<div id="shared-container" class="<?php print (!empty($isset_video)) ? " set-video" : ""; ?>">
   <div class="choose-top3-img">
     <div class="top-choose-top3"><div class="first-line"><?php print t('I Chose'); ?></div><?php print t('my top'); ?><span><?php print t('3'); ?></span></div>
     <?php if (!empty($logo)) : ?>
@@ -23,17 +23,17 @@
   <div class="chosen-items-block show-color">
     <div class="chosen-items-inner">
       <div id="chosen-player" class="chosen-player">
-        <div id="slider-player" data-player-src="<?php print $player; ?>"></div>
+        <div id="slider-player" data-player-src="<?php print $player; ?>" ></div>
         <div class="img-wrapper">
-          <img src="<?php print $active_element['image']; ?>">
+          <img src="<?php print $slides[0]['image']; ?>">
         </div>
-        <?php if (!empty($active_element['title'])): ?>
-          <div class="title"><?php print $active_element['title']; ?></div>
+        <?php if (!empty($slides[0]['title'])): ?>
+          <div class="title"><?php print $slides[0]['title']; ?></div>
         <?php endif; ?>
       </div>
       <div id="chosen-items-block-wrapper" class="chosen-items-block-wrapper">
         <?php foreach ($slides as $key => $slide) : ?>
-        <div class="chosen-item-thumb show-color show-font<?php print (!empty($slide['active'])) ?  ' active' : '' ?>"
+        <div class="chosen-item-thumb show-color show-font<?php print ($key == 0) ?  ' active' : '' ?>"
              data-src="<?php print !empty($slide['video']) ? $slide['video'] : ''; ?>"
              data-src-link="<?php print !empty($slide['video_link']) ? $slide['video_link'] : ''; ?>">
           <div class="img-wrapper">

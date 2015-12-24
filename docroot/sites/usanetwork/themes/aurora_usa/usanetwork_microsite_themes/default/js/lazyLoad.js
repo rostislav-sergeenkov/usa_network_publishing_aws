f401befccf592f1c13e51ab895774d0c42f3aa3d
NikolaiVerbitskii <verbitskiinikolai@gmail.com>
2015-12-24 13:35:58 +0300

    NBCUUSA-1837

---
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/default/js/lazyLoad.js
@@ -0,0 +1,58 @@
+(function ($) {
+  Drupal.behaviors.ms_lazyLoad = {
+
+    // lazyload checker
+    windowView: function (image){
+      // window variables
+      var $window = $(window);
+      var windowHeight = $window.height(),
+          windowWidth  = $window.width(),
+
+          windowBottom = windowHeight + $window.scrollTop(),
+          windowTop    = windowBottom - windowHeight,
+          windowRight  = windowWidth + $window.scrollLeft(),
+          windowLeft   = windowRight - windowWidth,
+
+      // image variables
+          imageHeight  = image.height(),
+          imageWidth   = image.width(),
+
+          imageTop     = image.offset().top,
+          imageBottom  = imageTop + imageHeight,
+          imageLeft    = image.offset().left,
+          imageRight   = imageLeft + imageWidth;
+
+      // This will return true if any corner of the image is within the screen
+      return (((windowBottom >= imageTop) && (windowTop <= imageTop)) || ((windowBottom >= imageBottom) && (windowTop <= imageBottom))) &&
+          (((windowRight >= imageLeft) && (windowLeft <= imageLeft)) || ((windowRight >= imageRight) && (windowLeft <= imageRight)));
+    },
+
+    loadImage: function(image){
+      image.hide().attr('src', image.data('src')).removeAttr('data-src');
+      image.load(function() {
+        image.siblings('img.lazyloader-icon').remove();
+      });
+    },
+
+    initImgShow: function(items) {
+      console.info('initImgShow');
+      var lazyloaderItems = items || $('img.lazyloader-icon');
+      $.each(lazyloaderItems, function (index, item) {
+        var image = $(item).parent().find('img[data-src]');
+        if(image.attr('data-src')){
+          var imageHeight = image.height(),
+              imageWidth = image.width(),
+              iconTop = Math.round(imageHeight/2),
+              iconLeft = Math.round(imageWidth/2),
+              iconFactor = Math.round(image.siblings('img.lazyloader-icon').height()/2);
+
+          image.siblings('img.lazyloader-icon').css({ top: iconTop - iconFactor, left: iconLeft - iconFactor });
+          if (Drupal.behaviors.ms_lazyLoad.windowView(image)) {
+            Drupal.behaviors.ms_lazyLoad.loadImage(image);
+            image.fadeIn('slow');
+          }
+        }
+      });
+    }
+  };
+})(jQuery);

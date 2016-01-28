8420aabbc263d1ae697889d3881fad095e0bb836
Ina Shulha <ina_shulha@epam.com>
2016-01-28 10:42:26 +0300

    NBCUUSA-1910 - All Colony episodes are not showing end card squeeze

--- docroot/sites/usanetwork/themes/aurora_usa/ds_layouts/usa_mpx_video/usa_mpx_video.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/ds_layouts/usa_mpx_video/usa_mpx_video.tpl.php
@@ -1,4 +1,3 @@
-<?php $next_video = array_shift($endcard) ?>
 <div class="video-block"
      data-tve-player
      data-usa-tve-player-container
@@ -86,91 +85,18 @@
               'accepted' => TRUE,
             ));
             ?>
+            <div data-usa-tve-player="pdk-player"
+                 class="pdk-player-wrap"
+                 data-episode-pid="<?php print $file_pid; ?>"
+                 data-next-url="<?php print $next_video_url; ?>">
+              <?php print $video; ?>
+            </div>
+            <!--  start endcart  -->
+            <?php if (!empty($endcard_enabled)) : ?>
+              <?php print $endcard_template; ?>
+            <?php endif; ?>
+            <!--  end endcart -->
           <?php endif; ?>
-          <div data-usa-tve-player="pdk-player"
-               class="pdk-player-wrap"
-               data-episode-pid="<?php print $file_pid; ?>"
-               data-next-url="<?php print $next_video['url']; ?>">
-            <?php print $video; ?>
-          </div>
-          <!--  start endcart  -->
-          <?php if (!empty($endcard_enabled)) : ?>
-<!--            <div class="close endcard_block" data-end-card="usaEndCard"-->
-<!--                 data-ng-click="hideShowCard()"></div>-->
-            <div class="reload-button endcard_top" data-replay="usa-replay"
-                 data-ng-click="replayVideo()">
-              <div class="replay-title">replay</div>
-              <div
-                class="episode-title"><?php print $filename; ?></div>
-            </div>
-            <div id="episode-up-next" class="endcard_block endcard_topRight"
-                 data-next-name="up next"
-                 data-end-card="usaEndCard"
-                 data-next-url="<?php print $next_video['url']; ?>">
-              <h3>up next...</h3>
-              <div class="node">
-                <a href="<?php print $next_video['url']; ?>" class="link link-up-next">
-                  <div class="asset-img">
-                    <img
-                      src="<?php print $next_video['images']['thumbnail']; ?>"
-                      class="img" alt="">
-                  </div>
-                  <div class="meta-wrapper">
-                    <div class="meta-wrapper-inner">
-                      <div class="meta-icon full-video-icon-default"></div>
-                      <div class="meta">
-                        <div class="additional">S<span
-                            class="season"><?php print $next_video['season']; ?></span>
-                          EP<span
-                            class="episode"><?php print $next_video['episode']; ?></span>:
-                        </div>
-                        <div
-                          class="title"><?php print $next_video['title']; ?></div>
-                      </div>
-                    </div>
-                  </div>
-                </a>
-              </div>
-            </div>
-            <div id="episode-share" class="endcard_block endcard_bottomLeft"
-                 data-end-card="usaEndCard">
-              <h3>share this episode</h3>
-            </div>
-            <div id="episode-related" class="endcard_block endcard_bottomRight"
-                 data-related-name="related clip"
-                 data-end-card="usaEndCard">
-              <h3>related video clips</h3>
-
-              <div id="player-episodes-list" class="player-episodes-list">
-                <ul class="related-slider">
-                  <?php foreach ($endcard as $eitem) { ?>
-                    <li class="slide-item">
-                      <div class="node">
-                        <a href="<?php print $eitem['url']; ?>" class="link link-related-clip">
-                          <div class="asset-img">
-                            <img
-                              src="<?php print $eitem['images']['thumbnail']; ?>"
-                              alt="">
-                          </div>
-                          <div class="meta-wrapper">
-                            <div class="meta-wrapper-inner">
-                              <div class="meta">
-                                <div
-                                  class="title"><?php print $eitem['title']; ?></div>
-                                <div
-                                  class="additional"><?php print $eitem['duration']; ?></div>
-                              </div>
-                            </div>
-                          </div>
-                        </a>
-                      </div>
-                    </li>
-                  <?php } ?>
-                </ul>
-              </div>
-            </div>
-          <?php endif; ?>
-          <!--  end endcart -->
           <?php if (!empty($watchwith) && !empty($ww_airing_id)) : ?>
             <div ng-non-bindable>
               <div id="wwRootContainer">
@@ -202,8 +128,22 @@
               'accepted' => TRUE,
             ));
             ?>
+            <?php if ($full_episode) : ?>
+              <div data-usa-tve-player="pdk-player"
+                   class="pdk-player-wrap"
+                   data-episode-pid="<?php print $file_pid; ?>"
+                   data-next-url="<?php print $next_video_url; ?>">
+                <?php print $video; ?>
+              </div>
+              <!--  start endcart  -->
+              <?php if (!empty($endcard_enabled)) : ?>
+                <?php print $endcard_template; ?>
+              <?php endif; ?>
+              <!--  end endcart -->
+            <?php else: ?>
+              <?php print $video; ?>
+            <?php endif;?>
           <?php endif; ?>
-          <?php print $video; ?>
         </div>
       <?php endif; ?>
     </div>

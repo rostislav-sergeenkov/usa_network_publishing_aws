779bf96f731a3384f9ad5a6a689ce276bb7eeb29
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-02-03 16:13:33 +0300

    fixed #1448

--- docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/mrrobot/templates/usanetwork-microsite-mrrobot-page-videos.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/usanetwork_microsite_themes/mrrobot/templates/usanetwork-microsite-mrrobot-page-videos.tpl.php
@@ -19,15 +19,19 @@
  */
 ?>
 
-<?php if (!empty($section_title)): ?>
-  <!-- section title -->
-  <h2 class="content"><?php print $section_title; ?></h2>
-<?php endif; ?>
+<?php if (!empty($section_title) || !empty($description)): ?>
+  <div class="section-title-block">
+    <?php if (!empty($section_title)): ?>
+      <!-- section title -->
+      <h2 class="content"><?php print $section_title; ?></h2>
+    <?php endif; ?>
 
-<?php if (!empty($description)): ?>
-<div id="video-section-description" class="section-description">
-<?php print $description; ?>
-</div>
+    <?php if (!empty($description)): ?>
+      <div id="video-section-description" class="section-description">
+        <?php print $description; ?>
+      </div>
+    <?php endif; ?>
+  </div>
 <?php endif; ?>
 
 <?php if (!empty($filter_enabled)): ?>

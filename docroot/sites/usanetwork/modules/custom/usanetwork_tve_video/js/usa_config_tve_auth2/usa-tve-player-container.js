8fef9ffc36fb9c47c5e493d2ad2b92115577f91a
NikolaiVerbitskii <verbitskiinikolai@gmail.com>
2016-03-10 17:06:27 +0300

    remade player iframe

--- docroot/sites/usanetwork/modules/custom/usanetwork_tve_video/js/usa_config_tve_auth2/usa-tve-player-container.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tve_video/js/usa_config_tve_auth2/usa-tve-player-container.js
@@ -2,79 +2,7 @@
 (function (ng, $) {
   'use strict';
 
-  // custome usanetwork directive usaTvePlayer
   ng.module('tve.auth.directives')
-      .directive('usaTvePlayer', [
-        '$rootScope',
-        '$sce', 'authService',
-        function ($rootScope, $sce, authService) {
-          return {
-            replace: false,
-            // apply client side iframe rendering to reduce cache problem for dynamic url
-            template: '<iframe data-ng-src="{{src}}" src="about:blank" id="{{id}}" allowfullscreen="" frameborder="0"></iframe>',
-            // directive describe below
-            require: '^usaTvePlayerContainer',
-            scope: true,
-            compile: function(tElement, tAttr) {
-              var config = tAttr;
-
-              return function(scope, element, attr, controller) {
-                var SRC_PARAMS = [{
-                      attrName: 'mbr',
-                      key: 'mbr'
-                    },
-                      {
-                        attrName: 'fwSiteSection',
-                        key: 'FWsiteSection'
-                      }],
-                    MVPD_ID_KEY = 'MVPDid',
-                    params;
-
-                scope.id = config.id;
-
-                authService.promise.then(init);
-
-                function init(status) {
-                  // passing iframe url as trusted to the template
-                  scope.src = $sce.trustAsResourceUrl(config.src + '?' + getQueryParams(status.isAuthenticated && status.mvpdId));
-                }
-
-                /**
-                 * Returns url params string for player source
-                 *
-                 * @param {boolean|string} mvpdId MVPD id string for the authenticated users and false for non.
-                 * @returns {string} query params string
-                 */
-                function getQueryParams(mvpdId) {
-                  if (scope.isEntitled === 'auth') {
-                    params = {
-                      autoPlay: false
-                    };
-                  } else {
-                    params = {
-                      autoPlay: true
-                    };
-                  }
-
-                  ng.forEach(SRC_PARAMS, function(param, i) {
-                    if (param.attrName in config) {
-                      params[param.key] = config[param.attrName];
-                    }
-                  });
-
-                  if (mvpdId) {
-                    params[MVPD_ID_KEY] = mvpdId;
-                  }
-
-                  return $.param(params);
-                }
-              };
-            }
-          };
-        }
-      ])
-
-      // custom usanetwork directive usaTvePlayerContainer
       .directive('usaTvePlayerContainer', [
         '$rootScope',
         'authService', 'tveAuthConfig', 'tveConfig', 'helper', 'tveModal', '$timeout', '$http', '$sce', '$cookies', 'usaPlayerService', 'usaEndCardService',
@@ -84,7 +12,7 @@
           return {
             scope: true,
             controller: ['$scope', function ($scope) {
-              this.isEntitled = function() {
+              this.isEntitled = function () {
                 return $scope.isEntitled;
               };
             }],
@@ -102,7 +30,7 @@
               episodeTitle = attr['episodeTitle'];
               mpxGuid = attr['mpxGuid'];
               isEntitlement = attr['entitlement'];
-              isShowEndCard = attr['showEndCard'] === 'true' ? true : false;
+              isShowEndCard = parseInt(attr['showEndCard']) === 1 ? true : false;
               playerId = tveConfig.PLAYER_ID;
               isMicrosite = body.hasClass('page-node-microsite') ? true : false;
               tveAnalytics = tve.analytics ? tve.analytics : {authzTrack: ng.noop};
@@ -148,12 +76,14 @@
                     if (status.isAuthenticated && isEntitlement === 'auth') {
                       initiateAuthorization();
                     }
-                    setTimeout(function () {
-                      _bindPlayerEvents(playerId);
-                    }, 0);
+                    //setTimeout(function () {
+                    //  _bindPlayerEvents(playerId);
+                    //}, 0);
                   }
                 }
               });
+
+              _bindPlayerEvents(playerId);
 
               // wait when load release
               usaPlayerService.promise.then(function (data) {
@@ -278,15 +208,16 @@
                * @private
                */
               function _onPlayerLoaded(pdkEvent) {
-                if (scope.statusSetToken && !scope.statusPlayerLoaded) {
+                if (!scope.statusPlayerLoaded) {
                   // change status for autoplay
                   scope.$apply(function () {
                     scope.statusPlayerLoaded = true;
                   });
+                }
+                if (scope.statusSetToken && scope.statusPlayerLoaded) {
                   usaPlayerService.resolve('auth success done and load release url finish');
                 }
               }
-
 
               /**
                * Callback for authz Success

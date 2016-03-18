d2e8469d48940372191cca328c2c306f42876d59
Ina Shulha <ina_shulha@epam.com>
2016-03-18 16:45:08 +0300

    Fix Js

--- docroot/sites/usanetwork/themes/aurora_usa/template.php
+++ docroot/sites/usanetwork/themes/aurora_usa/template.php
@@ -194,6 +194,9 @@ function aurora_usa_preprocess_page(&$vars) {
   if(!empty($node) && $node->type == 'consumpt_post') {
     drupal_add_js($theme_path . '/javascripts/consumptionator-gallery.js');
   }
+  if(!empty($node) && $node->type == 'person') {
+    drupal_add_js($theme_path . '/javascripts/consumptionator-gallery.js');
+  }
   // add ios touch icon
   $ios_icon = array(
     '#tag' => 'link',

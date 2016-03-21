b85de1063dbcce1cf5b3b6720b24543f12e4a16f
NikolaiVerbitskii <verbitskiinikolai@gmail.com>
2016-03-21 14:17:56 +0300

    player share card

---
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tve_video/js/usa_config_tve_auth2/usa-share-config.js
@@ -0,0 +1,15 @@
+function share_config(clip) {
+
+  console.log('start: share_config');
+
+  return {
+    "shareLinkVisible": true,
+    "shareLinkParam": "permalink",
+    "shareEmbedVisible": false,
+    "shareEmbedBaseUrl": "http://foo/bar",
+    "shareEmbedString": '<param name="permalink" value="http://www.nbc.com/late-night-with-seth-meyers/video/billy-eichner-julie-klausner-joel-edgerton-vintage-trouble/2882599"/>',
+    "shareFacebookVisible": true,
+    "shareFacebookLink": "http://foo/bar",
+    "shareGoogle+Visible": true
+  };
+}

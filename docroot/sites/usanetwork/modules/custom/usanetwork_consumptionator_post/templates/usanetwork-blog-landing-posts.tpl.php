b7d31fab783d294f90cd7d8d3fd3bcfcb1124058
Ina Shulha <ina_shulha@epam.com>
2016-03-30 17:12:36 +0300

    NBCUUSA-2078 - Initial commit

--- docroot/sites/usanetwork/modules/custom/usanetwork_consumptionator_post/templates/usanetwork-blog-landing-posts.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_consumptionator_post/templates/usanetwork-blog-landing-posts.tpl.php
@@ -6,11 +6,11 @@
 <?php if (!empty($ad)): ?>
   <div class="midbanner" id="show-blog-landing-<?php print $ad_id; ?>"></div>
 <?php endif; ?>
-<?php if (!$ajax_load): ?>
-<div class="landing-list-items-all blog-landing-list-items ajax-load-block<?php print (empty($load_more_link))? ' infinity-finished': '' ;?>" data-node-nid="<?php print $node_nid; ?>">
+<?php if (!empty($ajax_load)) : ?>
+<div class="landing-list-items-all blog-landing-list-items ajax-load-block<?php print (empty($load_more_link))? ' infinity-finished' : '' ;?>" data-node-nid="<?php print (!empty($node_nid)) ? $node_id : ''; ?>">
   <?php endif; ?>
   <div class="landing-list-items-one-item blog-landing-list-items-one-item">
-    <?php if (!$ajax_load): ?>
+    <?php if (!empty($ajax_load)) : ?>
       <div class="upper-bar">
         <div class="title">
           <h2><?php print $title; ?></h2>
@@ -20,7 +20,7 @@
             <div class="filter-label"><?php print !empty($active_filter_title) ? $active_filter_title : t('All seasons'); ?></div>
             <ul class="filter-menu">
               <?php foreach ($filters as $filter): ?>
-                <li class="filter-item"><a href="<?php print $filter['url']; ?>" class="no-ajax <?php if ($filter['active'] == TRUE): print 'active'; endif; ?>"><?php print $filter['title']; ?></a>
+                <li class="filter-item"><a href="<?php print $filter['url']; ?>" class="no-ajax <?php if ($filter['active'] == TRUE) : print 'active'; endif; ?>"><?php print $filter['title']; ?></a>
                 </li>
               <?php endforeach; ?>
             </ul>
@@ -30,16 +30,16 @@
     <?php endif; ?>
     <?php if (!empty($items)): ?>
       <div class="list-items">
-        <?php foreach ($items as $item): ?>
+        <?php foreach ($items as $item) : ?>
           <?php print $item; ?>
         <?php endforeach; ?>
       </div>
     <?php endif; ?>
   </div>
-  <?php if (!empty($load_more_link)): ?>
+  <?php if (!empty($load_more_link)) : ?>
     <div class="load-more-link"><a href="javascript:void(0)" class="more-posts"><?php print t('Load more'); ?></a>
     </div>
   <?php endif; ?>
-  <?php if (!$ajax_load): ?>
-    </div>
+  <?php if (!empty($ajax_load)) : ?>
+  </div>
 <?php endif; ?>

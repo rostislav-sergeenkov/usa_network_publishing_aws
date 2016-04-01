57130cc2e58409d92a34c83236acc63c0ac3f018
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-04-01 17:45:43 +0300

    made some changes for #2082

--- docroot/sites/usanetwork/modules/custom/usanetwork_menu/templates/usanetwork-menu-news.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_menu/templates/usanetwork-menu-news.tpl.php
@@ -1,12 +1,12 @@
 <h2 class="pane-title"><?php print t('Latest On Usa'); ?></h2>
-<div class="movies-list-wrap">
+<div class="news-list-wrap">
   <div class="see-all-link">
     <a href="/news"><?php print t('See more News'); ?></a>
   </div>
-  <div class="movies-list">
+  <div class="news-list">
     <?php foreach ($items as $item) :?>
-      <div class="node node-usanetwork-promo header-full-episodes-promo">
-        <a href="<?php print $item['url']; ?>" class="movies-link">
+      <div class="node node-usanetwork-promo header-news-promo">
+        <a href="<?php print $item['url']; ?>" class="news-link">
           <div class="meta-wrapper">
             <div class="meta-wrapper-inner">
               <div class="meta">

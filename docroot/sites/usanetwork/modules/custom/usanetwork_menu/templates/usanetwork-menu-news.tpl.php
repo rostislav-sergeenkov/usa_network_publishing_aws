be691eb16d22d1484ade71da80c1d69af711ef15
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-04-04 10:27:00 +0300

    made #2082

--- docroot/sites/usanetwork/modules/custom/usanetwork_menu/templates/usanetwork-menu-news.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_menu/templates/usanetwork-menu-news.tpl.php
@@ -7,6 +7,9 @@
     <?php foreach ($items as $item) :?>
       <div class="node node-usanetwork-promo header-news-promo">
         <a href="<?php print $item['url']; ?>" class="news-link">
+          <div class="asset-img">
+            <?php print $item['image']; ?>
+          </div>
           <div class="meta-wrapper">
             <div class="meta-wrapper-inner">
               <div class="meta">
@@ -15,9 +18,6 @@
                 <?php endif; ?>
               </div>
             </div>
-          </div>
-          <div class="asset-img">
-            <?php print $item['image']; ?>
           </div>
         </a>
       </div>

f67a3a8b22be85be9ec698e5cf76257546ba7ce2
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-04-11 14:54:46 +0300

    made #2124

--- docroot/sites/usanetwork/themes/aurora_usa/ds_layouts/usa_mpx_video/usa_mpx_video.tpl.php
+++ docroot/sites/usanetwork/themes/aurora_usa/ds_layouts/usa_mpx_video/usa_mpx_video.tpl.php
@@ -142,18 +142,6 @@
         </div>
       </div>
     <?php else: ?>
-      <div class="download-app">
-        <div class="download-app-wrapper">
-          <div class="image-block"></div>
-          <div
-            class="text-block"><?php print t('Get USA NOW to watch full episodes from any device.'); ?></div>
-          <div class="download-button show-color">
-            <a href="/app" target="_self">
-              <?php print t('Download the app'); ?>
-            </a>
-          </div>
-        </div>
-      </div>
       <?php if (!empty($clips_block)): print $clips_block; endif; ?>
     <?php endif; ?>
   </div>

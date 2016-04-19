d9278a91b201a03b432c4c9092a284decab46334
Ina Shulha <ina_shulha@epam.com>
2016-04-19 17:24:04 +0300

    Header Block

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-new-design-menu.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-new-design-menu.tpl.php
@@ -1,15 +1,20 @@
-<div class="show-title-block-wrapper show-border secondary">
-  <div class="show-title-block show-color">
+<div class="">
+  <div>
+    <a href="/" title="Home" rel="home" class="logo"></a>
+    <div>The Verge</div>
     <?php print $show_title; ?>
+    <?php if (!empty($tune_in_date)): ?>
+      <span><?php print $tune_in_date; ?></span>
+    <?php endif; ?>
+    <?php if (!empty($sponsored_path)) : ?>
+      <div class="sponsored" data-mpspath="<?php print $sponsored_path; ?>" data-scalemps="1"></div>
+    <?php endif; ?>
+  </div>
+  <div>
     <div class="social-icons social-follow">
       <?php foreach ($social_icons as $social_link): ?>
         <?php print $social_link; ?>
       <?php endforeach; ?>
-    </div>
-    <div class="schedule">
-      <?php if (!empty($tune_in_date)): ?>
-        <span><?php print $tune_in_date; ?></span>
-      <?php endif; ?>
     </div>
     <div class="show-menu-tab">
       <?php if (!empty($show_menu_tab_items)): ?>

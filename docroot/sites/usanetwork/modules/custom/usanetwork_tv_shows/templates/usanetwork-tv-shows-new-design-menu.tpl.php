87821e1393bd5c76f6b13de22e2449f4ff1ae9c0
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-04-21 15:40:44 +0300

    made some changes for mr_robot menu

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-new-design-menu.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-new-design-menu.tpl.php
@@ -1,29 +1,36 @@
-<div class="">
-  <div>
+<div class="top-menu-block show-border">
+  <div class="logo-block">
     <a href="/" title="Home" rel="home" class="logo"></a>
-    <div>The Verge</div>
+    <a href="#" class="second-logo"></a>
+  </div>
+  <div class="title-block">
     <?php print $show_title; ?>
     <?php if (!empty($tune_in_date)): ?>
       <span><?php print $tune_in_date; ?></span>
     <?php endif; ?>
-    <?php if (!empty($sponsored_path)) : ?>
-      <div class="sponsored" data-mpspath="<?php print $sponsored_path; ?>" data-scalemps="1"></div>
+  </div>
+  <?php if (!empty($sponsored_path)) : ?>
+    <div class="sponsored" data-mpspath="<?php print $sponsored_path; ?>" data-scalemps="1"></div>
+  <?php endif; ?>
+  <div class="menu-open-button"></div>
+</div>
+<div class="bottom-menu-block show-border">
+  <div class="show-menu-tab">
+    <?php if (!empty($show_menu_tab_items)): ?>
+      <ul class="show-menu menu">
+        <?php foreach ($show_menu_tab_items as $show_menu_tab_item): ?>
+          <li><?php print $show_menu_tab_item['main_link']; ?></li>
+        <?php endforeach; ?>
+      </ul>
     <?php endif; ?>
   </div>
-  <div>
+  <div class="social-block">
+    <div class="hashtag"></div>
     <div class="social-icons social-follow">
       <?php foreach ($social_icons as $social_link): ?>
         <?php print $social_link; ?>
       <?php endforeach; ?>
     </div>
-    <div class="show-menu-tab">
-      <?php if (!empty($show_menu_tab_items)): ?>
-        <ul class="show-menu menu">
-          <?php foreach ($show_menu_tab_items as $show_menu_tab_item): ?>
-            <li><?php print $show_menu_tab_item['main_link']; ?></li>
-          <?php endforeach; ?>
-        </ul>
-      <?php endif; ?>
-    </div>
   </div>
 </div>
+

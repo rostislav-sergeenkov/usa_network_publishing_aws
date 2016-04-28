0bcabf905206cf2b4a62ffb0ec0d97aa28f7161e
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-04-28 17:22:08 +0300

    add new layout for mrrobot show

---
+++ docroot/sites/usanetwork/modules/custom/usanetwork_layouts/plugins/layouts/usa_new_showlanding/usa-new-showlanding.tpl.php
@@ -0,0 +1,34 @@
+<div class="panel-display panel-onecol-stacked clearfix">
+  <div class="panel-wrapper">
+    <?php if($content['one']) : ?>
+      <div class="panel-panel panel-one panel-row first-row aspot-and-episodes">
+        <?php print $content['one']; ?>
+      </div>
+    <?php endif; ?>
+    <?php if($content['two']) : ?>
+      <div class="panel-panel panel-two panel-row editors-block">
+        <?php print $content['two']; ?>
+      </div>
+    <?php endif; ?>
+    <?php if($content['three']) : ?>
+      <div class="panel-panel panel-three panel-row b-c-d-block">
+        <?php print $content['three']; ?>
+      </div>
+    <?php endif; ?>
+    <?php if($content['four']) : ?>
+      <div class="panel-panel panel-four panel-row latest-episodes-block">
+        <?php print $content['four']; ?>
+      </div>
+    <?php endif; ?>
+    <?php if($content['five']) : ?>
+      <div class="panel-panel panel-five panel-row articles-block">
+        <?php print $content['five']; ?>
+      </div>
+    <?php endif; ?>
+    <?php if($content['six']) : ?>
+      <div class="panel-panel panel-six panel-row show-related-content-block">
+        <?php print $content['six']; ?>
+      </div>
+    <?php endif; ?>
+  </div>
+</div>

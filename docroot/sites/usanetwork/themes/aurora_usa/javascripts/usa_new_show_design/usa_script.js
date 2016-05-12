0830eba13456e841413f5cabbd316ea42f124dcc
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-05-12 15:53:19 +0300

    added some js for articles section

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/usa_new_show_design/usa_script.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/usa_new_show_design/usa_script.js
@@ -3,6 +3,19 @@
   //=======================================
   // usa changable-link
   //=======================================
+  function checkDescriptionLines() {
+    if (window.matchMedia("(min-width: " + window_size_tablet_portrait + "px)").matches) {
+      if($('.articles-block').length > 0) {
+        $('.title-and-additional').each(function(){
+          var captionHeight = parseFloat($(this).closest('.meta-wrapper').css('max-height')) - $(this).outerHeight();
+          var captionLineHeight = parseFloat($(this).next().css('line-height'));
+          var lines = Math.floor(captionHeight/captionLineHeight);
+          $(this).next().attr('style', '-webkit-line-clamp:'+ lines +';max-height:'+ captionLineHeight*lines +'px;');
+        });
+      }
+    }
+  }
+
   function initUsaChangableLink() {
     if (usa_deviceInfo.mobileDevice) {
       $('a.changable-link').each(function (i, el) {
@@ -12,7 +25,13 @@
   }
 
   $(document).ready(function () {
-    initUsaChangableLink()
+    initUsaChangableLink();
+    checkDescriptionLines();
+  });
+  $(window).resize(function(){
+    setTimeout(function () {
+      checkDescriptionLines();
+    }, 50);
   });
 
 })(jQuery);

4fc604793d71ebc2a9b4edb848d8560e9e75f503
Ina Shulha <ina_shulha@epam.com>
2016-05-30 13:47:54 +0300

    Sign Up Block

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-new-design-menu.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-new-design-menu.tpl.php
@@ -22,6 +22,11 @@
         <?php foreach ($show_menu_tab_items as $show_menu_tab_item): ?>
           <li><?php print $show_menu_tab_item['main_link']; ?></li>
         <?php endforeach; ?>
+        <?php if (!empty($sign_up_block)) : ?>
+          <li>
+            <a href="<?php print $sign_up_url; ?>" class="secondary show-color show-font"><?php print $sign_up_title; ?></a>
+          </li>
+        <?php endif; ?>
       </ul>
     <?php endif; ?>
   </div>
@@ -38,3 +43,8 @@
     </div>
   </div>
 </div>
+<?php if (!empty($sign_up_block)) : ?>
+  <div id="block-usanetwork-lyris-newsletter-subscription">
+    <?php print $sign_up_block; ?>
+  </div>
+<?php endif; ?>

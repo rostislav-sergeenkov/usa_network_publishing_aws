9c245df1f0d9edc72dd0783395d3aa2307029bdc
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-05-30 15:55:36 +0300

    made some changes for video landing

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-landing-videos-item-small.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-landing-videos-item-small.tpl.php
@@ -6,6 +6,11 @@
 <div class="node node-usanetwork-promo small three-line-video<?php if (!empty($classes)): print ' ' . $classes; endif; ?><?php if (!empty($custom_classes)): print ' ' . $custom_classes; endif; ?>">
   <?php if (!empty($target_url)): ?>
     <a href="<?php print $target_url; ?>">
+      <div class="asset-img">
+        <?php if (!empty($image_desktop)): ?>
+          <img src="<?php print $image_desktop; ?>" alt=""/>
+        <?php endif; ?>
+      </div>
       <div class="meta-wrapper">
         <div class="meta-wrapper-inner">
           <div class="meta-icon <?php print $media_icon; ?>"></div>
@@ -21,11 +26,6 @@
             <?php endif; ?>
           </div>
         </div>
-      </div>
-      <div class="asset-img">
-        <?php if (!empty($image_desktop)): ?>
-          <img src="<?php print $image_desktop; ?>" alt=""/>
-        <?php endif; ?>
       </div>
     </a>
   <?php endif; ?>

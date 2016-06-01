f5d5d2eaa8bc8bd10b34c2a84009acb74c6eccb8
Karavatsky <ihar_karavatski@epam.com>
2016-06-01 14:11:38 +0300

    NBCUUSA-2265. Custom title for relevant content block.

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/plugins/templates/new-design-relevant-content.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/plugins/templates/new-design-relevant-content.tpl.php
@@ -3,7 +3,9 @@
  * Relevant content.
  */
 ?>
-<div class="carousel-title-block show-color show-font">Most popular</div>
+<?php if (!empty($data['#content']['title'])): ?>
+  <div class="carousel-title-block show-color show-font"><?php print $data['#content']['title']; ?></div>
+<?php endif; ?>
 <div id="relevant-content-carousel" class="usa-carousel">
   <ul class="slider-list">
     <?php foreach ($data['#content']['items'] as $item): ?>

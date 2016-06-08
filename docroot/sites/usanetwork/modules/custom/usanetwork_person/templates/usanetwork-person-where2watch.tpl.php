8af5211150585699cbcd9be9ee6541df012012d4
Ina Shulha <ina_shulha@epam.com>
2016-06-08 10:05:08 +0300

    NBCUUSA-2173 - [STUB] Cast&Info Landing Page - Update FE template

--- docroot/sites/usanetwork/modules/custom/usanetwork_person/templates/usanetwork-person-where2watch.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_person/templates/usanetwork-person-where2watch.tpl.php
@@ -8,20 +8,40 @@
     <h2 class="title"><?php print !empty($title) ? $title : ''; ?></h2>
   </div>
   <div class="where-to-watch-inner">
-    <a href="/videos/live" class="live-link show-border"><?php print t('watch now on usanetwork.com'); ?><span class="show-color show-font"></span></a>
+    <?php if (!empty($is_full_exists)) : ?>
+      <div class="description-block">
+        <?php print t('Watch now on:'); ?>
+      </div>
+      <div class="show-color">
+        <?php print $usa_live_link; ?>
+      </div>
+      <div class="show-color">
+        <?php print $usa_now_path; ?>
+      </div>
+    <?php endif; ?>
+    <?php if (!empty($featured_elements)) : ?>
+      <div class="description-block">
+        <?php print t('Watch the entire first season on:'); ?>
+      </div>
+      <div class="usa-providers">
+        <?php foreach ($featured_elements as $featured_element) : ?>
+          <div class="providers-row">
+            <div class="providers-row-inner">
+              <div class="provider">
+                <a href="<?php print $featured_element['url']; ?>">
+                  <img src="<?php print $featured_element['image']; ?>">
+                </a>
+              </div>
+            </div>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    <?php endif; ?>
     <div class="description-block">
       <?php print $description; ?>
     </div>
     <div class="usa-providers">
       <?php $i = 1; ?>
-      <?php if (!empty($usa_now_path)) : ?>
-        <?php $i = 2; ?>
-        <div class="providers-row">
-          <div class="providers-row-inner show-border">
-            <div class="provider">
-              <a href="<?php print $usa_now_path; ?>"><img src="<?php print $usa_now_image; ?>"></a>
-            </div>
-      <?php endif; ?>
       <?php foreach ($elements as $element) : ?>
         <?php if ($i == 1) : ?>
             <div class="providers-row">

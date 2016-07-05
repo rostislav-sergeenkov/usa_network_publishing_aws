2d5be9866ce5bcd039fcaf37174bc9999c77699f
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-07-05 15:50:42 +0300

    changes image styles

--- docroot/sites/usanetwork/modules/custom/usanetwork_consumptionator_post/templates/usanetwork-cons-post-carousel-item-h.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_consumptionator_post/templates/usanetwork-cons-post-carousel-item-h.tpl.php
@@ -5,18 +5,24 @@
     <?php if (!empty($url)): ?>
       <a href="<?php print $url; ?>">
         <?php if (!empty($image)): ?>
-          <div class="asset-img" data-picture data-alt="" data-class="tile-img">
-            <?php if (!empty($image_mobile)): ?>
-              <div data-src="<?php print $image_mobile; ?>"></div>
-            <?php endif; ?>
-            <?php if (!empty($image)): ?>
-              <div data-media="(min-width: 641px)" data-src="<?php print $image; ?>"></div>
-              <!--[if (IE 8) & (!IEMobile)]>
-              <div data-src="<?php print $image; ?>"></div>
-              <![endif]-->
-              <noscript><img src="<?php print $image; ?>" width="633" height="356" alt="" title="" /></noscript>
-            <?php endif; ?>
-          </div>
+          <?php if (!$new_design): ?>
+            <div class="asset-img" data-picture data-alt="" data-class="tile-img">
+              <?php if (!empty($image_mobile)): ?>
+                <div data-src="<?php print $image_mobile; ?>"></div>
+              <?php endif; ?>
+              <?php if (!empty($image)): ?>
+                <div data-media="(min-width: 641px)" data-src="<?php print $image; ?>"></div>
+                <!--[if (IE 8) & (!IEMobile)]>
+                <div data-src="<?php print $image; ?>"></div>
+                <![endif]-->
+                <noscript><img src="<?php print $image; ?>" width="633" height="356" alt="" title="" /></noscript>
+              <?php endif; ?>
+            </div>
+          <?php else: ?>
+            <div class="asset-img">
+              <img src="<?php print $image; ?>" alt="" title="" />
+            </div>
+          <?php endif; ?>
         <?php endif; ?>
         <div class="meta-wrapper">
           <?php if (empty($new_design)) : ?>

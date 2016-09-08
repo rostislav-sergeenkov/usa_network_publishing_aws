8fce66452340d0331c2487b3c0b6d6652d067a1b
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-09-08 14:14:11 +0300

    made #2582

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/consumptionator-sticky-share.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/consumptionator-sticky-share.js
@@ -9,11 +9,18 @@
               stickySharePosition = stickyShare.offset()['top'],
               bottomLinePosition = $('#main-block-bottom-line').offset()['top'],
               bottomPadding = parseFloat(stickyShare.next().css('padding-bottom')),
-              topCoefficient = 150/1901,
-              currentCoefficient = ($('.sticky-share').offset()['top'] - $(window).scrollTop())/window.innerWidth;
+              newDesign = $('body').hasClass('show-new-design'),
+              stickyShareOffsetTop = $('.sticky-share').offset()['top'] - $(window).scrollTop(),
+              headerHeight = $('#header').outerHeight(),
+              topCoefficient = newDesign? 195/1600: 150/1901,
+              currentCoefficient = stickyShareOffsetTop/window.innerWidth;
+          console.info(topCoefficient);
+          console.info(currentCoefficient);
           if((bottomLinePosition - stickySharePosition - stickyShareHeight) < bottomPadding) {
             stickyShare.css({
-              bottom: bottomPadding + "px",
+              bottom: bottomPadding + 'px',
+              left: 0,
+              margin: 0,
               position: 'absolute',
               top: 'auto'
             })
@@ -21,13 +28,28 @@
             $('.sticky-share').removeAttr('style');
           }
           if (currentCoefficient > topCoefficient) {
-            $('.sticky-share').removeAttr('style')
+            $('.sticky-share').removeAttr('style');
           }
         } else {
           $('.sticky-share').removeAttr('style');
         }
       } else {
         $('.sticky-share').removeAttr('style');
+      }
+      if ($('body').hasClass('show-new-design') && window.matchMedia("(min-width: " + window_size_desktop_medium + "px)").matches) {
+        if ($('.right-rail-line').hasClass('visible') && $('header').hasClass('off-elements') && $('.sticky-share').css('position') == 'fixed') {
+          var stickyShare = $('.sticky-share'),
+              leftOffset = parseFloat(stickyShare.css('left')),
+              minLeft = 20;
+          if (leftOffset < minLeft) {
+            var leftDiff = minLeft - leftOffset;
+            stickyShare.css({
+              marginLeft: leftDiff + 'px'
+            })
+          } else {
+            stickyShare.removeAttr('style');
+          }
+        }
       }
     },
     attach: function (context, settings) {

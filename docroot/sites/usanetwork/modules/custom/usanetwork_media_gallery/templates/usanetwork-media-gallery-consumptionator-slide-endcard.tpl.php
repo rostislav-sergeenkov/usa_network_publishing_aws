a5d3009ca280e629d3002a3778fd7101845670e3
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-09-14 16:52:58 +0300

    made some changes for #2583

--- docroot/sites/usanetwork/modules/custom/usanetwork_media_gallery/templates/usanetwork-media-gallery-consumptionator-slide-endcard.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_media_gallery/templates/usanetwork-media-gallery-consumptionator-slide-endcard.tpl.php
@@ -1,10 +1,6 @@
 <?php
 /**
- * $gigya_id - id of Gigya
- * $image image url
- * $info => array(
- *   'description' => NULL, // Description of image
- * );
+ ***
  */
 ?>
 <div class="node node-gallery end-card">
@@ -14,17 +10,45 @@
     <?php endif; ?>
   </div>
   <div class="slide-info">
-    <div class="description"><?php if (!empty($info['description'])): ?><?php print $info['description']; ?><?php endif; ?></div>
-    <div>
-      <div><?php if (!empty($endcard['image'])): ?><?php print $endcard['image']; ?><?php endif; ?></div>
-      <div><?php if (!empty($endcard['title'])): ?><?php print $endcard['title']; ?><?php endif; ?></div>
-      <div><?php if (!empty($endcard['caption'])): ?><?php print $endcard['caption']; ?><?php endif; ?></div>
+    <div class="end-card-description">
+      <div class="end-card-description-wrapper">
+        <?php if (!empty($info['description'])): ?>
+          <div class="description"><?php print $info['description']; ?></div>
+        <?php endif; ?>
+      </div>
     </div>
     <div class="slider-counter">
       <span class="gallery-name">
         <?php if (!empty($info['gallery_name'])): ?><?php print $info['gallery_name']; ?><?php endif; ?>
       </span>
       <span class="slide-index"></span>
+    </div>
+  </div>
+  <div class="end-card-block">
+    <div class="end-card-block-back"></div>
+    <div class="end-card-block-wrapper">
+      <div class="end-card-block-meta">
+        <div class="up-next"><?php print t('Up next:'); ?></div>
+        <?php if (!empty($endcard['title'])): ?>
+          <div class="other-gallery-name">
+            <a href="#">
+              <?php print $endcard['title']; ?>
+            </a>
+          </div>
+        <?php endif; ?>
+        <?php if (!empty($endcard['caption'])): ?>
+          <div class="other-gallery-episode">
+            <?php print $endcard['caption']; ?>
+          </div>
+        <?php endif; ?>
+        <?php if (!empty($endcard['image'])): ?>
+          <div class="other-gallery-image">
+            <a href="#">
+              <img src="<?php print $endcard['image']; ?>" alt="">
+            </a>
+          </div>
+        <?php endif; ?>
+      </div>
     </div>
   </div>
 </div>

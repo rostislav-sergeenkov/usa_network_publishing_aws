8278202491034989373221c9fba51bbeada21579
Ina Shulha <ina_shulha@epam.com>
2016-09-14 17:21:48 +0300

    NBCUUSA-2583 - Gallery End-Cards ShareBar

--- docroot/sites/usanetwork/modules/custom/usanetwork_media_gallery/templates/usanetwork-media-gallery-consumptionator-slide-endcard.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_media_gallery/templates/usanetwork-media-gallery-consumptionator-slide-endcard.tpl.php
@@ -20,6 +20,7 @@
       <div><?php if (!empty($endcard['title'])): ?><?php print $endcard['title']; ?><?php endif; ?></div>
       <div><?php if (!empty($endcard['caption'])): ?><?php print $endcard['caption']; ?><?php endif; ?></div>
       <div><?php if (!empty($endcard['url'])): ?><?php print $endcard['url']; ?><?php endif; ?></div>
+      <div><?php if (!empty($endcard['share_bar'])): ?><?php print $endcard['share_bar']; ?><?php endif; ?></div>
     </div>
     <div class="slider-counter">
       <span class="gallery-name">

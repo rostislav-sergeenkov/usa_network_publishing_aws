62b3fea8b109d7ecd3f8d24702950f1b908511a3
Karavatsky <ihar_karavatski@epam.com>
2016-09-27 11:40:59 +0300

    NBCUUSA-2616. Oun button template for gigya buttons.

---
+++ docroot/sites/usanetwork/modules/custom/usanetwork_menu/templates/usanetwork-menu-gigya-sharebar-button.tpl.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Gigya share button template.
+ */
+
+?>
+
+
+<div class="usa-share-button $text" onclick="$onClick">
+  <div class="button-text">
+    Share on $text
+  </div>
+</div>

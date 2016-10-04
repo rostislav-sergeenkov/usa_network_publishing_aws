0c3f0a36579bd3bfcd2af11839925218f3d82408
Karavatsky <ihar_karavatski@epam.com>
2016-10-04 15:50:42 +0300

    NBCUUSA-2637. Add BE part of new right rail.

---
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-right-rail-b-item.tpl.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * @file
+ * Right rail item template.
+ *
+ * Available variables:
+ * - $url
+ * - $image_mobile
+ * - $logic_violator
+ * - $image_desktop
+ * - $title
+ * - $content_count
+ *
+ */
+
+?>
+
+<div class="node node-usanetwork-promo usa-aspot-carousel-promo">
+  <a href="<?php print !empty($url) ? $url : '#'; ?>">
+    <div class="image-block">
+      <div class="asset-img" data-picture="" data-alt="" data-class="tile-img">
+        <?php if (!empty($image_mobile)): ?>
+          <div data-src="<?php print $image_mobile; ?>"></div>
+        <?php endif; ?>
+        <?php if (!empty($image_desktop)): ?>
+          <div data-media="(min-width: 769px)" data-src="<?php print $image_desktop; ?>"></div>
+          <!--[if (IE 8) & (!IEMobile)]>
+          <div data-src="<?php print $image_desktop; ?>"></div>
+          <![endif]-->
+        <?php endif; ?>
+        <?php if (!empty($image_desktop)): ?>
+          <noscript><img src="<?php print $image_desktop; ?>" alt="" title="" /></noscript>
+        <?php endif; ?>
+      </div>
+      <div class="item-number-border show-color show-font"></div>
+      <div class="item-number">
+        <?php if (!empty($content_count)): ?>
+          <?php print $content_count; ?>
+        <?php endif; ?>
+      </div>
+    </div>
+    <div class="meta-wrapper">
+      <div class="meta-wrapper-inner">
+        <div class="meta">
+          <?php if (!empty($logic_violator)): ?>
+            <div class="violator"><?php print $logic_violator; ?></div>
+          <?php endif; ?>
+          <?php if (!empty($title)): ?>
+            <div class="title"><?php print $title; ?></div>
+          <?php endif; ?>
+        </div>
+      </div>
+    </div>
+  </a>
+</div>

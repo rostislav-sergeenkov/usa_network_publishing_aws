d88ff4dbf340ffa35aa259dd8af761afa325474b
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-10-28 11:28:52 +0300

    fixed #2676

--- docroot/sites/usanetwork/modules/custom/usanetwork_video_live/js/usanetwork_video_live.dynamic_content.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_video_live/js/usanetwork_video_live.dynamic_content.js
@@ -5,7 +5,7 @@
     // gigyaSharebar
     lazyLoadImages: function() {
       var items = $('.usanetwork-quiz').find('img[data-src]');
-      Drupal.behaviors.lazy_load_custom.lazyLoadImages(items);
+      Drupal.behaviors.lazy_load_custom.lazyLoadImages(items, true);
     },
     initGigyaSharebar: function (data) {
 

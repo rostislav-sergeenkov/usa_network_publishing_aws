f3b57f3f6aaf01730ba3ad76a2565fc504dc746d
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-11-18 12:06:28 +0300

    made promo rules for best-of blocks

--- docroot/sites/usanetwork/themes/aurora_usa/javascripts/lazy-load-custom.js
+++ docroot/sites/usanetwork/themes/aurora_usa/javascripts/lazy-load-custom.js
@@ -57,7 +57,6 @@
       $.each(items, function (i, carousel_item) {
 
         var images = $(carousel_item).find('img[data-src]');
-        console.info(images);
         Drupal.behaviors.lazy_load_custom.lazyLoadImages(images);
 
       });

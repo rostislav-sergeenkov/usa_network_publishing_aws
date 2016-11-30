192b2ac82758a4ffe3e5a4805498e18bdefd4f69
NikolaiVerbitskii <verbitskiinikolai@gmail.com>
2016-11-30 12:12:43 +0300

    2720 fix: added chrome check flash

--- docroot/sites/usanetwork/modules/custom/usanetwork_tve_video/js/usa_config_tve_auth2/usa-tve-auth2.js
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tve_video/js/usa_config_tve_auth2/usa-tve-auth2.js
@@ -1019,6 +1019,7 @@
                   }
                   if (reason === 'flash' && usaPlayerError.checkIfChrome55(navigator.userAgent)) {
                     isFlashBlockChrome55 = true;
+                    usaPlayerError.initFlashError();
                     tveErrorHandler.showErrorMessage(tveErrorHandler.errors.CHROME_FLASH_55);
                   } else if (navigator.userAgent.indexOf('Mac') > 0 && navigator.userAgent.indexOf('Safari') > 0 && navigator.userAgent.indexOf('Chrome') == -1) {
                     tveErrorHandler.showErrorMessage(tveErrorHandler.errors.MAC_SAFARI);

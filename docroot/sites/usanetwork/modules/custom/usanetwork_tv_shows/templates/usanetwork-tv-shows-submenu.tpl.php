1aec2980eeaf3acc3e9bee5768ad105b1a01dfb3
Aliaksandr Fiodarau <Aliaksandr_Fiodarau@epam.com>
2016-12-06 17:00:39 +0300

    made some changes

--- docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-submenu.tpl.php
+++ docroot/sites/usanetwork/modules/custom/usanetwork_tv_shows/templates/usanetwork-tv-shows-submenu.tpl.php
@@ -4,13 +4,13 @@
     <div class="social-icons social-follow">
       <?php foreach ($social_icons as $social_link): ?>
         <?php print $social_link['link']; ?>
-        <?php if ('social' == $social_link['type']): ?>
+        <?php/* if ($social_link['type'] == 'sing-up'): ?>
           <div class="usa-newsletter-subscription-wrap">
-              <div id="usa-newsletter-subscription-mobile" class="show-border">
-                <?php print $sign_up_block_additional; ?>
-              </div>
+            <div id="usa-newsletter-subscription-mobile" class="show-border">
+              <?php print $sign_up_block_additional; ?>
+            </div>
           </div>
-        <?php endif; ?>
+        <?php endif; */?>
       <?php endforeach; ?>
     </div>
     <div class="schedule">
@@ -35,9 +35,9 @@
   </div>
 </div>
 <?php if (!empty($sign_up_block)) : ?>
-    <div class="usa-newsletter-subscription-wrap">
-        <div id="usa-newsletter-subscription-desktop" class="show-border">
-          <?php print $sign_up_block; ?>
-        </div>
+  <div class="usa-newsletter-subscription-wrap">
+    <div id="usa-newsletter-subscription-desktop" class="show-border">
+      <?php print $sign_up_block; ?>
     </div>
+  </div>
 <?php endif; ?>
